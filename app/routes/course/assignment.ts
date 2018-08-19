7364e6d3e92fbaefd87d4617d0ec0334747d73ce
Alexandr Zhukovets <zhukovets95@gmail.com>
2018-08-19 13:16:17 +0300

    feat: added aggregation assignments with tasks

--- app/routes/course/assignment.ts
+++ app/routes/course/assignment.ts
@@ -1,14 +1,40 @@
 import { INTERNAL_SERVER_ERROR, OK } from 'http-status-codes';
 import * as Router from 'koa-router';
-import { AssignmentModel, IApiResponse, IAssignmentModel } from '../../models';
+import { AssignmentModel, IApiResponse, IAssignmentModel, TaskModel } from '../../models';
 import { setResponse } from '../utils';
 
 export const courseAssignmentGetRoute = async (ctx: Router.IRouterContext) => {
     try {
         const { courseId, studentId } = ctx.params;
-        const assignments = await AssignmentModel.find({ courseId, studentId }).exec();
+        const assignments = await AssignmentModel.find({
+            courseId,
+            studentId,
+        }).exec();
+
+        const taskIdCollection: string[] = [];
+        for (const index in assignments) {
+            if (assignments[index]) {
+                taskIdCollection.push(assignments[index].taskId);
+            }
+        }
+        const tasks = await TaskModel.find({
+            _id: taskIdCollection,
+        }).exec();
+
+        const tasksForAggregationWithAssignments: any = tasks;
+        const aggregatedAssignments: any = assignments;
+
+        for (const index in aggregatedAssignments) {
+            if (aggregatedAssignments.hasOwnProperty(index)) {
+                aggregatedAssignments[index]._doc = {
+                    ...tasksForAggregationWithAssignments[index]._doc,
+                    ...aggregatedAssignments[index]._doc,
+                };
+            }
+        }
+
         const body: IApiResponse<IAssignmentModel> = {
-            data: assignments,
+            data: aggregatedAssignments,
         };
         ctx.body = body;
         ctx.status = OK;
@@ -22,7 +48,13 @@ export const courseAssignmentPatchRoute = async (ctx: Router.IRouterContext) => 
     try {
         const { courseId, studentId } = ctx.params;
         const { ...body } = ctx.request.body;
-        const result = await AssignmentModel.findOneAndUpdate({ courseId, studentId }, body).exec();
+        const result = await AssignmentModel.findOneAndUpdate(
+            {
+                courseId,
+                studentId,
+            },
+            body,
+        ).exec();
         setResponse(ctx, OK, result);
 
         ctx.status = OK;
