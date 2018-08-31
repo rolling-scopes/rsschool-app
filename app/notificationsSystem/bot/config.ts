9dad977c55128cdcb9e65ab11bbf7721753b714f
Lozovsky Alexander <lozovsky.alx@yandex.ru>
2018-08-31 17:26:39 +0300

    refactor(notifications): bot config

--- app/notificationsSystem/bot/config.ts
+++ app/notificationsSystem/bot/config.ts
@@ -14,20 +14,19 @@ export interface IConfig {
 }
 
 export const config: IConfig = {
-    knownCommands: ['/start', '/help', '/enable', '/disable', '/settime', '/showsettings'],
+    knownCommands: ['/start', '/help', '/enable', '/disable', '/settime'],
     messages: {
-        disable: 'Notifications disabled',
-        enable: 'Notifications enabled',
-        iDontKnowYou: "I can't find you in app. Please, subscribe on me in your profile in rsschool-app",
+        disable: 'Notifications was disabled',
+        enable: 'Notifications was enabled',
+        iDontKnowYou: "I can't find you in app. Please, subscribe to me in your profile in rsschool-app",
         iDontUnderstandYou: "Sorry, I don't understand you",
-        invalidTimeInterval: 'Sorry, your parameters is invalid. Please, use this format:\nsetTime hh:mm hh:mm',
-        subscribe: 'You was subscribed on notifications',
+        invalidTimeInterval: 'Sorry, your parameters is invalid. Please, use this format:\n/setTime hh:mm hh:mm',
+        subscribe: 'You was subscribed to notifications',
         timeSetted: 'Time was setted',
         welcome: `Hello. I can help you manage notifications. List of available options:
 /enable - enable notifications
 /disable - disable notifications
-/settime - Set time interval when you want to receive notifications. For expample, /settime 12:00 13:00
-/chooseevents  - Choose the events that you want to receive notifications on`,
+/settime - Set time interval when you want to receive notifications. For expample, /settime 12:00 13:00`,
     },
     token: '560576009:AAGAH2VBbKQPrkewz_a_S5jk8e-dA5fQ4ew',
 };
