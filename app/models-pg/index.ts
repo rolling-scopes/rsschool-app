bce4df8f341c5791676c6f4ba0a76f9f1d8fffc0
Andrei Palchys <andrei_palchys@epam.com>
2019-04-16 13:36:39 +0300

    feat: update

--- app/models-pg/index.ts
+++ app/models-pg/index.ts
@@ -2,7 +2,10 @@ import { User } from './user';
 import { Feedback } from './feedback';
 import { Course } from './course';
 import { Task } from './task';
+import { TaskResult } from './taskResult';
+import { StudentTask } from './studentTask';
+import { CourseTask } from './courseTask';
 
-export const models = [User, Feedback, Course, Task];
+export const models = [User, Feedback, Course, Task, TaskResult, StudentTask, CourseTask];
 
 export { User, Feedback, Course, Task };
