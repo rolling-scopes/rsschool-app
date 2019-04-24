d7964134b91275e260ced0a281fe2a584db64db4
Andrei Palchys <andrei_palchys@epam.com>
2019-04-24 13:42:04 +0300

    fix: update

--- src/routes/course/score.ts
+++ src/routes/course/score.ts
@@ -21,6 +21,8 @@ export const postScore = (_: ILogger) => async (ctx: Router.RouterContext) => {
     return;
   }
 
+  console.log(ctx.request.body);
+
   const data: ScoreInput = ctx.request.body;
 
   if (!data.githubPrUrl) {
@@ -38,7 +40,7 @@ export const postScore = (_: ILogger) => async (ctx: Router.RouterContext) => {
   const id = ctx.state!.user.id;
   const mentor = await getRepository(Mentor)
     .createQueryBuilder('mentor')
-    .where('mentor.courseId = :courseId', { courseId })
+    .where('mentor."courseId" = :courseId', { courseId })
     .innerJoinAndSelect('mentor.user', 'user', 'user.id = :id', {
       id,
     })
