315739579b084710ea68ad60b03d57abcc152a7d
Andrei Palchys <andrei_palchys@epam.com>
2019-04-29 14:54:38 +0300

    feat: add get tasks api

--- src/routes/tasks/index.ts
+++ src/routes/tasks/index.ts
@@ -1,31 +1,51 @@
 import * as Router from 'koa-router';
 import { OK } from 'http-status-codes';
+import * as url from 'url';
 import { ILogger } from '../../logger';
 import { Task } from '../../models';
 import { getRepository } from 'typeorm';
 import { setResponse } from '../utils';
 import { adminGuard } from '../guards';
+import { OperationResult } from '../../services/operationResult';
 
 type TaskInput = {
   name: string;
+  descriptionUrl?: string;
 };
 
-const postTasks = (_: ILogger) => async (ctx: Router.RouterContext) => {
+const postTasks = (logger: ILogger) => async (ctx: Router.RouterContext) => {
   const data: TaskInput[] = ctx.request.body;
 
-  const response = [];
+  const response: OperationResult[] = [];
 
   for await (const item of data) {
     try {
-      const repository = getRepository(Task);
-      await repository.save(item);
-      response.push(`added: ${item.name}`);
+      if (item.descriptionUrl) {
+        try {
+          new url.URL(item.descriptionUrl);
+        } catch (e) {
+          const message = `[${item.descriptionUrl}] is not url`;
+          response.push({ status: 'failed', value: message });
+          continue;
+        }
+      }
+      const result = await getRepository(Task).save(item);
+      response.push({ status: 'created', value: result.id });
     } catch (e) {
-      response.push(`failed: ${item.name}`);
+      logger.error(e);
+      response.push({ status: 'failed', value: item.name });
     }
   }
 
   setResponse(ctx, OK, response);
+};
+
+const getTasks = (_: ILogger) => async (ctx: Router.RouterContext) => {
+  const tasks = await getRepository(Task)
+    .createQueryBuilder()
+    .getMany();
+
+  setResponse(ctx, OK, tasks);
 };
 
 export function tasksRoute(logger: ILogger) {
@@ -46,6 +66,22 @@ export function tasksRoute(logger: ILogger) {
    *          description: operation status
    */
   router.post('/', adminGuard, postTasks(logger));
+
+  /**
+   * @swagger
+   *
+   * /tasks:
+   *   get:
+   *      description: Gets tasks
+   *      security:
+   *        - cookieAuth: []
+   *      produces:
+   *        - application/json
+   *      responses:
+   *        200:
+   *          description: List of tasks
+   */
+  router.get('/', getTasks(logger));
 
   return router;
 }
