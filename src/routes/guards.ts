045ee77d1da9d8138ef1f93bd18dff6edccabc54
Andrei Palchys <andrei_palchys@epam.com>
2019-04-30 14:59:31 +0300

    fix: guard condition

--- src/routes/guards.ts
+++ src/routes/guards.ts
@@ -3,15 +3,15 @@ import * as Router from 'koa-router';
 const auth = require('koa-basic-auth'); //tslint:disable-line
 import { config } from '../config';
 
+const basicAuthAdmin = auth({ name: config.admin.username, pass: config.admin.password });
+
 export const guard = async (ctx: Router.RouterContext, next: () => Promise<void>) => {
   if (ctx.state.user != null && (ctx.isAuthenticated() || config.isDevMode)) {
-    await next();
+    return basicAuthAdmin(ctx, next);
   } else {
     ctx.status = UNAUTHORIZED;
   }
 };
-
-const basicAuthAdmin = auth({ name: config.admin.username, pass: config.admin.password });
 
 export const adminGuard = async (ctx: Router.RouterContext, next: () => Promise<void>) => {
   if (ctx.state.user != null && ctx.state.user.isAdmin) {
