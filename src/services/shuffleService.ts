bd34dba4bef06ded24a7bf062fdd96cb5ea7eeb5
Siarhei Melnik <siarhei.m.a@gmail.com>
2019-05-30 23:28:39 +0300

    feat: fair distribution (#54)

--- src/services/shuffleService.ts
+++ src/services/shuffleService.ts
@@ -29,10 +29,11 @@ export const shuffleCourseMentors = (logger: ILogger) => async (courseId: number
   }
 
   const students = mentors.map(m => m.students).reduce((acc: any, v) => acc.concat(v), []);
-  const notExpeledStudents = students.filter((std: any) => std.isExpelled === false);
+  const notExpeledStudents = students.filter((std: any) => Boolean(std.isExpelled) === false);
+
   const studentRestrictions = mentors.map(v => ({
     id: v.id,
-    maxStudents: (v.students || []).length,
+    maxStudents: (v.students || []).filter((std: any) => Boolean(std.isExpelled) === true).length,
   }));
 
   const randomStudents = shuffleArray(notExpeledStudents);
