228d8d0a559bba8fd8bbb10693fe25e740373a4d
Siarhei Melnik <siarhei.m.a@gmail.com>
2019-06-27 11:55:56 +0300

    Mentor feedback (#64)
    * feat: add mentor feedback

--- src/models/index.ts
+++ src/models/index.ts
@@ -10,6 +10,7 @@ import { Stage } from './stage';
 import { TaskChecker } from './taskChecker';
 import { TaskArtefact } from './taskArtefact';
 import { TaskInterviewResult } from './taskInterviewResult';
+import { StudentFeedback } from './studentFeedback';
 
 export * from './session';
 
@@ -26,6 +27,7 @@ export const models = [
   TaskInterviewResult,
   TaskResult,
   User,
+  StudentFeedback,
 ];
 
 export {
@@ -41,6 +43,7 @@ export {
   TaskInterviewResult,
   TaskResult,
   User,
+  StudentFeedback,
 };
 
 export interface IApiResponse<T> {
