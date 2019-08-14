906d3f3540040382a1d23157574fb8fbb43922c0
Andrei Palchys <andrei_palchys@epam.com>
2019-08-14 23:50:57 +0300

    feat: add single prettier

--- api/commitlint.config.js
+++ api/commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [2, 'always', ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'style', 'test']],
-    },
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["build", "ci", "docs", "feat", "fix", "perf", "refactor", "style", "test"]],
+  },
 };
--- lambdas/github-task-checker/handler.ts
+++ lambdas/github-task-checker/handler.ts
@@ -4,9 +4,13 @@ import 'source-map-support/register';
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2,
+    ),
   };
-}
+};
