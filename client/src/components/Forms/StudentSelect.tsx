6e045bc95ff6ec81f1046065b530477e277e7f99
Andrei Palchys <andrei_palchys@epam.com>
2019-08-20 10:28:09 +0300

    fix: enable student search

--- client/src/components/Forms/StudentSelect.tsx
+++ client/src/components/Forms/StudentSelect.tsx
@@ -22,12 +22,18 @@ export function StudentSelect(props: Props) {
           <Label>Student</Label>
           <Select
             {...input}
+            isSearchable
             onChange={(student: any) => input.onChange(student.id)}
             value={props.data.find(student => student.id === input.value)}
             placeholder={'Choose student'}
             getOptionValue={(person: Person) => person.id.toString()}
             components={{ Option, SingleValue }}
             options={props.data}
+            filterOption={(option: { data: Person }, searchText: string) =>
+              option.data.githubId.startsWith(searchText) ||
+              option.data.firstName.startsWith(searchText) ||
+              option.data.lastName.startsWith(searchText)
+            }
           />
           <ValidationError meta={meta} />
         </>
