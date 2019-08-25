d0be18814c38cd04f0af0404b27464c1b325861f
Andrei Palchys <andrei_palchys@epam.com>
2019-08-25 22:36:54 +0300

    fix: update certificate course name

--- server/src/routes/course/certificates.ts
+++ server/src/routes/course/certificates.ts
@@ -9,21 +9,45 @@ import { config } from '../../config';
 
 export const postCertificates = (_: ILogger) => async (ctx: Router.RouterContext) => {
   const courseId = ctx.params.courseId;
-  const students = await getRepository(Student)
+  const inputIds = ctx.request.body as { studentId: number }[];
+  let students: Student[] = [];
+  const initialQuery = getRepository(Student)
     .createQueryBuilder('student')
     .innerJoin('student.course', 'course')
     .innerJoin('student.user', 'user')
-    .addSelect(['user.id', 'user.firstName', 'user.lastName', 'user.githubId', 'course.name'])
-    .where('student."courseId" = :courseId AND student."isExpelled" = false AND student."isFailed" = false', {
-      courseId,
-    })
-    .getMany();
-  const result = students.map(student => ({
-    studentId: student.id,
-    course: student.course!.name,
-    name: `${student.user!.firstName} ${student.user!.lastName}`,
-    date: Date.now(),
-  }));
+    .addSelect([
+      'user.id',
+      'user.firstName',
+      'user.lastName',
+      'user.githubId',
+      'course.name',
+      'course.primarySkillName',
+    ]);
+  if (Array.isArray(inputIds) && inputIds.length > 0) {
+    students = await initialQuery
+      .where('student."id" IN (:...ids)', {
+        ids: inputIds,
+      })
+      .getMany();
+  } else {
+    students = [];
+    students = await initialQuery
+      .where('student."courseId" = :courseId AND student."isExpelled" = false AND student."isFailed" = false', {
+        courseId,
+      })
+      .getMany();
+  }
+
+  const result = students.map(student => {
+    const course = student.course!;
+    const user = student.user!;
+    return {
+      studentId: student.id,
+      course: `${course.name} (${course.primarySkillName})`,
+      name: `${user.firstName} ${user.lastName}`,
+      date: Date.now(),
+    };
+  });
   await axios.post(config.aws.certificateGenerationUrl, result, {
     headers: { 'x-api-key': config.aws.certificateGenerationApiKey },
   });
