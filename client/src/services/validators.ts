acf68b44638607ae5bc8a87cc1d258127bc6617f
Andrei Palchys <andrei_palchys@epam.com>
2019-09-09 18:24:18 +0300

    fix: name validation

--- client/src/services/validators.ts
+++ client/src/services/validators.ts
@@ -1,6 +1,6 @@
 export const emailPattern = /[^@]+@[^\.]+\..+/g;
 export const epamEmailPattern = /[^@]+_[^@]+@epam.com/gi;
 
-export const englishNamePattern = /^(\w|\s)+$/g;
+export const englishNamePattern = /^[\x00-\x7F]+$/g;
 export const phonePattern = /^\+[1-9]{1}[0-9]{3,14}$/g;
 export const urlPattern = /^(http|https):\/\/.+(\.[a-z]{2,10})/g;
