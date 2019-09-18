177cc87e0172d97d6037fbd1906e844c7a8ca27b
Andrei Palchys <andrei_palchys@epam.com>
2019-09-18 10:37:28 +0300

    feat: add GA

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -2,12 +2,17 @@ import * as React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 
 export default class extends Document {
+  enableGA = process.env.NODE_ENV === 'production';
+
   render() {
     return (
       <html lang="en">
         <Head>
           <link rel="shortcut icon" href="https://rs.school/favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+
+          {this.enableGA && <script async src="https://www.googletagmanager.com/gtag/js?id=UA-55428637-3" />}
+          {this.enableGA && <script dangerouslySetInnerHTML={{ __html: gaJsCode }} />}
         </Head>
         <body>
           <Main />
@@ -17,3 +22,11 @@ export default class extends Document {
     );
   }
 }
+
+const gaJsCode = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-55428637-3');
+`;
