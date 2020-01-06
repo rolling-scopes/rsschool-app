9c01873ccbe50709bc42474c6e93d8b05d172aa3
Andrei <andrei@palchys.com>
2020-01-06 23:42:46 +0300

    feat: migration to antd4

--- client/src/pages/epamlearningjs.tsx
+++ client/src/pages/epamlearningjs.tsx
@@ -1,22 +1,20 @@
-import { Form } from '@ant-design/compatible';
-import '@ant-design/compatible/assets/index.css';
-import { Button, Col, message, Row, Table, Select, Typography, Spin } from 'antd';
+import { Button, Col, Form, message, Row, Table, Typography, Spin } from 'antd';
 import { NextPageContext } from 'next';
-import { FormComponentProps } from 'antd/lib/form';
 import { withSession } from 'components';
 import * as React from 'react';
 import { AxiosError } from 'axios';
 import { CourseService, CourseTask } from 'services/course';
 import { UserService } from 'services/user';
 import { CoursePageProps } from 'services/models';
+import { CourseTaskSelect } from 'components/Forms';
 
-type Props = CoursePageProps & FormComponentProps;
-type State = { courseTasks: (CourseTask & { score: number })[]; loading: boolean };
+type Props = CoursePageProps;
+type State = { courseTaskId: number | null; courseTasks: (CourseTask & { score: number })[]; loading: boolean };
 const courseName = 'epamlearningjs';
 const registryUrl = `/registry/epamlearningjs`;
 
 class EpamLearningJs extends React.Component<Props, State> {
-  state: State = { loading: true, courseTasks: [] };
+  state: State = { loading: true, courseTasks: [], courseTaskId: null };
 
   private courseService: CourseService;
 
@@ -68,26 +66,14 @@ class EpamLearningJs extends React.Component<Props, State> {
   }
 
   render() {
-    const { getFieldDecorator: field, getFieldValue } = this.props.form;
-    const courseTaskId = getFieldValue('courseTaskId');
     const { courseTasks, loading } = this.state;
-    const task = courseTasks.find(t => t.id === courseTaskId);
+    const task = courseTasks.find(t => t.id === this.state.courseTaskId);
     return (
       <Spin spinning={loading}>
-        <Form style={{ margin: 32 }} onSubmit={this.handleSubmit} layout="vertical">
+        <Form style={{ margin: 32 }} onFinish={this.handleSubmit} layout="vertical">
           <Row align="bottom" gutter={24}>
             <Col style={{ marginBottom: 32 }} xs={24} sm={12} md={12} lg={10}>
-              <Form.Item label="Task">
-                {field('courseTaskId', { rules: [{ required: true, message: 'Please select a task' }] })(
-                  <Select size="large" placeholder="Select task">
-                    {courseTasks.map(task => (
-                      <Select.Option key={task.id} value={task.id}>
-                        {task.name}
-                      </Select.Option>
-                    ))}
-                  </Select>,
-                )}
-              </Form.Item>
+              <CourseTaskSelect data={courseTasks} onChange={courseTaskId => this.setState({ courseTaskId })} />
               {task && (
                 <>
                   <Typography.Paragraph>
@@ -138,35 +124,32 @@ class EpamLearningJs extends React.Component<Props, State> {
     );
   }
 
-  private handleSubmit = async (e: any) => {
-    e.preventDefault();
-    this.props.form.validateFields(async (err: any, values: any) => {
-      if (err || !this.props.course) {
-        return;
-      }
-      const { courseTaskId } = values;
-      const task = this.state.courseTasks.find(t => t.id === courseTaskId);
-      if (!task) {
-        return;
-      }
-      try {
-        const data = {
-          githubRepoName: task.githubRepoName,
-          sourceGithubRepoUrl: task.sourceGithubRepoUrl,
-        };
-        await this.courseService.postTaskVerification(courseTaskId, data);
-        this.setState({ loading: false });
-        message.success('The task has been submitted for verification and it will be checked soon.');
-        this.props.form.resetFields();
-      } catch (e) {
-        this.setState({ loading: false });
-        message.error('An error occured. Please try later.');
-      }
-    });
+  private handleSubmit = async (values: any) => {
+    if (!this.props.course) {
+      return;
+    }
+
+    const { courseTaskId } = values;
+    const task = this.state.courseTasks.find(t => t.id === courseTaskId);
+    if (!task) {
+      return;
+    }
+    try {
+      const data = {
+        githubRepoName: task.githubRepoName,
+        sourceGithubRepoUrl: task.sourceGithubRepoUrl,
+      };
+      await this.courseService.postTaskVerification(courseTaskId, data);
+      this.setState({ loading: false });
+      message.success('The task has been submitted for verification and it will be checked soon.');
+    } catch (e) {
+      this.setState({ loading: false });
+      message.error('An error occured. Please try later.');
+    }
   };
 }
 
-const Page = withSession(Form.create()(EpamLearningJs));
+const Page = withSession(EpamLearningJs);
 (Page as any).getInitialProps = EpamLearningJs.getInitialProps;
 (Page as any).redirectToRegistry = EpamLearningJs.redirectToRegistry;
 export default Page;
