00ed57eab63ec00eb6817a050fce6425ce721249
Andrei <andrei@palchys.com>
2020-01-21 12:08:10 +0300

    fix(feedback): update Private feedback page

--- client/src/components/PersonSelect.tsx
+++ client/src/components/PersonSelect.tsx
@@ -7,13 +7,14 @@ type Props = {
   [key: string]: any;
   data: { id: number; githubId: string; name?: string }[];
   keyField?: 'id' | 'githubId';
+  defaultValue?: string | number;
 };
 
 export class PersonSelect extends React.PureComponent<Props> {
   render() {
-    const { data, ...other } = this.props;
+    const { data, defaultValue, ...other } = this.props;
     return (
-      <Select showSearch optionFilterProp="children" defaultValue={undefined} placeholder="Select..." {...other}>
+      <Select showSearch optionFilterProp="children" defaultValue={defaultValue} placeholder="Select..." {...other}>
         {data.map(person => (
           <Select.Option key={person.id} value={this.props.keyField ? get(person, this.props.keyField) : person.id}>
             <GithubAvatar size={24} githubId={person.githubId} /> {person.name} ({person.githubId})
