4e46fd98650c4d3585927d929073bfd122b2addd
Dzmitry Tsebruk <mikhama@tut.by>
2020-02-10 23:51:02 +0300

    test: add tests for CommonCard and AboutCard

--- client/src/components/Profile/__test__/AboutCard.test.tsx
+++ client/src/components/Profile/__test__/AboutCard.test.tsx
@@ -31,4 +31,36 @@ describe('AboutCard', () => {
       expect(shallowToJson(output)).toMatchSnapshot();
     });
   });
+  describe('filterPermissions', () => {
+    it('should left only "isAboutVisible" in "permissionsSettings" object', () => {
+      const permissionsSettings = {
+        isProfileVisible: { all: true },
+        isAboutVisible: { all: true, mentor: true, student: true },
+        isEducationVisible: { all: true, mentor: true, student: true },
+        isEnglishVisible: { all: false, student: false },
+        isEmailVisible: { all: true, student: true },
+        isTelegramVisible: { all: false, student: false },
+        isSkypeVisible: { all: true, student: true },
+        isPhoneVisible: { all: false, student: false },
+        isContactsNotesVisible: { all: true, student: true },
+        isLinkedInVisible: { all: false, mentor: false, student: false },
+        isPublicFeedbackVisible: { all: true, mentor: true, student: true },
+        isMentorStatsVisible: { all: true, mentor: true, student: true },
+        isStudentStatsVisible: { all: true, student: true },
+      };
+      const wrapper = shallow(
+        <AboutCard
+          data={{} as GeneralInfo}
+          isEditingModeEnabled={false}
+          permissionsSettings={permissionsSettings}
+          onPermissionsSettingsChange={() => {}}
+          onProfileSettingsChange={() => {}}
+        />);
+      const instance = wrapper.instance();
+      const result = instance.filterPermissions(permissionsSettings);
+      expect(result).toEqual({ isAboutVisible: {
+        all: true, mentor: true, student: true,
+      } });
+    });
+  });
 });
