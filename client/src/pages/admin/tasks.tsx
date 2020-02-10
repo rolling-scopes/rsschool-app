c2f36d26f68f725a56e4eed76b561e8fdc5b5b47
Andrei <andrei@palchys.com>
2020-02-10 09:19:15 +0300

    fix: typo

--- client/src/pages/admin/tasks.tsx
+++ client/src/pages/admin/tasks.tsx
@@ -111,7 +111,7 @@ function Page(props: Props) {
             <Select.Option value="jstask">JS task</Select.Option>
             <Select.Option value="htmltask">HTML task</Select.Option>
             <Select.Option value="htmlcssacademy">HTML/CSS Academy</Select.Option>
-            <Select.Option value="cv:markdown">CV HTML</Select.Option>
+            <Select.Option value="cv:markdown">CV Markdown</Select.Option>
             <Select.Option value="cv:html">CV HTML</Select.Option>
             <Select.Option value="codewars:stage1">Codewars stage 1</Select.Option>
             <Select.Option value="codewars:stage2">Codewars stage 2</Select.Option>
