26803478396d8dc006fccf0358962956fb139597
Andrei <andrei@palchys.com>
2020-02-11 22:26:54 +0300

    chore: remove bundle analyzer

--- client/src/next.config.js
+++ client/src/next.config.js
@@ -1,8 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
 
 const nextConfig = {
   serverRuntimeConfig: {
@@ -22,4 +19,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
