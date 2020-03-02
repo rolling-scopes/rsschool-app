3b5466e4caebb02ffaff39fcee9176ad07d90c6c
Andrei <andrei@palchys.com>
2020-03-02 12:52:50 +0300

    feat(mentor-registry): update csv export

--- server/src/routes/registry/index.ts
+++ server/src/routes/registry/index.ts
@@ -253,6 +253,7 @@ async function getMentorRegistries() {
       'user.githubId',
       'user.primaryEmail',
       'user.locationName',
+      'user.contactsEpamEmail',
     ])
     .leftJoin('user.mentors', 'mentor')
     .addSelect(['mentor.id', 'mentor.courseId'])
@@ -267,7 +268,8 @@ function transformMentorRegistry(mentorRegistry: MentorRegistry) {
     comment: mentorRegistry.comment,
     englishMentoring: mentorRegistry.englishMentoring,
     githubId: user.githubId,
-    email: user.primaryEmail,
+    primaryEmail: user.primaryEmail,
+    contactsEpamEmail: user.contactsEpamEmail,
     locationName: user.locationName,
     maxStudentsLimit: mentorRegistry.maxStudentsLimit,
     name: `${user.firstName} ${user.lastName}`,
