47b9600a91ae43afa6cfdc5c10f36a6f2bf0a59b
AlreadyBored <shilaums@gmail.com>
2020-09-02 22:21:04 +0300

    feat: add basic elements & state management

--- client/src/components/ComplexConfirmation.tsx
+++ client/src/components/ComplexConfirmation.tsx
@@ -1,15 +1,55 @@
-import { Modal, Button, Input } from 'antd';
+import { Modal, Input, Typography } from 'antd';
 import { ExclamationCircleOutlined } from '@ant-design/icons';
 import { useState } from 'react';
+import random from 'lodash/random';
 
 type AnyFunc = (...params: any) => any;
 
+const { Text } = Typography;
+
 type Props = {
-  onOk: AnyFunc,
-  keyLength: number,
-  message: string
+  onOk: AnyFunc;
+  keyLength: number;
+  message: string;
 };
 
 export function ComplexConfirmation(props: Props) {
+  const { onOk, keyLength, message } = props;
+  const [isVisible, setVisibility] = useState(true);
+  const [isKeyMatch, setKeyMatch] = useState(false);
+  let key = '';
 
+  for (let i = 0; i < keyLength; i++) key += random(0, 9);
+
+  const checkKeyMatch = (e: any) => {
+    if (e.target.value === key) {
+      setKeyMatch(true);
+    } else {
+      setKeyMatch(false);
+    }
+  };
+
+  return (
+    <Modal
+      title="Confirm action"
+      onOk={onOk}
+      visible={isVisible}
+      onCancel={() => {
+        setVisibility(false);
+      }}
+      okButtonProps={{ disabled: !isKeyMatch }}
+    >
+      <p>
+        <ExclamationCircleOutlined />
+        Are you sure?
+      </p>
+      <br />
+      <p>{message}</p>
+      <br />
+      <p>Enter following number to confirm action:</p>
+      <br />
+      <Text strong>{key}</Text>
+      <Input placeholder="Enter" type="text" onChange={checkKeyMatch} />
+    </Modal>
+  );
 }
