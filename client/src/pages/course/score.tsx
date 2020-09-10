f45906dac858830f39806ed8ee146e8b6433107b
Andrei <andrei@palchys.com>
2020-09-10 21:01:48 +0300

    fix: update score once a day (#522)

--- client/src/pages/course/score.tsx
+++ client/src/pages/course/score.tsx
@@ -91,7 +91,7 @@ export function Page(props: CoursePageProps) {
               <span style={{ display: 'inline-block', lineHeight: '24px' }}>Active Students Only</span>{' '}
               <Switch checked={activeOnly} onChange={handleActiveOnlyChange} />
             </div>
-            <Text mark>Score is refreshed every 5 minutes</Text>
+            <Text mark>Total score and position is updated every day at 00:00 GMT+3</Text>
             {renderCsvExportButton(props)}
           </Row>
           {renderTable(loaded, students.content, columns, students.pagination, getCourseScore)}
