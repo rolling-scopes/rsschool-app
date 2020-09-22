433b140c7472abc619aee13f2721f16785f9cf1b
Andrei <andrei@palchys.com>
2020-09-22 10:25:10 +0300

    fix: temporary update score once in 10 mins

--- server/src/schedule.ts
+++ server/src/schedule.ts
@@ -4,7 +4,7 @@ import { getStudentsScore, getCourseTasks, updateScoreStudents, getCourses } fro
 import { round, mapValues, keyBy, sum } from 'lodash';
 
 export function startBackgroundJobs(logger: ILogger) {
-  scheduleJob('0 21 * * *', async () => {
+  scheduleJob('*/10 * * * *', async () => {
     logger.info('Starting score update job');
 
     const courses = await getCourses();
