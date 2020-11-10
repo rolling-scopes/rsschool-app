aa742c40154f75d915df02f927d751f5d8a30c66
AlreadyBored <shilaums@gmail.com>
2020-11-10 20:04:17 +0300

    feat: add type annotation for education record

--- common/models/cv.ts
+++ common/models/cv.ts
@@ -3,3 +3,10 @@ export interface Contact {
   contactText: string;
   transformable: boolean;
 }
+
+export interface EducationRecord {
+  organization: string;
+  education: string;
+  startYear: number;
+  finishYear: number;
+}
