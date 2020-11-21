f2a76461d3af98831e86525e287f78b6bc424b8d
AlreadyBored <shilaums@gmail.com>
2020-11-21 21:28:51 +0300

    refactor: place forms into separate components

--- client/src/components/cv/FormCV.tsx
+++ client/src/components/cv/FormCV.tsx
@@ -1,23 +1,11 @@
 import * as React from 'react';
-import { Form, Input, Select, DatePicker, Space, Divider } from 'antd';
-import { Contact, EducationRecord, EmploymentRecord } from '../../../../common/models/cv';
-import moment from 'moment';
-
-const { Item } = Form;
-const { Option } = Select;
-const { RangePicker } = DatePicker;
+import { ContactsForm, EducationHistoryForm, EmploymentHistoryForm, UserDataForm } from './forms';
+import { Contact, EducationRecord, EmploymentRecord, UserData } from '../../../../common/models/cv';
 
 type Props = {
   cvData: {
     contactsList: Contact[],
-    userData: {
-      name: string,
-      desiredPosition: string,
-      selfIntroLink: string,
-      englishLevel: any,
-      militaryService: any
-    },
-    notes: string,
+    userData: UserData,
     educationHistory: EducationRecord[],
     employmentHistory: EmploymentRecord[]
   }
@@ -27,14 +15,7 @@ function FormCV(props: Props) {
   const {
     cvData: {
       contactsList,
-      userData: {
-        name,
-        desiredPosition,
-        selfIntroLink,
-        englishLevel,
-        militaryService
-      },
-      notes,
+      userData,
       educationHistory,
       employmentHistory
     }
@@ -42,95 +23,10 @@ function FormCV(props: Props) {
 
   return (
     <>
-      <Form name='userData'>
-        <Item label='Name' name='name' initialValue={name}>
-          <Input />
-        </Item>
-        <Item label='Desired position' name='desiredPosition' initialValue={desiredPosition}>
-          <Input />
-        </Item>
-        <Item label='Self introduction video' name='selfIntroLink' initialValue={selfIntroLink}>
-          <Input />
-        </Item>
-        <Item label='Select your English level' name='englishLevel' initialValue={englishLevel}>
-          <Select>
-            <Option value='a0'>A0</Option>
-            <Option value='a1'>A1</Option>
-            <Option value='a1+'>A1+</Option>
-            <Option value='a2'>A2</Option>
-            <Option value='a2+'>A2+</Option>
-            <Option value='b1'>B1</Option>
-            <Option value='b1+'>B1+</Option>
-            <Option value='b2'>B2</Option>
-            <Option value='b2+'>B2+</Option>
-            <Option value='c1'>C1</Option>
-            <Option value='c1+'>C1+</Option>
-            <Option value='c2'>C2</Option>
-          </Select>
-        </Item>
-        <Item label='Military service' name='militaryService' initialValue={militaryService}>
-          <Select>
-            <Option value='served'>Served</Option>
-            <Option value='liable'>Liable</Option>
-            <Option value='notLiable'>Not liable</Option>
-          </Select>
-        </Item>
-        <Item label='About me' name='notes' initialValue={notes}>
-          <Input.TextArea rows={4} />
-        </Item>
-      </Form>
-      <Form name='contacts'>
-        {contactsList.map(contact => {
-          const { contactType, contactText } = contact;
-          return (
-            <Item initialValue={contactText} label={contactType} name={`contacts-${contactType}`} key={contactType}>
-              <Input />
-            </Item>
-          )
-        })}
-      </Form>
-      <Form name='educationHistory'>
-        <Space direction='vertical' style={{width: '100%'}}>
-          {educationHistory.map(((educationRecord, index) => {
-            const { organization, education, startYear, finishYear } = educationRecord;
-            const yearFormat = 'YYYY';
-            return (
-              <React.Fragment key={`edu_fg_${index}`}>
-                <Item initialValue={organization} label='Organization' name={`education-${index}-${organization}`}>
-                  <Input />
-                </Item>
-                <Item initialValue={education} label='Education' name={`education-${index}-${education}`}>
-                  <Input />
-                </Item>
-                <Item initialValue={[moment(String(startYear), yearFormat), moment(String(finishYear), yearFormat)]} label='Education years' name={`educationYears-${index}`}>
-                  <RangePicker picker='year' />
-                </Item>
-                <Divider />
-              </React.Fragment>
-            );
-          }))}
-        </Space>
-      </Form>
-      <Form name='employmentHistory'>
-        {employmentHistory.map(((employmentRecord, index) => {
-          const { organization, position, startYear, finishYear } = employmentRecord;
-          const yearFormat = 'YYYY';
-          return (
-            <React.Fragment key={`empl_fg_${index}`}>
-              <Item initialValue={organization} label='Organization' name={`employment-${index}-${organization}`} >
-                <Input />
-              </Item>
-              <Item initialValue={position} label='Position' name={`employment-${index}-${position}`}>
-                <Input />
-              </Item>
-              <Item initialValue={[moment(String(startYear), yearFormat), moment(String(finishYear), yearFormat)]} label='Work years' name={`employmentYears-${index}`}>
-                <RangePicker picker='year' />
-              </Item>
-              <Divider />
-            </React.Fragment>
-          );
-        }))}
-      </Form>
+      <UserDataForm userData={userData} />
+      <ContactsForm contactsList={contactsList} />
+      <EducationHistoryForm educationHistory={educationHistory} />
+      <EmploymentHistoryForm employmentHistory={employmentHistory} />
     </>
   );
 
