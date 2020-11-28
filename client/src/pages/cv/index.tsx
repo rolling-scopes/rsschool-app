c47a691f0b1f802191639bd3fc5eb1d7fda8f26c
AlreadyBored <shilaums@gmail.com>
2020-11-28 15:06:57 +0300

    refactor: remove unnecessary code & separate functionality between 2 views of CV

--- client/src/pages/cv/index.tsx
+++ client/src/pages/cv/index.tsx
@@ -1,18 +1,9 @@
 import * as React from 'react';
-import { Layout, Space, Switch } from 'antd';
+import { Layout, Switch } from 'antd';
 import { NextRouter, withRouter } from 'next/router';
 import withSession, { Session } from 'components/withSession';
-import {
-  MainSection,
-  AboutSection,
-  EducationSection,
-  EmploymentSection,
-  CoursesSection,
-  BadgesSection
-} from 'components/cv/sections';
 import FormCV from 'components/cv/FormCV';
-import { mockContactsList, mockUserData, notes, educationHistory, employmentHistory, coursesData, badgesData } from './mockData';
-import {EnglishLevel, MilitaryService} from '../../../../common/models/cv';
+import ViewCV from 'components/cv/ViewCV';
 
 const { Content } = Layout;
 
@@ -32,53 +23,6 @@ class CVPage extends React.Component<Props, State> {
     editMode: true
   };
 
-  private extractCoursesData(coursesData: any) {
-    const coursesRaw = coursesData;
-    return coursesRaw.map((course: any) => {
-      const {
-        certificateId,
-        courseFullName,
-        isExpelled,
-        locationName,
-        position,
-        isCourseCompleted,
-        totalScore
-      } = course;
-
-      return {
-        certificateId,
-        courseFullName,
-        isExpelled,
-        locationName,
-        position,
-        isCourseCompleted,
-        totalScore
-      }
-    });
-  }
-
-  private extractBadgesData(badges: any) {
-    const uniqueBadgesSummarized = badges.reduce((uniqueBadges: any, badge: any) => {
-      const { badgeId } = badge;
-      if (uniqueBadges[badgeId]) {
-        uniqueBadges[badgeId]++;
-      } else {
-        uniqueBadges[badgeId] = 1;
-      }
-      return uniqueBadges;
-    }, {} as any);
-
-    const badgesFormatted = Object.entries(uniqueBadgesSummarized).map(entry => {
-      const [badgeId, badgeCount] = entry;
-      return `${badgeId}: ${badgeCount}`;
-    });
-
-    return {
-      badges: badgesFormatted,
-      total: badges.length
-    };
-  }
-
   private switchView(checked: boolean) {
     if (checked) {
       this.setState({
@@ -92,63 +36,24 @@ class CVPage extends React.Component<Props, State> {
   }
 
   render() {
-    const {name, desiredPosition, selfIntroLink, englishLevel, militaryService} = mockUserData;
     const { editMode } = this.state;
-
-    const cvData = {
-      userData: {
-        name,
-        desiredPosition,
-        selfIntroLink,
-        englishLevel: englishLevel as EnglishLevel,
-        militaryService: militaryService as MilitaryService,
-        notes
-      },
-      educationHistory,
-      employmentHistory,
-      contactsList: mockContactsList,
-    };
 
     return (
       <>
         <Layout style={{ paddingTop: '30px', margin: 'auto', maxWidth: '960px' }}>
-            <Content>
-              <label>
-                Switch view:
+          <Content>
+            <label>
+              Switch view:
                 <br />
-                <Switch defaultChecked={editMode} onChange={this.switchView.bind(this)} checkedChildren='CV view' unCheckedChildren='Form view'/>
-              </label>
-              {editMode ? (
-                <Space direction="vertical" style={{ width: '100%' }}>
-                  <MainSection
-                    contacts={mockContactsList}
-                    name={name}
-                    desiredPosition={desiredPosition}
-                    selfIntroLink={selfIntroLink}
-                    englishLevel={englishLevel as EnglishLevel}
-                    militaryService={militaryService as MilitaryService}
-                  />
-                  <AboutSection
-                    notes={notes}
-                  />
-                  <EducationSection
-                    educationHistory={educationHistory}
-                  />
-                  <EmploymentSection
-                    employmentHistory={employmentHistory}
-                  />
-                  <CoursesSection
-                    coursesData={this.extractCoursesData(coursesData)}
-                  />
-                  <BadgesSection
-                    badgesData={this.extractBadgesData(badgesData)}
-                  />
-                </Space>
-              ) : (
-                <FormCV cvData={cvData} />
+              <Switch defaultChecked={editMode} onChange={this.switchView.bind(this)} checkedChildren='CV view' unCheckedChildren='Form view' />
+            </label>
+            {editMode ? (
+              <ViewCV />
+            ) : (
+                <FormCV />
               )}
-            </Content>
-          </Layout>
+          </Content>
+        </Layout>
       </>
     );
   }
