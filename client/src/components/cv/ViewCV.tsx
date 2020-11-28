3736b6d2cae6908d788c62c0add639c2a45c92eb
AlreadyBored <shilaums@gmail.com>
2020-11-28 14:44:56 +0300

    feat: udpate components hierarchy & refactor some of them

---
+++ client/src/components/cv/ViewCV.tsx
@@ -0,0 +1,151 @@
+import * as React from 'react';
+import { Layout, Space } from 'antd';
+import { LoadingScreen } from 'components/LoadingScreen';
+import {
+  MainSection,
+  AboutSection,
+  EducationSection,
+  EmploymentSection,
+  CoursesSection,
+  BadgesSection
+} from 'components/cv/sections';
+import { mockContactsList, mockUserData, notes, educationHistory, employmentHistory, coursesData, badgesData } from 'pages/cv/mockData';
+import { EnglishLevel, MilitaryService, Contact, UserData, EducationRecord, EmploymentRecord, CourseData, BadgesData } from '../../../../common/models/cv';
+
+const { Content } = Layout;
+
+type State = {
+  isLoading: boolean;
+  contactsList: Contact[] | null;
+  userData: UserData | null;
+  educationHistory: EducationRecord[] | null;
+  employmentHistory: EmploymentRecord[] | null;
+  coursesData: CourseData[] | null;
+  badgesData: BadgesData | null;
+};
+
+class ViewCV extends React.Component<State> {
+  state: State = {
+    isLoading: false,
+    contactsList: null,
+    userData: null,
+    educationHistory: null,
+    employmentHistory: null,
+    coursesData: null,
+    badgesData: null
+  };
+
+  private async fetchData() {
+    await this.setState({
+      isLoading: true
+    });
+
+    const badgesDataExtracted = this.extractBadgesData(badgesData);
+    const coursesDataExtracted = this.extractCoursesData(coursesData);
+
+    await this.setState({
+      contactsList: mockContactsList,
+      userData: mockUserData,
+      educationHistory: educationHistory,
+      employmentHistory: employmentHistory,
+      coursesData: coursesDataExtracted,
+      badgesData: badgesDataExtracted
+    });
+
+    await this.setState({
+      isLoading: false
+    });
+  }
+
+  private extractCoursesData(coursesData: any) {
+    const coursesRaw = coursesData;
+    return coursesRaw.map((course: any) => {
+      const {
+        certificateId,
+        courseFullName,
+        isExpelled,
+        locationName,
+        position,
+        isCourseCompleted,
+        totalScore
+      } = course;
+
+      return {
+        certificateId,
+        courseFullName,
+        isExpelled,
+        locationName,
+        position,
+        isCourseCompleted,
+        totalScore
+      }
+    });
+  }
+
+  private extractBadgesData(badges: any) {
+    const uniqueBadgesSummarized = badges.reduce((uniqueBadges: any, badge: any) => {
+      const { badgeId } = badge;
+      if (uniqueBadges[badgeId]) {
+        uniqueBadges[badgeId]++;
+      } else {
+        uniqueBadges[badgeId] = 1;
+      }
+      return uniqueBadges;
+    }, {} as any);
+
+    const badgesFormatted = Object.entries(uniqueBadgesSummarized).map(entry => {
+      const [badgeId, badgeCount] = entry;
+      return `${badgeId}: ${badgeCount}`;
+    });
+
+    return {
+      badges: badgesFormatted,
+      total: badges.length
+    };
+  }
+
+  async componentDidMount() {
+    await this.fetchData();
+  }
+
+  render() {
+    const { name, desiredPosition, selfIntroLink, englishLevel, militaryService } = mockUserData;
+    const { isLoading } = this.state;
+
+    return (
+      <LoadingScreen show={isLoading}>
+        <Layout style={{ paddingTop: '30px', margin: 'auto', maxWidth: '960px' }}>
+          <Content>
+            <Space direction="vertical" style={{ width: '100%' }}>
+              <MainSection
+                contacts={mockContactsList}
+                name={name}
+                desiredPosition={desiredPosition}
+                selfIntroLink={selfIntroLink}
+                englishLevel={englishLevel as EnglishLevel}
+                militaryService={militaryService as MilitaryService}
+              />
+              <AboutSection
+                notes={notes}
+              />
+              <EducationSection
+                educationHistory={educationHistory}
+              />
+              <EmploymentSection
+                employmentHistory={employmentHistory}
+              />
+              <CoursesSection
+                coursesData={this.extractCoursesData(coursesData)}
+              />
+              <BadgesSection
+                badgesData={this.extractBadgesData(badgesData)}
+              />
+            </Space>
+          </Content>
+        </Layout>
+      </LoadingScreen>
+    );
+  }
+}
+
+export default ViewCV;
--- client/src/components/cv/forms/EducationHistoryForm.tsx
+++ client/src/components/cv/forms/EducationHistoryForm.tsx
@@ -1,41 +1,100 @@
 import * as React from 'react';
 import moment from 'moment';
-import { Form, Input, Space, Divider, DatePicker } from 'antd';
+import { Form, Input, Space, Divider, DatePicker, Button, message } from 'antd';
 import { EducationRecord } from '../../../../../common/models/cv';
 
 const { Item } = Form;
 const { RangePicker } = DatePicker;
 
 type Props = {
-  educationHistory: EducationRecord[]
+  educationHistory: EducationRecord[];
+  handleFunc: (data: any) => void;
 };
 
 export default function UserDataForm(props: Props) {
 
-  const {educationHistory} = props;
+  const { educationHistory, handleFunc } = props;
+
+  const [ownEducationHistory, setOwnEducationHistory] = React.useState([...educationHistory]);
+
+  const addEmptyRecord = () => {
+    const newRecord = ({
+      organization: 'Organization',
+      education: 'Education',
+      startYear: 1970,
+      finishYear: 1970
+    });
+
+    setOwnEducationHistory([
+      ...ownEducationHistory,
+      newRecord
+    ]);
+  };
+
+  const transformData = (data: {[key: string]: string | object[]}) => {
+    const acc: {
+        [key: string]: {
+            [key: string]: string | object[]
+        }
+    } = {};
+    Object.entries(data).forEach(([key, value]) => {
+      const [, index, type] = key.split('-');
+      if (!acc[index]) {
+          acc[index] = {};
+      }
+
+      acc[index][type] = value;
+
+    });
+    const valuesArr = Object.values(acc);
+    handleFunc(valuesArr);
+  };
+
+  const submitData = (data: {[key: string]: string}) => {
+    const transformedData = transformData(data);
+    handleFunc(transformedData);
+  };
+
+  const removeRecord = (index: number): void => {
+    if (index === 0 && ownEducationHistory.length === 1) {
+      message.warn('You can\'t remove last record');
+      return;
+    }
+    const newOwnEducationHistory = [...ownEducationHistory];
+
+    newOwnEducationHistory.splice(index, 1);
+
+    setOwnEducationHistory(newOwnEducationHistory);
+  };
+
 
   return (
-    <Form name='educationHistory'>
-      <Space direction='vertical' style={{width: '100%'}}>
-        {educationHistory.map(((educationRecord, index) => {
+    <Form name='educationHistory' onFinish={submitData}>
+      <Space direction='vertical' style={{ width: '100%' }}>
+        {ownEducationHistory.map(((educationRecord, index) => {
           const { organization, education, startYear, finishYear } = educationRecord;
           const yearFormat = 'YYYY';
           return (
             <React.Fragment key={`edu_fg_${index}`}>
-              <Item initialValue={organization} label='Organization' name={`education-${index}-${organization}`}>
+              <Item initialValue={organization} label='Organization' name={`education-${index}-organization`}>
                 <Input />
               </Item>
-              <Item initialValue={education} label='Education' name={`education-${index}-${education}`}>
+              <Item initialValue={education} label='Education' name={`education-${index}-education`}>
                 <Input />
               </Item>
-              <Item initialValue={[moment(String(startYear), yearFormat), moment(String(finishYear), yearFormat)]} label='Education years' name={`educationYears-${index}`}>
+              <Item initialValue={[moment(String(startYear), yearFormat), moment(String(finishYear), yearFormat)]} label='Education years' name={`education-${index}-years`}>
                 <RangePicker picker='year' />
               </Item>
+              <Button type='dashed' htmlType='button' onClick={() => removeRecord(index)}>Remove</Button>
               <Divider />
             </React.Fragment>
           );
         }))}
       </Space>
+      <Item>
+        <Button type='primary' htmlType='button' onClick={addEmptyRecord}>Add new</Button>
+        <Button type='primary' htmlType='submit'>Save</Button>
+      </Item>
     </Form>
   );
 }
