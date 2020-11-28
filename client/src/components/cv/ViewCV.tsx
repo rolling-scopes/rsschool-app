d717ee034e9b1cdcc1d0d27d6d9ada600e189bab
AlreadyBored <shilaums@gmail.com>
2020-11-28 14:44:56 +0300

    feat: udpate components hierarchy & refactor some of them

--- client/src/components/cv/ViewCV.tsx
+++ client/src/components/cv/ViewCV.tsx
@@ -1,156 +1,151 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import * as React from 'react';
 import { Layout, Space } from 'antd';
 import { LoadingScreen } from 'components/LoadingScreen';
-import { MainSection, AboutSection, CoursesSection, FeedbackSection } from 'components/cv/sections';
-import { Contacts, UserData, CourseData, GetCVData } from '../../../../common/models/cv';
-import { PublicFeedback } from '../../../../common/models/profile';
-import { CVService } from '../../services/cv';
+import {
+  MainSection,
+  AboutSection,
+  EducationSection,
+  EmploymentSection,
+  CoursesSection,
+  BadgesSection
+} from 'components/cv/sections';
+import { mockContactsList, mockUserData, notes, educationHistory, employmentHistory, coursesData, badgesData } from 'pages/cv/mockData';
+import { EnglishLevel, MilitaryService, Contact, UserData, EducationRecord, EmploymentRecord, CourseData, BadgesData } from '../../../../common/models/cv';
 
 const { Content } = Layout;
 
-type Props = {
-  ownerId: string;
-};
-
 type State = {
   isLoading: boolean;
-  contactsList: Contacts | null;
+  contactsList: Contact[] | null;
   userData: UserData | null;
+  educationHistory: EducationRecord[] | null;
+  employmentHistory: EmploymentRecord[] | null;
   coursesData: CourseData[] | null;
-  publicFeedback: PublicFeedback[] | null;
-  expires: number | null;
+  badgesData: BadgesData | null;
 };
 
-const cvService = new CVService();
-
-const extractCoursesData = (coursesData: any) => {
-  return coursesData.map((course: any) => {
-    const {
-      certificateId,
-      courseFullName,
-      isExpelled,
-      locationName,
-      position,
-      isCourseCompleted,
-      totalScore,
-      mentor,
-    } = course;
-
-    return {
-      certificateId,
-      courseFullName,
-      isExpelled,
-      locationName,
-      position,
-      isCourseCompleted,
-      totalScore,
-      mentor,
-    };
-  });
-};
-
-function ViewCV(props: Props) {
-  const [state, setState] = useState<State>({
+class ViewCV extends React.Component<State> {
+  state: State = {
     isLoading: false,
     contactsList: null,
     userData: null,
+    educationHistory: null,
+    employmentHistory: null,
     coursesData: null,
-    publicFeedback: null,
-    expires: null,
-  });
+    badgesData: null
+  };
 
-  const fetchData = useCallback(async () => {
-    const { ownerId } = props;
-
-    await setState({
-      ...state,
-      isLoading: true,
+  private async fetchData() {
+    await this.setState({
+      isLoading: true
     });
 
-    const cvData: GetCVData = await cvService.getCVData(ownerId);
+    const badgesDataExtracted = this.extractBadgesData(badgesData);
+    const coursesDataExtracted = this.extractCoursesData(coursesData);
 
-    const {
-      notes,
-      name,
-      selfIntroLink,
-      startFrom,
-      militaryService,
-      avatarLink,
-      desiredPosition,
-      englishLevel,
-      email,
-      github,
-      linkedin,
-      location,
-      phone,
-      skype,
-      telegram,
-      website,
-      fullTime,
-      publicFeedback,
-      courses,
-      expires: expiresRaw,
-    } = cvData;
-
-    const userData = {
-      notes,
-      name,
-      selfIntroLink,
-      militaryService,
-      avatarLink,
-      desiredPosition,
-      englishLevel,
-      startFrom,
-      fullTime,
-    };
-
-    const contactsList = {
-      email,
-      github,
-      linkedin,
-      location,
-      phone,
-      skype,
-      telegram,
-      website,
-    };
-
-    const coursesDataExtracted = extractCoursesData(courses);
-
-    await setState({
-      contactsList,
-      userData,
+    await this.setState({
+      contactsList: mockContactsList,
+      userData: mockUserData,
+      educationHistory: educationHistory,
+      employmentHistory: employmentHistory,
       coursesData: coursesDataExtracted,
-      publicFeedback,
-      expires: Number(expiresRaw),
-      isLoading: false,
+      badgesData: badgesDataExtracted
     });
-  }, []);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+    await this.setState({
+      isLoading: false
+    });
+  }
 
-  const { isLoading, userData, contactsList, coursesData, publicFeedback, expires } = state;
+  private extractCoursesData(coursesData: any) {
+    const coursesRaw = coursesData;
+    return coursesRaw.map((course: any) => {
+      const {
+        certificateId,
+        courseFullName,
+        isExpelled,
+        locationName,
+        position,
+        isCourseCompleted,
+        totalScore
+      } = course;
 
-  return (
-    <LoadingScreen show={isLoading}>
-      <Layout className="view-cv-layout" style={{ marginBottom: '15px', width: '960px', backgroundColor: '#FFF' }}>
-        <Content>
-          <Space direction="vertical" style={{ width: '100%', backgroundColor: '#FFF' }}>
-            {userData && contactsList && (
-              <>
-                <MainSection userData={userData} contacts={contactsList} expires={expires} />
-                {userData.notes && <AboutSection notes={userData.notes} />}
-              </>
-            )}
-            {coursesData?.length ? <CoursesSection coursesData={coursesData} /> : ''}
-            {publicFeedback && <FeedbackSection feedback={publicFeedback} />}
-          </Space>
-        </Content>
-      </Layout>
-    </LoadingScreen>
-  );
+      return {
+        certificateId,
+        courseFullName,
+        isExpelled,
+        locationName,
+        position,
+        isCourseCompleted,
+        totalScore
+      }
+    });
+  }
+
+  private extractBadgesData(badges: any) {
+    const uniqueBadgesSummarized = badges.reduce((uniqueBadges: any, badge: any) => {
+      const { badgeId } = badge;
+      if (uniqueBadges[badgeId]) {
+        uniqueBadges[badgeId]++;
+      } else {
+        uniqueBadges[badgeId] = 1;
+      }
+      return uniqueBadges;
+    }, {} as any);
+
+    const badgesFormatted = Object.entries(uniqueBadgesSummarized).map(entry => {
+      const [badgeId, badgeCount] = entry;
+      return `${badgeId}: ${badgeCount}`;
+    });
+
+    return {
+      badges: badgesFormatted,
+      total: badges.length
+    };
+  }
+
+  async componentDidMount() {
+    await this.fetchData();
+  }
+
+  render() {
+    const { name, desiredPosition, selfIntroLink, englishLevel, militaryService } = mockUserData;
+    const { isLoading } = this.state;
+
+    return (
+      <LoadingScreen show={isLoading}>
+        <Layout style={{ paddingTop: '30px', margin: 'auto', maxWidth: '960px' }}>
+          <Content>
+            <Space direction="vertical" style={{ width: '100%' }}>
+              <MainSection
+                contacts={mockContactsList}
+                name={name}
+                desiredPosition={desiredPosition}
+                selfIntroLink={selfIntroLink}
+                englishLevel={englishLevel as EnglishLevel}
+                militaryService={militaryService as MilitaryService}
+              />
+              <AboutSection
+                notes={notes}
+              />
+              <EducationSection
+                educationHistory={educationHistory}
+              />
+              <EmploymentSection
+                employmentHistory={employmentHistory}
+              />
+              <CoursesSection
+                coursesData={this.extractCoursesData(coursesData)}
+              />
+              <BadgesSection
+                badgesData={this.extractBadgesData(badgesData)}
+              />
+            </Space>
+          </Content>
+        </Layout>
+      </LoadingScreen>
+    );
+  }
 }
 
 export default ViewCV;
