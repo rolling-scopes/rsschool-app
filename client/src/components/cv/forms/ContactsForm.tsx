5cb9961bb95e81a9a3b161e73da0408ed1d3e548
AlreadyBored <shilaums@gmail.com>
2020-12-16 23:10:37 +0300

    feat: add basic validation to ContactsForm & fix reset btn type in UserDataForm

--- client/src/components/cv/forms/ContactsForm.tsx
+++ client/src/components/cv/forms/ContactsForm.tsx
@@ -20,33 +20,36 @@ export default function ContactsForm(props: Props) {
 
   return (
     <Form form={form} name="contacts" onFinish={handleFunc}>
-      <Item label="Phone" name="phone">
+      <Item label="Phone" name="phone" rules={[{max: 15, whitespace: false}]}>
         <Input />
       </Item>
-      <Item label="Email" name="email">
+      <Item label="Email" name="email" rules={[{max: 50, whitespace: false}]}>
         <Input />
       </Item>
-      <Item label="Skype" name="skype">
+      <Item label="Skype" name="skype" rules={[{max: 30, whitespace: false}]}>
         <Input />
       </Item>
-      <Item label="Telegram" name="telegram">
+      <Item label="Telegram" name="telegram" rules={[{max: 30, whitespace: false}]}>
         <Input />
       </Item>
-      <Item label="LinkedIn" name="linkedin">
+      <Item label="LinkedIn" name="linkedin" rules={[{max: 30, whitespace: false}]}>
         <Input />
       </Item>
-      <Item label="Location" name="location">
+      <Item label="Location" name="location" rules={[{max: 100, whitespace: false}]}>
         <Input />
       </Item>
-      <Item label="Github" name="github">
+      <Item label="Github" name="github" rules={[{max: 30, whitespace: false}]}>
         <Input />
       </Item>
-      <Item label="Website" name="website">
+      <Item label="Website" name="website" rules={[{max: 100, whitespace: false}]}>
         <Input />
       </Item>
       <Item>
         <Button type="primary" htmlType="submit">
           Save
+        </Button>
+        <Button type="primary" htmlType="submit" onClick={() => form.resetFields()}>
+          Reset
         </Button>
       </Item>
     </Form>
