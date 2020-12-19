18673886445a773c0b0ce2f36ae5beece453db82
AlreadyBored <shilaums@gmail.com>
2020-12-19 19:17:35 +0300

    feat: add basic validation to EmploymentHistory form & minor fixes

--- client/src/components/cv/forms/ContactsForm.tsx
+++ client/src/components/cv/forms/ContactsForm.tsx
@@ -20,34 +20,36 @@ export default function ContactsForm(props: Props) {
 
   return (
     <Form form={form} name="contacts" onFinish={handleFunc}>
-      <Item label="Phone" name="phone" rules={[{max: 15, whitespace: false}]}>
+      <Item label="Phone" name="phone" rules={[{ max: 15, whitespace: false }]}>
         <Input />
       </Item>
-      <Item label="Email" name="email" rules={[{max: 50, whitespace: false}]}>
+      <Item label="Email" name="email" rules={[{ max: 50, whitespace: false }]}>
         <Input />
       </Item>
-      <Item label="Skype" name="skype" rules={[{max: 30, whitespace: false}]}>
+      <Item label="Skype" name="skype" rules={[{ max: 30, whitespace: false }]}>
         <Input />
       </Item>
-      <Item label="Telegram" name="telegram" rules={[{max: 30, whitespace: false}]}>
+      <Item label="Telegram" name="telegram" rules={[{ max: 30, whitespace: false }]}>
         <Input />
       </Item>
-      <Item label="LinkedIn" name="linkedin" rules={[{max: 30, whitespace: false}]}>
+      <Item label="LinkedIn" name="linkedin" rules={[{ max: 30, whitespace: false }]}>
         <Input />
       </Item>
-      <Item label="Location" name="location" rules={[{max: 100, whitespace: false}]}>
+      <Item label="Location" name="location" rules={[{ max: 100, whitespace: false }]}>
         <Input />
       </Item>
-      <Item label="Github" name="github" rules={[{max: 30, whitespace: false}]}>
+      <Item label="Github" name="github" rules={[{ max: 30, whitespace: false }]}>
         <Input />
       </Item>
-      <Item label="Website" name="website" rules={[{max: 100, whitespace: false}]}>
+      <Item label="Website" name="website" rules={[{ max: 100, whitespace: false }]}>
         <Input />
       </Item>
       <Item>
         <Button type="primary" htmlType="submit">
           Save
         </Button>
+      </Item>
+      <Item>
         <Button type="primary" htmlType="submit" onClick={() => form.resetFields()}>
           Reset
         </Button>
