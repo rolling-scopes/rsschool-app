c9ac9eb4c5200db77b9bf5e7a16c887328816681
AlreadyBored <shilaums@gmail.com>
2020-12-23 21:19:47 +0300

    refactor: delete unused code

--- client/src/components/cv/sections/index.ts
+++ client/src/components/cv/sections/index.ts
@@ -1,8 +1,6 @@
 import AboutSection from './AboutSection';
 import BadgesSection from './BadgesSection';
 import CoursesSection from './CoursesSection';
-import EducationSection from './EducationSection';
-import EmploymentSection from './EmploymentSection';
 import MainSection from './MainSection';
 
-export { AboutSection, BadgesSection, CoursesSection, EducationSection, EmploymentSection, MainSection };
+export { AboutSection, BadgesSection, CoursesSection, MainSection };
