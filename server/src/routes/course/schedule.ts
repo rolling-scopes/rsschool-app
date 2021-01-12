ec66e901b2f75d30302ad16b3c7113dcba2db1f9
Aleksandr Sharikov <notsaint.mail@gmail.com>
2021-01-12 18:49:48 +0300

    chore: edit export data columns

--- server/src/routes/course/schedule.ts
+++ server/src/routes/course/schedule.ts
@@ -2,16 +2,8 @@ import { OK } from 'http-status-codes';
 import { parseAsync } from 'json2csv';
 import Router from '@koa/router';
 import { ILogger } from '../../logger';
-import { courseService } from '../../services';
 import { getCourseTasks, getEvents } from '../../services/course.service';
-import { setCsvResponse, setResponse } from '../utils';
-
-export const getStudents = (_: ILogger) => async (ctx: Router.RouterContext) => {
-  const courseId = Number(ctx.params.courseId);
-  const status: string = ctx.query.status;
-  const students = await courseService.getStudents(courseId, status === 'active');
-  setResponse(ctx, OK, students);
-};
+import { setCsvResponse } from '../utils';
 
 export const getScheduleAsCsv = (_: ILogger) => async (ctx: Router.RouterContext) => {
   const courseId = Number(ctx.params.courseId);
@@ -31,28 +23,8 @@ export const getScheduleAsCsv = (_: ILogger) => async (ctx: Router.RouterContext
     special: item.special,
     name: item.task.name,
     descriptionUrl: item.task.descriptionUrl,
-    description: item.task.description,
-    tags: item.task.tags,
-    maxScore: item.maxScore,
-    scoreWeight: item.scoreWeight,
     githubId: item.taskOwner ? item.taskOwner.githubId : null,
     place: null,
-    comment: null,
-    discipline: null,
-    coordinator: null,
-    detailsUrl: null,
-    broadcastUrl: null,
-    githubPrRequired: !!item.task.githubPrRequired,
-    pairsCount: item.pairsCount,
-    disabled: item.disabled,
-    mentorStartDate: item.mentorStartDate,
-    mentorEndDate: item.mentorEndDate,
-    checker: item.checker,
-    githubRepoName: item.task.githubRepoName,
-    sourceGithubRepoUrl: item.task.sourceGithubRepoUrl,
-    useJury: item.task.useJury,
-    allowStudentArtefacts: item.task.allowStudentArtefacts,
-    verification: item.task.verification,
   }));
   const eventsToCsv = courseEvents.map(item => ({
     entityType: 'event',
@@ -65,12 +37,8 @@ export const getScheduleAsCsv = (_: ILogger) => async (ctx: Router.RouterContext
     special: item.special,
     name: item.event.name,
     descriptionUrl: item.event.descriptionUrl,
-    description: item.event.description,
     githubId: item.organizer ? item.organizer.githubId : null,
-    discipline: item.event.discipline,
-    coordinator: item.coordinator,
-    detailsUrl: item.detailsUrl,
-    broadcastUrl: item.broadcastUrl,
+    place: item.place,
   }));
 
   const csv = await parseAsync([...tasksToCsv, ...eventsToCsv]);
