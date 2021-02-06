842f072fbc63a257f3de4911d0b2951d6b863162
Maksim Shylau <shilaums@gmail.com>
2021-02-06 15:13:39 +0300

    refactor: put files into right place

--- client/src/components/cv/ContactsListCV.tsx
+++ client/src/components/cv/ContactsListCV.tsx
@@ -27,54 +27,30 @@ const allowedContacts: AllowedContacts = {
   },
   email: {
     icon: <MailOutlined />,
-    transformFunc: contact => (
-      <a className="black-on-print" href={`mailto:${contact}`}>
-        {contact}
-      </a>
-    ),
+    transformFunc: contact => <a href={`mailto:${contact}`}>{contact}</a>,
   },
   skype: {
     icon: <SkypeOutlined />,
-    transformFunc: contact => (
-      <a className="black-on-print" href={`skype:${contact}?chat`}>
-        {contact}
-      </a>
-    ),
+    transformFunc: contact => <a href={`skype:${contact}?chat`}>{contact}</a>,
   },
   telegram: {
     icon: <PhoneOutlined />,
-    transformFunc: contact => (
-      <a className="black-on-print" href={`https://t.me/${contact}`}>
-        {contact}
-      </a>
-    ),
+    transformFunc: contact => <a href={`https://t.me/${contact}`}>{contact}</a>,
   },
   linkedin: {
     icon: <LinkedinOutlined />,
-    transformFunc: contact => (
-      <a className="black-on-print" href={`https://www.linkedin.com/in/${contact}`}>
-        {contact}
-      </a>
-    ),
+    transformFunc: contact => <a href={`https://www.linkedin.com/in/${contact}`}>{contact}</a>,
   },
   location: {
     icon: <AimOutlined />,
   },
   github: {
     icon: <GithubOutlined />,
-    transformFunc: contact => (
-      <a className="black-on-print" href={`https://github.com/${contact}`}>
-        {contact}
-      </a>
-    ),
+    transformFunc: contact => <a href={`https://github.com/${contact}`}>{contact}</a>,
   },
   website: {
     icon: <IdcardOutlined />,
-    transformFunc: contact => (
-      <a className="black-on-print" href={contact}>
-        {contact}
-      </a>
-    ),
+    transformFunc: contact => <a href={contact}>{contact}</a>,
   },
 };
 
@@ -86,35 +62,38 @@ type EntryOf<T extends object> = { [K in keyof T]: [K, T[K]] }[keyof T];
 
 function ContactsListCV(props: Props) {
   const { contacts } = props;
+  let res;
+  if (contacts !== null) {
+    const contactsFiltered = Object.entries(contacts);
+    const renderContacts = contactsFiltered.filter((contact): contact is EntryOf<Contacts> => contact[1] !== null);
+    res = (
+      <List
+        dataSource={renderContacts}
+        renderItem={contact => {
+          const contactType: ContactType = contact[0];
+          const contactText = contact[1];
+          const icon = allowedContacts[contactType].icon;
+          let contactTextElem;
+          const transformFunction = allowedContacts[contactType].transformFunc;
+          if (transformFunction) {
+            contactTextElem = transformFunction(contactText as string);
+          } else {
+            contactTextElem = <Text>{contactText}</Text>;
+          }
+          return (
+            <Item>
+              {icon}
+              {contactTextElem}
+            </Item>
+          );
+        }}
+      />
+    );
+  } else {
+    res = null;
+  }
 
-  if (!contacts) return null;
-
-  const contactsEntries = Object.entries(contacts);
-  const contactsToRender = contactsEntries.filter((contact): contact is EntryOf<Contacts> => contact[1] !== null);
-
-  return (
-    <List
-      size="small"
-      dataSource={contactsToRender}
-      renderItem={contact => {
-        const [contactType, contactText] = contact;
-        const icon = allowedContacts[contactType].icon;
-
-        const transformFunction = allowedContacts[contactType].transformFunc;
-        const contactTextElem = transformFunction ? (
-          transformFunction(contactText as string)
-        ) : (
-          <Text>{contactText}</Text>
-        );
-        return (
-          <Item>
-            {icon}
-            {contactTextElem}
-          </Item>
-        );
-      }}
-    />
-  );
+  return res;
 }
 
 export default ContactsListCV;
