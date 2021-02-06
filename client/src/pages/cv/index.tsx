57813c0892cc637e10cbbe6e576829e395473c14
Maxim Shylau <34455330+AlreadyBored@users.noreply.github.com>
2021-02-06 15:02:39 +0300

    Add files via upload

--- client/src/pages/cv/index.tsx
+++ client/src/pages/cv/index.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
-import { Layout, Switch } from 'antd';
+import { Layout, Switch, Typography } from 'antd';
+import { LoadingScreen } from 'components/LoadingScreen';
 import { NextRouter, withRouter } from 'next/router';
 import withSession, { Session } from 'components/withSession';
 import { Header, FooterLayout } from 'components';
-import FormCV from 'components/cv/FormCV';
-import ViewCV from 'components/cv/ViewCV';
+import FormCV from 'components/CV/FormCV';
+import ViewCV from 'components/CV/ViewCV';
+import NoConsentViewCV from 'components/CV/NoConsentViewCV';
+
+import { UserService } from '../../services/user';
 
 const { Content } = Layout;
+const { Text } = Typography;
 
 type Props = {
   router: NextRouter;
@@ -16,52 +21,118 @@ type Props = {
 type State = {
   isLoading: boolean;
   editMode: boolean;
+  opportunitiesConsent: boolean | null;
 };
 
 class CVPage extends React.Component<Props, State> {
   state: State = {
-    isLoading: false,
-    editMode: true,
+    isLoading: true,
+    editMode: false,
+    opportunitiesConsent: null
   };
 
-  private switchView(checked: boolean) {
+  private userService = new UserService();
+
+  private async switchView(checked: boolean) {
     if (checked) {
-      this.setState({
-        editMode: true,
+      await this.setState({
+        editMode: false,
       });
     } else {
-      this.setState({
-        editMode: false,
+      await this.setState({
+        editMode: true,
       });
     }
   }
 
+  private async giveConsent(githubId: string) {
+    await this.setState({ isLoading: true });
+    const newConsent = await this.userService.changeOpportunitiesConsent(githubId, true);
+    await this.setState({
+      opportunitiesConsent: newConsent
+    });
+    await this.setState({ isLoading: false });
+  }
+
+  private async withdrawConsent(githubId: string) {
+    await this.setState({ isLoading: true });
+    const newConsent = await this.userService.changeOpportunitiesConsent(githubId, false);
+    await this.setState({
+      opportunitiesConsent: newConsent
+    });
+    await this.setState({ isLoading: false });
+  }
+
+  async componentDidMount() {
+    await this.setState({ isLoading: true });
+    const opportunitiesConsent = await this.userService.getOpportunitiesConsent(this.props.router.query.githubId as string);
+    await this.setState({ opportunitiesConsent });
+    await this.setState({ isLoading: false });
+  }
+
   render() {
-    const { editMode } = this.state;
-    const githubId = this.props.session.githubId;
+
+    const { editMode, opportunitiesConsent, isLoading } = this.state;
+    const userGithubId = this.props.session.githubId;
+
+    const ownerId = this.props.router.query.githubId as string;
+
+    const isOwner = userGithubId === ownerId;
+
+    let content;
+
+    if (isOwner) {
+      if (opportunitiesConsent) {
+        content = (
+          <>
+            <Text className='hide-on-print'>Switch view:</Text>
+            <Switch
+              className='hide-on-print'
+              style={{ marginLeft: '5px' }}
+              defaultChecked={!editMode}
+              onChange={this.switchView.bind(this)}
+              checkedChildren="CV view"
+              unCheckedChildren="Form view"
+            />
+            {editMode ? <FormCV ownerId={userGithubId} withdrawConsent={() => this.withdrawConsent(ownerId as string)} /> : <ViewCV ownerId={userGithubId} />}
+          </>
+        );
+      } else {
+        content = <NoConsentViewCV isOwner={true} giveConsent={() => this.giveConsent(ownerId as string)} />;
+      }
+    } else {
+      if (opportunitiesConsent) {
+        content = <ViewCV ownerId={ownerId} />;
+      } else {
+        content = <NoConsentViewCV isOwner={false} />;
+      }
+    }
 
     return (
       <>
-        <Header username={githubId} />
-        <Layout style={{ paddingTop: '30px', margin: 'auto', maxWidth: '960px' }}>
-          <Content>
-            <label>
-              Switch view:
-              <br />
-              <Switch
-                defaultChecked={editMode}
-                onChange={this.switchView.bind(this)}
-                checkedChildren="CV view"
-                unCheckedChildren="Form view"
-              />
-            </label>
-            {editMode ? <ViewCV /> : <FormCV ownerId={githubId} />}
+      <LoadingScreen show={isLoading}>
+        <Header username={userGithubId} />
+        <Layout style={{ margin: 'auto', maxWidth: '960px', backgroundColor: '#FFF' }}>
+          <Content style={{ backgroundColor: '#FFF', minHeight: '500px', margin: 'auto' }}>
+            {content}
           </Content>
+
         </Layout>
         <FooterLayout />
+
+      </LoadingScreen>
+      <style jsx global>{`
+          @media print {
+            .hide-on-print, .ant-avatar-icon, .profile, .footer {
+              display: none
+            }
+          }
+      `}</style>
       </>
     );
   }
 }
+
+
 
 export default withRouter(withSession(CVPage));
