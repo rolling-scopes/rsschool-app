4e4aeed5a3b2a5dbf17b5981ec1ef0621fedcce4
Maxim Shylau <34455330+AlreadyBored@users.noreply.github.com>
2021-02-06 15:28:20 +0300

    Add files via upload

--- common/models/cv.ts
+++ common/models/cv.ts
@@ -1,3 +1,4 @@
+import { StudentStats, PublicFeedback } from './profile';
 export interface CourseData {
   locationName: string;
   courseFullName: string;
@@ -10,17 +11,19 @@ export interface CourseData {
 
 export type EnglishLevel = 'a0' | 'a1' | 'a1+' | 'a2' | 'a2+' | 'b1' | 'b1+' | 'b2' | 'b2+' | 'c1' | 'c1+' | 'c2';
 export type MilitaryService = 'served' | 'liable' | 'notLiable' | null;
-export type SelfIntroLink = string | null;
+
 export type ContactType = 'phone' | 'email' | 'skype' | 'telegram' | 'linkedin' | 'location' | 'github' | 'website';
 
 export interface UserData {
   avatarLink: string | null;
   name: string;
   desiredPosition: string;
-  selfIntroLink: SelfIntroLink;
+  selfIntroLink: string | null;
   englishLevel: EnglishLevel;
   militaryService: MilitaryService;
-  notes: string;
+  notes: string | null;
+  startFrom: string | null;
+  fullTime: boolean
 }
 
 export type Contacts = {
@@ -36,6 +39,11 @@ export interface ContactsFromProfile {
   location: string | null;
 }
 
+export interface CVDataFromProfile extends ContactsFromProfile {
+  name: string;
+  notes: string | null;
+}
+
 export interface FieldData {
   name: string[];
   value: any;
@@ -43,3 +51,38 @@ export interface FieldData {
   validating: boolean;
   errors: string[];
 }
+
+export interface CVUserData {
+  selfIntroLink: string | null;
+  militaryService: MilitaryService;
+  avatarLink: string | null;
+  desiredPosition: string;
+  englishLevel: EnglishLevel;
+  cvName: string | null;
+  cvNotes: string | null;
+  cvPhone: string | null;
+  cvEmail: string | null;
+  cvSkype: string | null;
+  cvTelegram: string | null;
+  cvLinkedin: string | null;
+  cvLocation: string | null;
+  cvGithub: string | null;
+  cvWebsite: string | null;
+  startFrom: string | null;
+  fullTime: boolean;
+}
+
+export interface JobSeekerData {
+  cvName: string | null;
+  desiredPosition: string | null;
+  githubId: string;
+  englishLevel: EnglishLevel;
+  fullTime: boolean;
+  cvLocation: string | null;
+  startFrom: string | null;
+  courses: StudentStats;
+  publicFeedback: PublicFeedback[]
+}
+
+
+
