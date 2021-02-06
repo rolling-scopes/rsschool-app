558c93c25fcf91f9500ea1b38ba4e075d8fbf701
Maksim Shylau <shilaums@gmail.com>
2021-02-06 17:05:14 +0300

    refactor: remove unnecessary files

--- client/src/CV/sections/index.ts
+++
@@ -1,6 +0,0 @@
-import AboutSection from './AboutSection';
-import FeedbackSection from './FeedbackSection';
-import CoursesSection from './CoursesSection';
-import MainSection from './MainSection';
-
-export { AboutSection, FeedbackSection, CoursesSection, MainSection };
