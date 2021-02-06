76489bf10fea3c5f47734471b8394b7195f2779c
Maxim Shylau <34455330+AlreadyBored@users.noreply.github.com>
2021-02-06 15:23:08 +0300

    Add files via upload

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { Button, Menu, Dropdown } from 'antd';
 import { GithubAvatar } from 'components/GithubAvatar';
 import css from 'styled-jsx/css';
-// TODO: ucnomment after testing
-import { EyeOutlined, EditOutlined, LogoutOutlined, SaveTwoTone /* ExperimentOutlined */ } from '@ant-design/icons';
+
+import { EyeOutlined, EditOutlined, LogoutOutlined, SaveTwoTone, ExperimentOutlined } from '@ant-design/icons';
 
 type Props = {
   username: string;
@@ -17,13 +17,7 @@ type Props = {
 };
 
 export function Header(props: Props) {
-  const {
-    isProfilePage,
-    onChangeProfilePageMode,
-    isProfileEditingModeEnabled,
-    // TODO: uncomment after testing
-    isSaveButtonVisible /* username */,
-  } = props;
+  const { isProfilePage, onChangeProfilePageMode, isProfileEditingModeEnabled, isSaveButtonVisible, username } = props;
 
   const menuActiveItemStyle = { backgroundColor: '#e0f2ff' };
   const menu = (
@@ -48,8 +42,7 @@ export function Header(props: Props) {
           <EditOutlined /> Edit
         </Button>
       </Menu.Item>
-      {/* TODO: uncomment after testing */}
-      {/*       <Menu.Item key="2" style={isProfileEditingModeEnabled ? menuActiveItemStyle : undefined}>
+      <Menu.Item key="2" style={isProfileEditingModeEnabled ? menuActiveItemStyle : undefined}>
         <Button
           type="link"
           href={`/cv?githubId=${username}`}
@@ -57,7 +50,7 @@ export function Header(props: Props) {
         >
           <ExperimentOutlined /> My CV
         </Button>
-      </Menu.Item> */}
+      </Menu.Item>
       <Menu.Divider />
       <Menu.Item key="3">
         <Button type="link" href={'/api/auth/logout'} style={{ textAlign: 'left' }}>
