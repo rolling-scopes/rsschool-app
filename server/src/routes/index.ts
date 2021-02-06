24bb89845adc1112093be17286b0642452bba548
Maxim Shylau <34455330+AlreadyBored@users.noreply.github.com>
2021-02-06 15:33:35 +0300

    feat: solve conflicts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -28,7 +28,11 @@ import { jwtRoute } from './jwt';
 import { userRoute } from './user';
 import { consentRoute } from './consent';
 import { repositoryRoute } from './repository';
+<<<<<<< HEAD
+import { filesRoute } from './file';
+=======
 import { opportunitiesRoute } from './opportunities';
+>>>>>>> 6ebdb3f (Add files via upload)
 
 import { ILogger } from '../logger';
 import { userRolesMiddleware, courseMiddleware } from './middlewares';
@@ -75,7 +79,11 @@ export const routesMiddleware: RoutesMiddleware = (logger: ILogger) => {
   applyRouter(router, userRoute(logger));
   applyRouter(router, consentRoute(logger));
   applyRouter(router, repositoryRoute(logger));
+<<<<<<< HEAD
+  applyRouter(router, filesRoute(logger));
+=======
   applyRouter(router, opportunitiesRoute(logger));
+>>>>>>> 6ebdb3f (Add files via upload)
 
   return { publicRouter: router };
 };
