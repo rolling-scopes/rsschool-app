2c13be42b55e0f925caaf30a424f4d2f00f63285
Maxim Shylau <34455330+AlreadyBored@users.noreply.github.com>
2021-02-06 15:08:37 +0300

    Add files via upload

---
+++ client/src/CV/forms/ContactsForm.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import { Form, Input, Button } from 'antd';
+import { Contacts } from '../../../../../common/models/cv';
+
+const { Item } = Form;
+
+type Props = {
+  contactsList: Contacts;
+  handleFunc: (data: any) => void;
+};
+
+export default function ContactsForm(props: Props) {
+  const { contactsList, handleFunc } = props;
+
+  const [form] = Form.useForm();
+
+  React.useEffect(() => {
+    form.setFieldsValue(contactsList);
+  }, [contactsList]);
+
+  const sumbitData = (values: FormData) => {
+
+    const dataObj = {
+      type: 'contacts',
+      data: values
+    };
+
+    handleFunc(dataObj);
+
+  };
+
+  return (
+    <Form form={form} name="contacts" onFinish={sumbitData}>
+      <Item label="Phone" name="phone" rules={[{ max: 15, whitespace: false }]}>
+        <Input />
+      </Item>
+      <Item label="Email" name="email" rules={[{ max: 50, whitespace: false }]}>
+        <Input />
+      </Item>
+      <Item label="Skype" name="skype" rules={[{ max: 30, whitespace: false }]}>
+        <Input />
+      </Item>
+      <Item label="Telegram" name="telegram" rules={[{ max: 30, whitespace: false }]}>
+        <Input />
+      </Item>
+      <Item label="LinkedIn" name="linkedin" rules={[{ max: 30, whitespace: false }]}>
+        <Input />
+      </Item>
+      <Item label="Location" name="location" rules={[{ max: 100, whitespace: false }]}>
+        <Input />
+      </Item>
+      <Item label="Github" name="github" rules={[{ max: 30, whitespace: false }]}>
+        <Input />
+      </Item>
+      <Item label="Website" name="website" rules={[{ max: 100, whitespace: false }]}>
+        <Input />
+      </Item>
+      <Item>
+        <Button type="primary" htmlType="submit">
+          Save
+        </Button>
+      </Item>
+      <Item>
+        <Button type="primary" htmlType="button" onClick={() => form.resetFields()}>
+          Reset
+        </Button>
+      </Item>
+    </Form>
+  );
+}
