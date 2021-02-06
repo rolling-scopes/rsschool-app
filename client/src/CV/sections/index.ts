39520e0e0c543dbebb313244fe8d1b63aed02dbe
Maxim Shylau <34455330+AlreadyBored@users.noreply.github.com>
2021-02-06 15:08:37 +0300

    Add files via upload

---
+++ client/src/CV/sections/index.ts
@@ -0,0 +1,6 @@
+import AboutSection from './AboutSection';
+import FeedbackSection from './FeedbackSection';
+import CoursesSection from './CoursesSection';
+import MainSection from './MainSection';
+
+export { AboutSection, FeedbackSection, CoursesSection, MainSection };
