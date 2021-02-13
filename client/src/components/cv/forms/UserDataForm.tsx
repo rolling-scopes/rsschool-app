371c2c7e86c24b1e18858a7686a5ccec68a2b66c
Maksim Shylau <shilaums@gmail.com>
2021-02-13 20:36:49 +0300

    feat: improve withdraw consent btn & minor changes

--- client/src/components/cv/forms/UserDataForm.tsx
+++ client/src/components/cv/forms/UserDataForm.tsx
@@ -35,19 +35,19 @@ export default function UserDataForm(props: Props) {
 
     const content = (
         <>
-            <Item label="Name" name="name" rules={[{ required: true, max: 100, whitespace: false }]}>
+            <Item label="Name" wrapperCol={{span: 24}} labelCol={{span: 24}} name="name" rules={[{ required: true, max: 100, whitespace: false }]}>
                 <Input placeholder="Enter your name" />
             </Item>
-            <Item label="Desired position" name="desiredPosition" rules={[{ required: true, max: 100, whitespace: false }]}>
+            <Item label="Desired position" wrapperCol={{span: 24}} labelCol={{span: 24}} name="desiredPosition" rules={[{ required: true, max: 100, whitespace: false }]}>
                 <Input placeholder="Enter desired position" />
             </Item>
-            <Item label="Self introduction video" name="selfIntroLink" rules={[{ max: 300, whitespace: false }]}>
+            <Item label="Self introduction video" wrapperCol={{span: 24}} labelCol={{span: 24}} name="selfIntroLink" rules={[{ max: 300, whitespace: false }]}>
                 <Input placeholder="Link to video with self introduction" />
             </Item>
-            <Item label="Link to avatar" name="avatarLink" rules={[{ max: 300, whitespace: false }]}>
+            <Item label="Link to avatar" wrapperCol={{span: 24}} labelCol={{span: 24}} name="avatarLink" rules={[{ max: 300, whitespace: false }]}>
                 <Input placeholder="Link to image" />
             </Item>
-            <Item label="Select your English level" name="englishLevel" rules={[{ required: true }]}>
+            <Item label="Select your English level" wrapperCol={{span: 24}} labelCol={{span: 24}} name="englishLevel" rules={[{ required: true }]}>
                 <Select placeholder="Not selected yet">
                     <Option value="a0">A0</Option>
                     <Option value="a1">A1</Option>
@@ -63,20 +63,20 @@ export default function UserDataForm(props: Props) {
                     <Option value="c2">C2</Option>
                 </Select>
             </Item>
-            <Item label="Military service" name="militaryService">
+            <Item label="Military service" wrapperCol={{span: 24}} labelCol={{span: 24}} name="militaryService">
                 <Select placeholder="Not selected yet">
                     <Option value="served">Served</Option>
                     <Option value="liable">Liable</Option>
                     <Option value="notLiable">Not liable</Option>
                 </Select>
             </Item>
-            <Item label="Ready to start work from" name="startFrom">
+            <Item label="Ready to start work from" wrapperCol={{span: 24}} labelCol={{span: 24}} name="startFrom">
                 <DatePicker placeholder="Not selected yet" picker="date" />
             </Item>
-            <Item label="Ready to work full time" name="fullTime" valuePropName="checked">
+            <Item label="Ready to work full time" colon={false} name="fullTime" valuePropName="checked">
                 <Checkbox />
             </Item>
-            <Item label="About me" name="notes" rules={[{ required: true, max: 1000, min: 30, whitespace: false }]}>
+            <Item label="About me" wrapperCol={{span: 24}} labelCol={{span: 24}} name="notes" rules={[{ required: true, max: 1000, min: 30, whitespace: false }]}>
                 <TextArea rows={4} placeholder="Short info about you (30-1000 symbols)" />
             </Item>
         </>
