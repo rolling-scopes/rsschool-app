e1ca8055941c3ed7555f21448a1e11b6b99bd731
Maksim Shylau <shilaums@gmail.com>
2021-02-14 21:07:16 +0300

    refactor: change mechanism of data saving & bound it to 1 button

--- client/src/components/cv/forms/UserDataForm.tsx
+++ client/src/components/cv/forms/UserDataForm.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import moment from 'moment';
-import { Form, Input, Select, DatePicker, Checkbox } from 'antd';
+import { Form, Input, Select, DatePicker, Checkbox, Card } from 'antd';
 import { UserData } from '../../../../../common/models/cv';
-import FormCV from '../FormCV';
 
 const { Item } = Form;
 const { Option } = Select;
@@ -10,11 +9,10 @@ const { TextArea } = Input;
 
 type Props = {
     userData: UserData;
-    handleFunc: (data: any) => void;
 };
 
-export default function UserDataForm(props: Props) {
-    const { userData, handleFunc } = props;
+const UserDataForm = React.forwardRef((props: Props, ref: any) => {
+    const { userData } = props;
 
     const { avatarLink, name, desiredPosition, selfIntroLink, englishLevel, militaryService, notes } = userData;
 
@@ -33,56 +31,62 @@ export default function UserDataForm(props: Props) {
         fullTime
     };
 
-    const content = (
-        <>
-            <Item label="Name" wrapperCol={{span: 24}} labelCol={{span: 24}} name="name" rules={[{ required: true, max: 100, whitespace: false }]}>
-                <Input placeholder="Enter your name" />
-            </Item>
-            <Item label="Desired position" wrapperCol={{span: 24}} labelCol={{span: 24}} name="desiredPosition" rules={[{ required: true, max: 100, whitespace: false }]}>
-                <Input placeholder="Enter desired position" />
-            </Item>
-            <Item label="Self introduction video" wrapperCol={{span: 24}} labelCol={{span: 24}} name="selfIntroLink" rules={[{ max: 300, whitespace: false }]}>
-                <Input placeholder="Link to video with self introduction" />
-            </Item>
-            <Item label="Link to avatar" wrapperCol={{span: 24}} labelCol={{span: 24}} name="avatarLink" rules={[{ max: 300, whitespace: false }]}>
-                <Input placeholder="Link to image" />
-            </Item>
-            <Item label="Select your English level" wrapperCol={{span: 24}} labelCol={{span: 24}} name="englishLevel" rules={[{ required: true }]}>
-                <Select placeholder="Not selected yet">
-                    <Option value="a0">A0</Option>
-                    <Option value="a1">A1</Option>
-                    <Option value="a1+">A1+</Option>
-                    <Option value="a2">A2</Option>
-                    <Option value="a2+">A2+</Option>
-                    <Option value="b1">B1</Option>
-                    <Option value="b1+">B1+</Option>
-                    <Option value="b2">B2</Option>
-                    <Option value="b2+">B2+</Option>
-                    <Option value="c1">C1</Option>
-                    <Option value="c1+">C1+</Option>
-                    <Option value="c2">C2</Option>
-                </Select>
-            </Item>
-            <Item label="Military service" wrapperCol={{span: 24}} labelCol={{span: 24}} name="militaryService">
-                <Select placeholder="Not selected yet">
-                    <Option value="served">Served</Option>
-                    <Option value="liable">Liable</Option>
-                    <Option value="notLiable">Not liable</Option>
-                </Select>
-            </Item>
-            <Item label="Ready to start work from" wrapperCol={{span: 24}} labelCol={{span: 24}} name="startFrom">
-                <DatePicker placeholder="Not selected yet" picker="date" />
-            </Item>
-            <Item label="Ready to work full time" colon={false} name="fullTime" valuePropName="checked">
-                <Checkbox />
-            </Item>
-            <Item label="About me" wrapperCol={{span: 24}} labelCol={{span: 24}} name="notes" rules={[{ required: true, max: 1000, min: 30, whitespace: false }]}>
-                <TextArea rows={4} placeholder="Short info about you (30-1000 symbols)" />
-            </Item>
-        </>
-    )
+    const [form] = Form.useForm();
+
+    React.useEffect(() => {
+        form.setFieldsValue(formValues);
+    }, [formValues]);
 
     return (
-        <FormCV name="userData" title="General info" content={content} submitFunc={handleFunc} data={formValues} />
+        <Card title="General info">
+            <Form form={form} ref={ref} name="userData">
+                <Item label="Name" wrapperCol={{ span: 24 }} labelCol={{ span: 24 }} name="name" rules={[{ required: true, max: 100, whitespace: false }]}>
+                    <Input placeholder="Enter your name" />
+                </Item>
+                <Item label="Desired position" wrapperCol={{ span: 24 }} labelCol={{ span: 24 }} name="desiredPosition" rules={[{ required: true, max: 100, whitespace: false }]}>
+                    <Input placeholder="Enter desired position" />
+                </Item>
+                <Item label="Self introduction video" wrapperCol={{ span: 24 }} labelCol={{ span: 24 }} name="selfIntroLink" rules={[{ max: 300, whitespace: false }]}>
+                    <Input placeholder="Link to video with self introduction" />
+                </Item>
+                <Item label="Link to avatar" wrapperCol={{ span: 24 }} labelCol={{ span: 24 }} name="avatarLink" rules={[{ max: 300, whitespace: false }]}>
+                    <Input placeholder="Link to image" />
+                </Item>
+                <Item label="Select your English level" wrapperCol={{ span: 24 }} labelCol={{ span: 24 }} name="englishLevel" rules={[{ required: true }]}>
+                    <Select placeholder="Not selected yet">
+                        <Option value="a0">A0</Option>
+                        <Option value="a1">A1</Option>
+                        <Option value="a1+">A1+</Option>
+                        <Option value="a2">A2</Option>
+                        <Option value="a2+">A2+</Option>
+                        <Option value="b1">B1</Option>
+                        <Option value="b1+">B1+</Option>
+                        <Option value="b2">B2</Option>
+                        <Option value="b2+">B2+</Option>
+                        <Option value="c1">C1</Option>
+                        <Option value="c1+">C1+</Option>
+                        <Option value="c2">C2</Option>
+                    </Select>
+                </Item>
+                <Item label="Military service" wrapperCol={{ span: 24 }} labelCol={{ span: 24 }} name="militaryService">
+                    <Select placeholder="Not selected yet">
+                        <Option value="served">Served</Option>
+                        <Option value="liable">Liable</Option>
+                        <Option value="notLiable">Not liable</Option>
+                    </Select>
+                </Item>
+                <Item label="Ready to start work from" wrapperCol={{ span: 24 }} labelCol={{ span: 24 }} name="startFrom">
+                    <DatePicker placeholder="Not selected yet" picker="date" />
+                </Item>
+                <Item label="Ready to work full time" colon={false} name="fullTime" valuePropName="checked">
+                    <Checkbox />
+                </Item>
+                <Item label="About me" wrapperCol={{ span: 24 }} labelCol={{ span: 24 }} name="notes" rules={[{ required: true, max: 1000, min: 30, whitespace: false }]}>
+                    <TextArea rows={4} placeholder="Short info about you (30-1000 symbols)" />
+                </Item>
+            </Form>
+        </Card>
     );
-}
+});
+
+export default UserDataForm;
