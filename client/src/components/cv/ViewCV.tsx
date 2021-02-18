0f7f4658addca54fd3ebe66ce34df172eb4fb129
Maksim Shylau <shilaums@gmail.com>
2021-02-18 21:39:34 +0300

    refactor: remove border

--- client/src/components/cv/ViewCV.tsx
+++ client/src/components/cv/ViewCV.tsx
@@ -114,7 +114,7 @@ class ViewCV extends React.Component<Props, State> {
 
     return (
       <LoadingScreen show={isLoading}>
-        <Layout className="hide-border-on-print" style={{ marginBottom: '15px', maxWidth: '960px', backgroundColor: '#FFF', border: '1px solid black' }}>
+        <Layout className="hide-border-on-print" style={{ marginBottom: '15px', maxWidth: '960px', backgroundColor: '#FFF'}}>
           <Content>
             <Space direction="vertical" style={{ width: '100%', backgroundColor: '#FFF' }}>
               {userData && contactsList && (
