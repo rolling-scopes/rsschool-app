b10ccce12adeb31f5032cd0084275bccd05562fb
Maksim Shylau <shilaums@gmail.com>
2021-03-04 21:17:21 +0300

    refactor: format code

--- client/src/components/cv/NoConsentViewCV.tsx
+++ client/src/components/cv/NoConsentViewCV.tsx
@@ -18,8 +18,8 @@ function NoConsentViewCV(props: Props) {
         'Personal information (name, desired position, English level, military service, avatar, link to self-presentation, short self-description, etc.);',
         'Contact details (phone, email, skype, telegram, linkedIn, location to work, github, website link);',
         'Information about passed school courses (courses info, mentor, course status, score, position);',
-        'Public feedback information (grattitudes)'
-      ]
+        'Public feedback information (grattitudes)',
+      ],
     },
     ru: {
       header: 'Вы уверены? Следующая информация будет доступна рекрутерам:',
@@ -27,16 +27,24 @@ function NoConsentViewCV(props: Props) {
         'Личная информация (имя, желаемая позиция, уровень английского, отношение к военное службе, аватар, ссылка на самопрезентацию, краткое самоописание и т.д.);',
         'Контактные данные (телефон, электронная почта, скайп, телеграм, linkedIn, локация, в которой хотите работать, гитхаб, ссылка на вебсайт);',
         'Информация о пройденных в школе курсах (в каких курсах принято участие, статус курса для участника, скор, место в скоре);',
-        'Информация о публичной обратной связи (grattitudes)'
-      ]
-    }
+        'Информация о публичной обратной связи (grattitudes)',
+      ],
+    },
   };
 
   const confirmationModalContent = (
     <List
-      header={<Tooltip placement='topLeft' title={confirmationModalInfo.ru.header}>{confirmationModalInfo.en.header}</Tooltip>}
+      header={
+        <Tooltip placement="topLeft" title={confirmationModalInfo.ru.header}>
+          {confirmationModalInfo.en.header}
+        </Tooltip>
+      }
       dataSource={confirmationModalInfo.en.availableDataList}
-      renderItem={(item, idx) => <Tooltip placement='topLeft' title={confirmationModalInfo.ru.availableDataList[idx]}><Item>{item}</Item></Tooltip>}
+      renderItem={(item, idx) => (
+        <Tooltip placement="topLeft" title={confirmationModalInfo.ru.availableDataList[idx]}>
+          <Item>{item}</Item>
+        </Tooltip>
+      )}
     />
   );
 
@@ -48,20 +56,23 @@ function NoConsentViewCV(props: Props) {
       maskClosable: true,
       onOk() {
         giveConsent!();
-      }
+      },
     });
   };
 
   return isOwner ? (
     <Result
-      status='info'
-      title='To create your CV page for recruiters to view, you need to allow the use and processing of your data.'
-      extra={<Button htmlType='button' onClick={showConfirmationModal}>Give consent</Button>}
+      status="info"
+      title="To create your CV page for recruiters to view, you need to allow the use and processing of your data."
+      extra={
+        <Button htmlType="button" onClick={showConfirmationModal}>
+          Give consent
+        </Button>
+      }
     />
   ) : (
-      <Result status={403} title="This user doesn't have CV yet" />
-    );
-
+    <Result status={403} title="This user doesn't have CV yet" />
+  );
 }
 
 export default NoConsentViewCV;
