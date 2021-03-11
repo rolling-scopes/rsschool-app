4c610cae1eef520b49e211445dc3e9304d1f1d09
Maksim Shylau <shilaums@gmail.com>
2021-03-11 17:15:20 +0300

    refactor: place student status in separate component

--- client/src/components/cv/sections/CoursesSection.tsx
+++ client/src/components/cv/sections/CoursesSection.tsx
@@ -7,6 +7,22 @@ import { CourseData } from '../../../../../common/models/cv';
 const { Text } = Typography;
 const { Item } = List;
 
+function StudentStatus(props: { isExpelled: boolean; certificateId: string | null; isCourseCompleted: boolean }) {
+  const { isExpelled, certificateId, isCourseCompleted } = props;
+  if (isExpelled) return <Text>Expelled</Text>;
+  const certificateLink = certificateId ? `https://app.rs.school/certificate/${certificateId}` : '';
+
+  if (certificateId)
+    return (
+      <>
+        <Text>Completed with </Text>
+        <a href={certificateLink}>certificate</a>
+      </>
+    );
+  if (isCourseCompleted) return <Text>Completed</Text>;
+  return <Text>In progress</Text>;
+}
+
 type Props = {
   coursesData: CourseData[];
 };
@@ -30,25 +46,8 @@ function CoursesSection(props: Props) {
           mentor: { name: mentorName, githubId: mentorGithubId },
         } = record;
         const title = `${courseFullName}${locationName ? locationName : ''}`;
-        const certificateLink = certificateId ? `https://app.rs.school/certificate/${certificateId}` : '';
         const courseStats = `Score: ${totalScore}
         Position: ${position}`;
-        let courseStatus;
-        if (isExpelled) {
-          courseStatus = <Text>Expelled</Text>;
-        } else if (certificateId) {
-          courseStatus = (
-            <>
-              <Text>Completed with </Text>
-              <a href={certificateLink}>certificate</a>
-            </>
-          );
-        } else if (isCourseCompleted) {
-          courseStatus = <Text>Completed</Text>;
-        } else {
-          courseStatus = <Text>In progress</Text>;
-        }
-
         return (
           <Item style={{ fontSize: '16px' }}>
             <Row justify="space-between" style={{ width: '100%' }}>
@@ -56,7 +55,11 @@ function CoursesSection(props: Props) {
                 <Text strong>{title}</Text>
                 <br />
                 <Text>Course status: </Text>
-                {courseStatus}
+                <StudentStatus
+                  isExpelled={isExpelled}
+                  certificateId={certificateId}
+                  isCourseCompleted={isCourseCompleted}
+                />
               </Col>
               <Col span={3}>
                 <Text>
