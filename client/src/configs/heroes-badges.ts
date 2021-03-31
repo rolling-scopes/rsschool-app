57c42184dbd5256fa23c147b6b138c798a612ee1
Aleh Serhiyenia <Aleh_Serhiyenia@epam.com>
2021-03-31 15:41:04 +0300

    feat: add unicorn badges

--- client/src/configs/heroes-badges.ts
+++ client/src/configs/heroes-badges.ts
@@ -2,29 +2,36 @@ export default {
   Congratulations: {
     name: 'Congratulations',
     pictureId: 23,
+    url: 'Congratulations.svg',
   },
   Expert_help: {
     name: 'Expert help',
     pictureId: 39,
+    url: 'ExpertHelp.svg',
   },
   Great_speaker: {
     name: 'Great speaker',
     pictureId: 29,
+    url: 'GreatSpeaker.svg',
   },
   Good_job: {
     name: 'Good job',
     pictureId: 17,
+    url: 'GoodJob.svg',
   },
   Helping_hand: {
     name: 'Helping hand',
     pictureId: 14,
+    url: 'HelpingHand.svg',
   },
   Hero: {
     name: 'Hero',
     pictureId: 25,
+    url: 'Hero.svg',
   },
   Thank_you: {
     name: 'Thank you',
     pictureId: 12,
+    url: 'ThankYou.svg',
   },
 };
