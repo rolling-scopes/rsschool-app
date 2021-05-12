a3b1362c990b565de967d14f2fc14ee50e7baecd
Fodin <fadmin@list.ru>
2021-05-12 19:09:04 +0300

    Update crossCheck.ts

--- server/src/rules/distribution/crossCheck.ts
+++ server/src/rules/distribution/crossCheck.ts
@@ -7,7 +7,7 @@ function randomInteger(min: number, max: number): number {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1));
 }
 
-function shuffle<T>(array: T[]): T[]{
+function shuffle<T>(array: T[]): T[] {
   const arrcopy = [...array];
 
   for (let i = arrcopy.length - 1; i > 0; i -= 1) {
@@ -26,16 +26,20 @@ function createShifts(numberOfShifts: number, maxShiftValue: number): number[] {
   return Array.from(shifts);
 }
 
-export function createCrossCheckPairs(studentIds: number[], checkersNumber: number): CrossCheckPair[] {
+export function createCrossCheckPairs(
+  studentIds: number[],
+  checkersNumber: number,
+): CrossCheckPair[] {
   const pairs: CrossCheckPair[] = [];
   const shuffledStudentsIds = shuffle(studentIds);
   const studentsNumber = studentIds.length;
   const shifts = createShifts(checkersNumber, Math.floor((studentsNumber - 1) / 2));
+  console.log(shifts);
 
   shuffledStudentsIds.forEach((studentId, index) => {
     for (let i = 0; i < checkersNumber; i++) {
       const checkerId = shuffledStudentsIds[(index + studentsNumber + shifts[i]) % studentsNumber];
-      pairs.push({checkerId, studentId});
+      pairs.push({ checkerId, studentId });
     }
   });
   return pairs;
