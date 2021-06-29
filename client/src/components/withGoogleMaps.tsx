5fd516dc040fa4665403eabffbf85e0e17084324
Andrei <andrei@palchys.com>
2021-06-29 10:30:49 +0300

    fix formatting

--- client/src/components/withGoogleMaps.tsx
+++ client/src/components/withGoogleMaps.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import Head from 'next/head';
 import { mapsApiKey } from 'configs/gcp';
 
+const url = `https://maps.googleapis.com/maps/api/js?key=${mapsApiKey}&libraries=places&language=en`;
+
 function withGoogleMaps(WrappedComponent: React.ComponentType<any>) {
   return class extends React.Component<any> {
     render() {
@@ -9,7 +11,7 @@ function withGoogleMaps(WrappedComponent: React.ComponentType<any>) {
         <>
           {mapsApiKey && (
             <Head>
-              <script src={`https://maps.googleapis.com/maps/api/js?key=${mapsApiKey}&libraries=places&language=en`}></script>
+              <script src={url}></script>
             </Head>
           )}
           <WrappedComponent {...this.props} />
