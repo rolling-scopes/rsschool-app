dd2016cb1cd2974b00f00b2137f2009a8cd7a1fa
Maksim Shylau <shilaums@gmail.com>
2021-08-07 17:22:07 +0300

    fix: replace useless index key with unique value

--- client/src/pages/employer/index.tsx
+++ client/src/pages/employer/index.tsx
@@ -118,7 +118,7 @@ function Page(props: Props) {
       ...getColumnSearchProps('locations'),
       render: (locationsRaw: string) => {
         const locations = locationsRaw.split(';');
-        const locationsItems = locations.map((location, index) => <li key={index}>{location}</li>);
+        const locationsItems = locations.map(location => <li key={location}>{location}</li>);
         return <ol>{locationsItems}</ol>;
       },
     },
