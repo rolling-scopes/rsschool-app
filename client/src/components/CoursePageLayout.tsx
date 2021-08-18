1af04022e7417c19409cf657ee95d3974f0a9f24
Andrei <andrei@palchys.com>
2021-08-18 16:43:16 +0300

    refactor(score): score restructure (#837)

---
+++ client/src/components/CoursePageLayout.tsx
@@ -0,0 +1,20 @@
+import { Layout, Spin } from 'antd';
+import { Course } from 'services/models';
+import { CourseNoAccess } from './CourseNoAccess';
+import { Header } from './Header';
+
+type Props = { loading: boolean; githubId: string; course: Course; title?: string; children?: any };
+
+export function CoursePageLayout(props: Props) {
+  if (props.course == null) {
+    return <CourseNoAccess />;
+  }
+  return (
+    <Layout style={{ background: 'transparent' }}>
+      <Header title={props.title} username={props.githubId} courseName={props.course.name} />
+      <Layout.Content style={{ margin: 16 }}>
+        <Spin spinning={props.loading}>{props.children}</Spin>
+      </Layout.Content>
+    </Layout>
+  );
+}
---
+++ client/src/modules/Score/data/getColumns.tsx
@@ -0,0 +1,109 @@
+import { SettingFilled } from '@ant-design/icons';
+import { Typography } from 'antd';
+import { GithubAvatar } from 'components';
+import { dateRenderer, getColumnSearchProps } from 'components/Table';
+import { isArray } from 'lodash';
+import { StudentScore } from 'services/course';
+
+const { Text } = Typography;
+
+type Props = {
+  taskColumns: any[];
+  cityName: string | string[];
+  mentor: string | string[];
+  handleSettings: () => void;
+};
+
+export function getColumns(props: Props) {
+  const { cityName, mentor, handleSettings, taskColumns } = props;
+  return [
+    {
+      title: '#',
+      fixed: 'left',
+      dataIndex: 'rank',
+      key: 'rank',
+      width: 50,
+      sorter: 'rank',
+      render: (value: number) => (value >= 999999 ? 'New' : value),
+    },
+    {
+      title: 'Github',
+      fixed: 'left',
+      key: 'githubId',
+      dataIndex: 'githubId',
+      sorter: 'githubId',
+      width: 150,
+      render: (value: string) => (
+        <div>
+          <GithubAvatar githubId={value} size={24} />
+          &nbsp;
+          <a target="_blank" href={`https://github.com/${value}`}>
+            {value}
+          </a>
+        </div>
+      ),
+      ...getColumnSearchProps('githubId'),
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      width: 150,
+      sorter: 'name',
+      render: (value: any, record: StudentScore) => <a href={`/profile?githubId=${record.githubId}`}>{value}</a>,
+      ...getColumnSearchProps('name'),
+    },
+    {
+      title: 'Location',
+      dataIndex: 'cityName',
+      width: 150,
+      sorter: 'cityName',
+      defaultFilteredValue: cityName ? (isArray(cityName) ? cityName : [cityName]) : undefined,
+      ...getColumnSearchProps('cityName'),
+    },
+    {
+      title: 'Total',
+      dataIndex: 'totalScore',
+      width: 80,
+      sorter: 'totalScore',
+      render: (value: number) => <Text strong>{value}</Text>,
+    },
+    {
+      title: 'Cross-Check',
+      dataIndex: 'crossCheckScore',
+      width: 90,
+      sorter: 'crossCheckScore',
+      render: (value: number) => <Text strong>{value}</Text>,
+    },
+    ...taskColumns,
+    {
+      title: 'Change Date',
+      dataIndex: 'totalScoreChangeDate',
+      width: 80,
+      sorter: 'totalScoreChangeDate',
+      render: dateRenderer,
+    },
+    {
+      title: 'Last Commit Date',
+      dataIndex: 'repositoryLastActivityDate',
+      width: 80,
+      sorter: 'repositoryLastActivityDate',
+      render: dateRenderer,
+    },
+    {
+      title: 'Mentor',
+      dataIndex: ['mentor', 'githubId'],
+      width: 150,
+      sorter: 'mentor',
+      defaultFilteredValue: mentor ? (isArray(mentor) ? mentor : [mentor]) : undefined,
+      render: (value: string) => <a href={`/profile?githubId=${value}`}>{value}</a>,
+      ...getColumnSearchProps('mentor.githubId'),
+    },
+    {
+      title: () => <SettingFilled onClick={handleSettings} />,
+      fixed: 'right',
+      width: 30,
+      align: 'center',
+      render: () => '',
+    },
+  ];
+}
