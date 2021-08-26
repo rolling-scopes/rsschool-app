7bddfc11b08b3ad4d4f1d5847bfd23b9553b103d
Konstantin Dedulya <eximal@gmail.com>
2021-08-26 17:12:45 +0300

    refactor: change guard

--- server/src/routes/checks/index.ts
+++ server/src/routes/checks/index.ts
@@ -2,13 +2,13 @@ import { getBadComment } from './getBadComment';
 import { getMaxScoreCheckers } from './getMaxScoreCheckers';
 import Router from '@koa/router';
 import { ILogger } from '../../logger';
-import { adminGuard } from '../guards';
+import { courseManagerGuard } from '../guards';
 
 export function checksRoute(logger: ILogger) {
   const router = new Router<any, any>({ prefix: '/checks' });
 
-  router.get('/maxscore/:taskId', adminGuard, getMaxScoreCheckers(logger));
-  router.get('/badcomment/:taskId', adminGuard, getBadComment(logger));
+  router.get('/maxscore/:taskId', courseManagerGuard, getMaxScoreCheckers(logger));
+  router.get('/badcomment/:taskId', courseManagerGuard, getBadComment(logger));
 
   return router;
 }
