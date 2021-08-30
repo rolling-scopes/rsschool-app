3c0551963beabc246a59274c94b68183efd781d6
Valery Dluski <valerydluski@gmail.com>
2021-08-30 16:32:39 +0300

    fix: fix links in AdminSider

--- client/src/components/AdminSider.tsx
+++ client/src/components/AdminSider.tsx
@@ -12,6 +12,7 @@ import {
   UserOutlined,
 } from '@ant-design/icons';
 import { Layout, Menu } from 'antd';
+import Link from 'next/link';
 import { useState } from 'react';
 
 const { Sider } = Layout;
@@ -36,77 +37,95 @@ export function AdminSider(props: Props) {
 
       <Menu theme="dark" mode="inline">
         <Menu.Item key="1">
-          <a href="/">
-            <HomeOutlined />
-            <span>Main</span>
-          </a>
+          <Link href="/">
+            <a>
+              <HomeOutlined />
+              <span>Main</span>
+            </a>
+          </Link>
         </Menu.Item>
 
         {props.isAdmin ? (
           <Menu.Item key="2">
-            <a href="/admin/courses">
-              <GlobalOutlined />
-              <span>Courses</span>
-            </a>
+            <Link href="/admin/courses">
+              <a>
+                <GlobalOutlined />
+                <span>Courses</span>
+              </a>
+            </Link>
           </Menu.Item>
         ) : null}
 
         {props.isAdmin || props.isCoursePowerUser ? (
           <Menu.Item key="3">
-            <a href="/admin/interview-questions">
-              <QuestionOutlined />
-              <span>Interview questions</span>
-            </a>
+            <Link href="/admin/interview-questions">
+              <a>
+                <QuestionOutlined />
+                <span>Interview questions</span>
+              </a>
+            </Link>
           </Menu.Item>
         ) : null}
 
         <Menu.Item key="4">
-          <a href="/admin/tasks">
-            <AlertOutlined />
-            <span>Tasks</span>
-          </a>
+          <Link href="/admin/tasks">
+            <a>
+              <AlertOutlined />
+              <span>Tasks</span>
+            </a>
+          </Link>
         </Menu.Item>
 
         <Menu.Item key="5">
-          <a href="/admin/events">
-            <BellOutlined />
-            <span>Events</span>
-          </a>
+          <Link href="/admin/events">
+            <a>
+              <BellOutlined />
+              <span>Events</span>
+            </a>
+          </Link>
         </Menu.Item>
 
         {props.isAdmin ? (
           <Menu.Item key="6">
-            <a href="/admin/users">
-              <UserOutlined />
-              <span>Users</span>
-            </a>
+            <Link href="/admin/users">
+              <a>
+                <UserOutlined />
+                <span>Users</span>
+              </a>
+            </Link>
           </Menu.Item>
         ) : null}
 
         {props.isAdmin || props.isCoursePowerUser ? (
           <Menu.Item key="7">
-            <a href="/admin/mentor-registry">
-              <IdcardFilled />
-              <span>Mentor Registry</span>
-            </a>
+            <Link href="/admin/mentor-registry">
+              <a>
+                <IdcardFilled />
+                <span>Mentor Registry</span>
+              </a>
+            </Link>
           </Menu.Item>
         ) : null}
 
         {props.isAdmin ? (
           <Menu.Item key="8">
-            <a href="/admin/discord-server">
-              <RobotFilled />
-              <span>Discord Servers</span>
-            </a>
+            <Link href="/admin/discord-server">
+              <a>
+                <RobotFilled />
+                <span>Discord Servers</span>
+              </a>
+            </Link>
           </Menu.Item>
         ) : null}
 
         {props.isAdmin ? (
           <Menu.Item key="9">
-            <a href="/admin/user-group">
-              <TeamOutlined />
-              <span>User Groups</span>
-            </a>
+            <Link href="/admin/user-group">
+              <a>
+                <TeamOutlined />
+                <span>User Groups</span>
+              </a>
+            </Link>
           </Menu.Item>
         ) : null}
       </Menu>
