37362b5803f36c373fcf668bed0f4a0771b65b8a
Andrei <andrei@palchys.com>
2021-09-22 21:03:07 +0300

    chore: update migrations

--- server/src/migrations/index.ts
+++ server/src/migrations/index.ts
@@ -3,6 +3,7 @@ import { TaskResult1630341383942 } from './1630341383942-TaskResult';
 import { StudentMigration1630342025950 } from './1630342025950-StudentMigration';
 import { UserMigration1630342266002 } from './1630342266002-UserMigration';
 import { StudentMigration1630347897950 } from './1630347897950-StudentMigration';
+import { ResumeMigration1632333725126 } from './1632333725126-ResumeMigration';
 
 export const migrations = [
   UserMigration1630340371992,
@@ -10,4 +11,5 @@ export const migrations = [
   StudentMigration1630342025950,
   UserMigration1630342266002,
   StudentMigration1630347897950,
+  ResumeMigration1632333725126,
 ];
