0655a0a6a9e5c7ac6695c12fc2c3642ed46c7c99
Valery Dluski <valerydluski@gmail.com>
2021-09-30 13:42:37 +0300

    feat: add 404 page

---
+++ client/src/pages/404.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import withSession, { Session } from 'components/withSession';
+import { PageLayout } from 'components';
+import { Row } from 'antd';
+
+type IProps = {
+  session: Session;
+};
+
+function Page(props: IProps) {
+  return (
+    <PageLayout loading={false} githubId={props.session.githubId}>
+      <Row justify="center">
+        <Image src="/static/svg/err.svg" alt="Picture of the author" width={400} height={400} />
+      </Row>
+      <Row justify="center">
+        <h2>404</h2>
+      </Row>
+      <Row justify="center">
+        <h3>That page doesn't exist!</h3>
+      </Row>
+      <Row justify="center">
+        <p>Sorry, the page you were looking for could not be found.</p>
+      </Row>
+    </PageLayout>
+  );
+}
+
+export default withSession(Page);
