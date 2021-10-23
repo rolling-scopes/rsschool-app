48e8f02b2e8fab76f943ecbee7bdcf9629a484b8
Andrei <andrei@palchys.com>
2021-10-23 16:32:40 +0300

    feat: add cloudflare analytics (#988)

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
-const enableGA = process.env.NODE_ENV === 'production';
+const enableAnalytics = process.env.NODE_ENV === 'production';
 
 export default class extends Document {
   render() {
@@ -10,8 +10,15 @@ export default class extends Document {
         <Head>
           <link rel="shortcut icon" href="https://rs.school/favicon.ico" />
 
-          {enableGA && <script async src="https://www.googletagmanager.com/gtag/js?id=UA-55428637-3" />}
-          {enableGA && <script dangerouslySetInnerHTML={{ __html: gaJsCode }} />}
+          {enableAnalytics && <script async src="https://www.googletagmanager.com/gtag/js?id=UA-55428637-3" />}
+          {enableAnalytics && <script dangerouslySetInnerHTML={{ __html: gaJsCode }} />}
+          {enableAnalytics && (
+            <script
+              defer
+              src="https://static.cloudflareinsights.com/beacon.min.js"
+              data-cf-beacon={'{"token": "e607238d732c4713b01b851ed3df61c2"}'}
+            />
+          )}
         </Head>
         <body>
           <Main />
