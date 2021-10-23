96c5f8339a2e0132dbbb910e3161662e033e1b20
Andrei <andrei@palchys.com>
2021-10-23 23:06:19 +0300

    fix(nestjs): port value

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.NODE_PORT || 3002;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
