d26f785d0b3f87e22d7a63a4bf5d6cdfac3d678f
Andrei <andrei@palchys.com>
2021-10-23 15:52:49 +0300

    chore: update @octokit packages (#985)

--- server/src/services/github.service.ts
+++ server/src/services/github.service.ts
@@ -3,14 +3,12 @@ import { Octokit } from '@octokit/rest';
 import { App } from '@octokit/app';
 
 const { appId, privateKey } = config.github;
-const app = new App({ id: Number(appId), privateKey });
+const app = new App({ appId: Number(appId), privateKey });
 
 export class GithubService {
   public static async initGithub(): Promise<Octokit> {
     const { installationId } = config.github;
-    const installationAccessToken = await app.getInstallationAccessToken({
-      installationId: Number(installationId),
-    });
+    const installationAccessToken = await app.getInstallationOctokit(Number(installationId));
     return new Octokit({ auth: `token ${installationAccessToken}` });
   }
 }
