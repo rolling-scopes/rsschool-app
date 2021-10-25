842c79706ee4496017f7668b0c295e6cf123cdcb
Andrei <andrei@palchys.com>
2021-10-25 20:59:52 +0300

    chore: remove unused files, code (#1010)

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const webpack = require('webpack');
 const isProd = process.env.NODE_ENV === 'production';
 
@@ -26,13 +25,7 @@ const nextConfig = {
     APP_VERSION: process.env.APP_VERSION,
     RSSHCOOL_UI_GCP_MAPS_API_KEY: process.env.RSSHCOOL_UI_GCP_MAPS_API_KEY,
   },
-  // assetPrefix: isProd ? 'https://cdn.rs.school' : '',
   webpack: config => {
-    config.resolve.alias['configs'] = path.join(__dirname, 'src/configs');
-    config.resolve.alias['components'] = path.join(__dirname, 'src/components');
-    config.resolve.alias['services'] = path.join(__dirname, 'src/services');
-    config.resolve.alias['utils'] = path.join(__dirname, 'src/utils');
-    config.resolve.alias['domain'] = path.join(__dirname, 'src/domain');
     config.plugins.push(new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en-gb/));
     return config;
   },
