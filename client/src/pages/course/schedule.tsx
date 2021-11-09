e86e4a496ca04d3d095a0910f4cb4f8ea74f7380
Andrei <andrei@palchys.com>
2021-11-09 09:50:53 +0200

    fix displaying score

--- client/src/pages/course/schedule.tsx
+++ client/src/pages/course/schedule.tsx
@@ -287,7 +287,7 @@ const createCourseEventFromTask = (task: CourseTaskDetails, type: string): Cours
     isTask: true,
     special: task.special,
     duration: task.duration,
-    score: task.score ? `${task.score}/${task.maxScore}` : '',
+    score: task.score != null ? `${task.score}/${task.maxScore}` : '',
     done: task.score && task.maxScore ? Math.round((task.score / task.maxScore) * 100) : 0,
   } as CourseEvent;
 };
