ece50da0c4c32c32fe6b5915bbe590e8cc81c104
Andrei <andrei@palchys.com>
2021-11-22 16:28:08 +0200

    perf: add indices for StageInterview

--- server/src/migrations/index.ts
+++ server/src/migrations/index.ts
@@ -5,6 +5,7 @@ import { UserMigration1630342266002 } from './1630342266002-UserMigration';
 import { StudentMigration1630347897950 } from './1630347897950-StudentMigration';
 import { ResumeMigration1632333725126 } from './1632333725126-ResumeMigration';
 import { User1635365797478 } from './1635365797478-User';
+import { StageInterview1637591194886 } from './1637591194886-StageInterview';
 
 export const migrations = [
   UserMigration1630340371992,
@@ -14,4 +15,5 @@ export const migrations = [
   StudentMigration1630347897950,
   ResumeMigration1632333725126,
   User1635365797478,
+  StageInterview1637591194886,
 ];
