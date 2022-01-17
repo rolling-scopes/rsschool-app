b760d80df46e6bd6d3a5ea6ce034b4fe9afc36ed
Andrei <andrei@palchys.com>
2022-01-17 10:36:17 +0200

    fix: base url

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -13,7 +13,15 @@ export function getServerAxiosProps(token?: string, baseUrl = ''): Partial<Axios
   };
 }
 
+function getNestJsServerAxiosProps(token?: string): Partial<AxiosRequestConfig> {
+  const { rsHost } = serverRuntimeConfig;
+  return {
+    baseURL: rsHost ? serverRuntimeConfig.rsHost + BASE_PATH : undefined,
+    headers: token ? { Authorization: `Bearer ${token}` } : undefined,
+  };
+}
+
 export function getApiConfiguration(token?: string): Configuration {
-  const props = getServerAxiosProps(token, BASE_PATH);
+  const props = getNestJsServerAxiosProps(token);
   return new Configuration({ basePath: props.baseURL, baseOptions: props });
 }
