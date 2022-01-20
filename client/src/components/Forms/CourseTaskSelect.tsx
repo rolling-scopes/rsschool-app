984430b4b2fcd67bae471afeb299f4a6dd1ff5b7
vlutskevich <vlad01309@mail.ru>
2022-01-20 20:52:53 +0300

    feat: add groups for select options

--- client/src/components/Forms/CourseTaskSelect.tsx
+++ client/src/components/Forms/CourseTaskSelect.tsx
@@ -3,22 +3,34 @@ import { Select, Form } from 'antd';
 import { CourseTask } from 'services/course';
 
 type Props = { data: CourseTask[]; onChange?: (id: number) => void };
-const INFINITYDATE = '2060-01-01T21:00:00.000Z';
 
 export function CourseTaskSelect(props: Props) {
   const { data, onChange, ...rest } = props;
   data.sort(function (a, b) {
-    return Date.parse(b.studentEndDate ?? INFINITYDATE) - Date.parse(a.studentEndDate ?? INFINITYDATE);
+    return Date.parse(b.studentEndDate ?? '') - Date.parse(a.studentEndDate ?? '');
   });
   const selectProps = onChange ? { onChange } : {};
   return (
     <Form.Item {...rest} name="courseTaskId" label="Task" rules={[{ required: true, message: 'Please select a task' }]}>
       <Select placeholder="Select task" {...selectProps}>
-        {data.map(task => (
-          <Select.Option key={task.id} value={task.id}>
-            {task.name}
-          </Select.Option>
-        ))}
+        <Select.OptGroup label="Active">
+          {data
+            .filter(task => Date.now() < Date.parse(task.studentEndDate ?? ''))
+            .map(task => (
+              <Select.Option key={task.id} value={task.id}>
+                {task.name}
+              </Select.Option>
+            ))}
+        </Select.OptGroup>
+        <Select.OptGroup label="Expired">
+          {data
+            .filter(task => Date.now() >= Date.parse(task.studentEndDate ?? ''))
+            .map(task => (
+              <Select.Option key={task.id} value={task.id}>
+                {task.name}
+              </Select.Option>
+            ))}
+        </Select.OptGroup>
       </Select>
     </Form.Item>
   );
