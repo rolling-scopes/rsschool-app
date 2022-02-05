1bf66f634626944de902b6bd6c6c7a30c83e3afa
artsiom aliakseyenka <alekseenkoart@gmail.com>
2022-02-05 16:40:47 +0100

    fix review: add admin role guard

--- nestjs/src/notifications/notifications.controller.ts
+++ nestjs/src/notifications/notifications.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { ApiForbiddenResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { DefaultGuard, RoleGuard } from 'src/auth';
+import { DefaultGuard, RequiredAppRoles, Role, RoleGuard } from 'src/auth';
 import { NotificationDto } from './dto/notification.dto';
 import { UpdateNotificationDto } from './dto/update-notification.dto';
 
@@ -15,6 +15,7 @@ export class NotificationsController {
   @Get('/')
   @ApiOperation({ operationId: 'getNotifications' })
   @ApiForbiddenResponse()
+  @RequiredAppRoles([Role.Admin])
   @ApiOkResponse({ type: [NotificationDto] })
   public async getNotifications() {
     const notifications = await this.notificationsService.getNotifications();
@@ -25,6 +26,7 @@ export class NotificationsController {
   @ApiOperation({ operationId: 'updateNotification' })
   @ApiOkResponse({ type: NotificationDto })
   @ApiForbiddenResponse()
+  @RequiredAppRoles([Role.Admin])
   public async updateNotification(@Body() dto: UpdateNotificationDto) {
     const notification = await this.notificationsService.saveNotification(dto);
     return new NotificationDto(notification);
@@ -38,10 +40,12 @@ export class NotificationsController {
     const notification = await this.notificationsService.createNotification(dto);
     return new NotificationDto(notification);
   }
+
   @Delete('/:id')
   @ApiOperation({ operationId: 'deleteNotification' })
   @ApiOkResponse()
   @ApiForbiddenResponse()
+  @RequiredAppRoles([Role.Admin])
   public async deleteNotification(@Param('id') id: string) {
     await this.notificationsService.deleteNotification(id);
   }
