6063e2feadabfcafb5c8fd1615bdf48fff68994b
Andrei <andrei@palchys.com>
2022-02-20 15:50:16 +0200

    fix: remove obsolete page (#1229)
    * fix: remove obsolete page
    
    * feat: add user id for repository events

--- server/src/repositories/repositoryEvent.repository.ts
+++ server/src/repositories/repositoryEvent.repository.ts
@@ -1,9 +1,26 @@
 import { AbstractRepository, EntityRepository, getRepository } from 'typeorm';
-import { RepositoryEvent } from '../models';
+import { RepositoryEvent, User } from '../models';
 
 @EntityRepository(RepositoryEvent)
 export class RepositoryEventRepository extends AbstractRepository<RepositoryEvent> {
   public async save(events: Pick<RepositoryEvent, 'repositoryUrl' | 'action' | 'githubId'>[]): Promise<void> {
-    await getRepository(RepositoryEvent).save(events);
+    const repository = getRepository(RepositoryEvent);
+    const userRepository = getRepository(User);
+    const cache = new Map<string, number>();
+
+    for (const event of events) {
+      let userId: number | undefined;
+      if (cache.has(event.githubId)) {
+        userId = cache.get(event.githubId);
+      } else {
+        const [user] = await userRepository.find({
+          select: ['id', 'githubId'],
+          where: { githubId: event.githubId },
+        });
+        userId = user?.id;
+        cache.set(event.githubId, userId);
+      }
+      await repository.save({ ...event, userId });
+    }
   }
 }
