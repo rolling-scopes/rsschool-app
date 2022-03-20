801ec8a903ad84e956cd43966797f88c1a208fd5
Maksim Shylau <shilaums@gmail.com>
2022-03-21 00:39:52 +0100

    chore: fix codestyle

--- server/src/migrations/1647103154082-CrossCheckScheduling.ts
+++ server/src/migrations/1647103154082-CrossCheckScheduling.ts
@@ -1,16 +1,17 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CrossCheckScheduling1647103154082 implements MigrationInterface {
-    name = 'CrossCheckScheduling1647103154082'
+  name = 'CrossCheckScheduling1647103154082';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "course_task" ADD "crossCheckEndDate" TIMESTAMP WITH TIME ZONE`);
-        await queryRunner.query(`ALTER TABLE "course_task" ADD "crossCheckState" character varying NOT NULL DEFAULT 'initial'`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "course_task" ADD "crossCheckEndDate" TIMESTAMP WITH TIME ZONE`);
+    await queryRunner.query(
+      `ALTER TABLE "course_task" ADD "crossCheckState" character varying NOT NULL DEFAULT 'initial'`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "course_task" DROP COLUMN "crossCheckState"`);
-        await queryRunner.query(`ALTER TABLE "course_task" DROP COLUMN "crossCheckEndDate"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "course_task" DROP COLUMN "crossCheckState"`);
+    await queryRunner.query(`ALTER TABLE "course_task" DROP COLUMN "crossCheckEndDate"`);
+  }
 }
