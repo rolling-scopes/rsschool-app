317f0eb7056c33b40dda6cdca976c68c30fe5e10
Artsiom Aliakseyenka <alekseenkoart@gmail.com>
2022-03-25 16:00:17 +0100

    chore: migrate profile update to nestjs (#1327)
    * chore: migrate profile update to nestjs

--- nestjs/src/profile/profile.controller.ts
+++ nestjs/src/profile/profile.controller.ts
@@ -1,18 +1,19 @@
-import { Controller, ForbiddenException, Get, Param, Req, UseGuards } from '@nestjs/common';
-import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, ForbiddenException, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { ApiBody, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { DefaultGuard } from 'src/auth';
 import { CoursesService } from 'src/courses/courses.service';
 import { CurrentRequest } from '../auth/auth.service';
 import { ProfileCourseDto } from './dto';
+import { ProfileInfoDto } from './dto/update-profile.dto';
 import { ProfileService } from './profile.service';
 
 @Controller('profile')
 @ApiTags('profile')
+@UseGuards(DefaultGuard)
 export class ProfileController {
   constructor(private readonly profileService: ProfileService, private readonly coursesService: CoursesService) {}
 
   @Get(':username/courses')
-  @UseGuards(DefaultGuard)
   @ApiOperation({ operationId: 'getUserCourses' })
   @ApiOkResponse({ type: [ProfileCourseDto] })
   public async getCourses(
@@ -29,5 +30,16 @@ export class ProfileController {
     }
     const data = await this.profileService.getCourses(user);
     return data.map(course => new ProfileCourseDto(course));
+  }
+
+  @Post('/info')
+  @ApiOperation({ operationId: 'updateProfileInfo' })
+  @ApiBody({ type: ProfileInfoDto })
+  public async updateProfileInfo(@Req() req: CurrentRequest, @Body() dto: ProfileInfoDto) {
+    const {
+      user: { id },
+    } = req;
+
+    await this.profileService.updateProfile(id, dto);
   }
 }
