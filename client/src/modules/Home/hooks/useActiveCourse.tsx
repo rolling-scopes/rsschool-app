b914acf0f27b4eea558b2d8bf86f5375462efcd0
Oleg Vaskevich <oleg.vaskevich.it@gmail.com>
2022-04-15 19:35:36 +0300

    refactor: move to useLocalStorage

--- client/src/modules/Home/hooks/useActiveCourse.tsx
+++ client/src/modules/Home/hooks/useActiveCourse.tsx
@@ -1,23 +1,17 @@
 import { useState } from 'react';
+import { useLocalStorage } from 'react-use';
 import { Course } from 'services/models';
 
 export function useActiveCourse(courses: Course[]): [Course | null, (courseId: number) => void] {
   const [courseId, setCourseId] = useState<number>();
-  const activeCourseId = courseId || Number(loadActiveCourseId());
+  const [storageValue, setStorageValue] = useLocalStorage('activeCourseId');
+  const activeCourseId = courseId || Number(storageValue);
   const course = courses.find(course => course.id === activeCourseId) ?? courses[0];
   return [
     course,
     (courseId: number) => {
       setCourseId(courseId);
-      saveActiveCouseId(courseId);
+      setStorageValue(courseId);
     },
   ];
-}
-
-function loadActiveCourseId() {
-  return localStorage.getItem('activeCourseId');
-}
-
-function saveActiveCouseId(courseId: number) {
-  localStorage.setItem('activeCourseId', courseId as any);
 }
