e9a09bd4b351aeb6240bcf192cef3c6720f54e07
artsiom aliakseyenka <alekseenkoart@gmail.com>
2022-04-26 21:57:34 +0200

    temporary disable discord on UI

--- client/src/modules/Notifications/components/Consents.tsx
+++ client/src/modules/Notifications/components/Consents.tsx
@@ -23,7 +23,7 @@ export function Consents({
 }) {
   const hasEmail = !!email?.enabled;
   const hasTelegram = !!telegram?.enabled;
-  const hasDiscord = !!discord?.enabled;
+  const hasDiscord = !!discord?.enabled || true;
 
   if (hasEmail && hasTelegram && hasDiscord) return null;
 
