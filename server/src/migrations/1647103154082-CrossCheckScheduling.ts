f3dccb81852330477aa475b46d425b1d32823154
Maksim Shylau <shilaums@gmail.com>
2022-05-14 15:21:38 +0200

    feat:  set initial state in migration

--- server/src/migrations/1647103154082-CrossCheckScheduling.ts
+++ server/src/migrations/1647103154082-CrossCheckScheduling.ts
@@ -8,6 +8,28 @@ export class CrossCheckScheduling1647103154082 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "course_task" ADD "crossCheckStatus" character varying NOT NULL DEFAULT 'initial'`,
     );
+    await queryRunner.query(`
+      UPDATE course_task AS c
+      SET "crossCheckStatus" = 'completed'
+      FROM task_result t
+      WHERE c.id = t."courseTaskId"
+      AND c.checker = 'crossCheck'
+      AND (c."studentEndDate" < CURRENT_DATE OR c."studentEndDate" IS NULL)
+    `);
+    await queryRunner.query(`
+      UPDATE course_task AS ct
+      SET "crossCheckStatus" = 'distributed'
+      FROM (
+        SELECT c.id
+        FROM course_task AS c
+        LEFT JOIN task_result AS t ON t."courseTaskId" = c.id
+        WHERE c.checker = 'crossCheck'
+        AND t.score IS NULL
+        AND c."studentEndDate" < CURRENT_DATE
+        GROUP BY c.id
+      ) tt
+      WHERE ct.id = tt.id
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
