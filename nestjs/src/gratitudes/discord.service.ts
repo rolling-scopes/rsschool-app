306e561f7bb94aba2aa0ad035ec37019e86ad125
Seid Akhmed Agitaev <a.agitaev@gmail.com>
2022-05-17 02:39:59 +0500

    fix: handle discord rate limiter

--- nestjs/src/gratitudes/discord.service.ts
+++ nestjs/src/gratitudes/discord.service.ts
@@ -1,6 +1,6 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
-import { lastValueFrom } from 'rxjs';
+import { map, retry } from 'rxjs';
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const isProd = process.env.NODE_ENV === 'production';
@@ -33,10 +33,38 @@ export class DiscordService {
       username: params.fromGithubId,
       content: `${mention}\n${params.comment}`,
     };
+
     if (!isProd) {
       this.logger.log(`Skip sending discord message in develoment: ${JSON.stringify(message)}`);
       return;
     }
-    await lastValueFrom(this.httpService.post(params.gratitudeUrl, message));
+
+    this.httpService
+      .post(params.gratitudeUrl, message)
+      .pipe(
+        map(response => response.data),
+        retry({
+          count: 5,
+          delay: 1000,
+        }),
+      )
+      .subscribe({
+        next: () =>
+          this.logger.log({
+            msg: 'Sending gratitude',
+            mention: params.toDiscordId || params.toGithubId,
+          }),
+        error: e =>
+          this.logger.error({
+            msg: 'Error sending gratitude',
+            mention: params.toDiscordId || params.toGithubId,
+            error: e.toString(),
+          }),
+        complete: () =>
+          this.logger.log({
+            msg: 'Gratitude sent successfully',
+            mention: params.toDiscordId || params.toGithubId,
+          }),
+      });
   }
 }
