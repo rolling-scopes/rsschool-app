a70984182baf6c8c84a70d84c7cd01a26b24dd95
artsiom aliakseyenka <alekseenkoart@gmail.com>
2022-05-17 14:45:39 +0200

    feat(notifications): turn on notifications by default

--- client/src/modules/Notifications/components/NotificationsUserSettingsTable.tsx
+++ client/src/modules/Notifications/components/NotificationsUserSettingsTable.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useMemo } from 'react';
+import { useMemo } from 'react';
 import css from 'styled-jsx/css';
 import { Table } from 'antd';
 import { ColumnType } from 'antd/lib/table';
@@ -12,7 +12,7 @@ export function NotificationsTable({
 }: {
   notifications: UserNotificationSettings[];
   disabledChannels?: NotificationChannel[];
-  onCheck: (dataIndex: string[], record: UserNotificationSettings, event: ChangeEvent<HTMLInputElement>) => void;
+  onCheck: (dataIndex: string[], record: UserNotificationSettings, checked: boolean) => void;
 }) {
   const columns = useMemo(() => buildColumns(onCheck, disabledChannels), [onCheck, disabledChannels]);
 
@@ -49,7 +49,7 @@ const { className: disabledClassName, styles: disabledStyles } = css.resolve`
 `;
 
 function buildColumns(
-  onCheck: (dataIndex: string[], record: UserNotificationSettings, event: ChangeEvent<HTMLInputElement>) => void,
+  onCheck: (dataIndex: string[], record: UserNotificationSettings, checked: boolean) => void,
   disabledChannels: NotificationChannel[] = [],
 ) {
   const columns: ColumnType<UserNotificationSettings>[] = [
@@ -69,7 +69,7 @@ function buildColumns(
         }`,
         title: channel,
         dataIndex,
-        render: buildCheckBoxRenderer<UserNotificationSettings>(dataIndex, onCheck),
+        render: buildCheckBoxRenderer<UserNotificationSettings>(dataIndex, onCheck, true),
       };
     }),
   );
