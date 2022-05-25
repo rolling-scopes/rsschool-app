a328d1ade5b60866043a7fe696912f6a913e9a85
Valery <valerydluski@gmail.com>
2022-05-25 13:59:13 +0200

    fix: rename icon

--- client/src/components/SolidarityUkraine.tsx
+++ client/src/components/SolidarityUkraine.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
 
 export function SolidarityUkraine() {
-  return <Image src="/static/svg/solidarityUkraine.svg" alt="Stand With Ukraine" width={146} height={32} />;
+  return <Image src="/static/svg/solidarity-Ukraine.svg" alt="Stand With Ukraine" width={146} height={32} />;
 }
