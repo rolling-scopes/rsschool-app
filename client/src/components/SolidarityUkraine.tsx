583b5b32dd05b6781e877ec2585122459871189e
Valery <57412523+valerydluski@users.noreply.github.com>
2022-05-25 08:32:35 +0200

    fix: update solidarityUkraine svg (#1445)

--- client/src/components/SolidarityUkraine.tsx
+++ client/src/components/SolidarityUkraine.tsx
@@ -1,21 +1,5 @@
-import { Space, Typography } from 'antd';
 import Image from 'next/image';
 
-const { Text } = Typography;
-
 export function SolidarityUkraine() {
-  return (
-    <Space>
-      <Image src="/static/svg/solidarityUkraine.svg" alt="Stand With Ukraine" width={60} height={38} />
-      <div>
-        <Text type="secondary" style={{ fontSize: 12 }}>
-          #StandWithUkraine
-        </Text>
-        <br />
-        <Text type="secondary" style={{ fontSize: 12 }}>
-          #StopWar
-        </Text>
-      </div>
-    </Space>
-  );
+  return <Image src="/static/svg/solidarityUkraine.svg" alt="Stand With Ukraine" width={146} height={32} />;
 }
