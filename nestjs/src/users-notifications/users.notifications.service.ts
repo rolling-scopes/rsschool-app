f511227e9a56ef7861905308cd38d88314f167d1
artsiom aliakseyenka <alekseenkoart@gmail.com>
2022-06-10 12:08:21 +0200

    fix: disable discord. its gone :rotating_light:

--- nestjs/src/users-notifications/users.notifications.service.ts
+++ nestjs/src/users-notifications/users.notifications.service.ts
@@ -194,6 +194,7 @@ export class UserNotificationsService {
 
     const channelMap = new Map<NotificationChannelId, NotificationData>();
     channels.forEach(channel => {
+      if (channel.channelId === 'discord') return;
       const message = this.notificationsService.buildChannelMessage(channel, data);
       if (message) {
         const { channelId, template, to } = message;
