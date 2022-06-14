1e729a4151c41909a68c3a360bcf2d66d7dee0cf
Oleg Vaskevich <oleg.vaskevich.it@gmail.com>
2022-06-14 21:41:14 +0300

    refactor: set proper types (#1481)
    Co-authored-by: Andrei <andrei@palchys.com>

--- client/src/modules/Score/data/getTaskColumns.tsx
+++ client/src/modules/Score/data/getTaskColumns.tsx
@@ -1,47 +1,50 @@
-import { QuestionCircleOutlined } from '@ant-design/icons';
 import { Popover } from 'antd';
+import { QuestionCircleOutlined } from '@ant-design/icons';
 import { CourseTaskDto } from 'api';
 import { dateTimeRenderer } from 'components/Table';
-import { IColumn, StudentScore } from 'services/course';
+import { ColumnTypeWithName, StudentScore } from 'services/course';
 
-export function getTaskColumns(courseTasks: CourseTaskDto[]): IColumn[] {
-  const columns = courseTasks.map(courseTask => ({
-    dataIndex: courseTask.id.toString(),
-    title: () => {
-      const icon = (
-        <Popover
-          content={
-            <ul>
-              <li>Coefficient: {courseTask.scoreWeight}</li>
-              <li>Deadline: {dateTimeRenderer(courseTask.studentEndDate)}</li>
-              <li>Max. score: {courseTask.maxScore}</li>
-            </ul>
-          }
-          trigger="click"
-        >
-          <QuestionCircleOutlined title="Click for details" />
-        </Popover>
-      );
-      return courseTask.descriptionUrl ? (
-        <>
-          <a className="table-header-link" target="_blank" href={courseTask.descriptionUrl}>
-            {courseTask.name}
-          </a>{' '}
-          {icon}
-        </>
-      ) : (
-        <div>
-          {courseTask.name} {icon}
-        </div>
-      );
-    },
-    width: 100,
-    className: 'align-right',
-    render: (_: any, d: StudentScore) => {
-      const currentTask = d.taskResults.find(taskResult => taskResult.courseTaskId === courseTask.id);
-      return currentTask ? <div>{currentTask.score}</div> : 0;
-    },
-    name: courseTask.name,
-  }));
+export function getTaskColumns(courseTasks: CourseTaskDto[]) {
+  const columns = courseTasks.map(courseTask => {
+    const column: ColumnTypeWithName<StudentScore> = {
+      dataIndex: courseTask.id.toString(),
+      title: () => {
+        const icon = (
+          <Popover
+            content={
+              <ul>
+                <li>Coefficient: {courseTask.scoreWeight}</li>
+                <li>Deadline: {dateTimeRenderer(courseTask.studentEndDate)}</li>
+                <li>Max. score: {courseTask.maxScore}</li>
+              </ul>
+            }
+            trigger="click"
+          >
+            <QuestionCircleOutlined title="Click for details" />
+          </Popover>
+        );
+        return courseTask.descriptionUrl ? (
+          <>
+            <a className="table-header-link" target="_blank" href={courseTask.descriptionUrl}>
+              {courseTask.name}
+            </a>{' '}
+            {icon}
+          </>
+        ) : (
+          <div>
+            {courseTask.name} {icon}
+          </div>
+        );
+      },
+      width: 100,
+      className: 'align-right',
+      render: (_, d) => {
+        const currentTask = d.taskResults.find(taskResult => taskResult.courseTaskId === courseTask.id);
+        return currentTask ? <div>{currentTask.score}</div> : 0;
+      },
+      name: courseTask.name,
+    };
+    return column;
+  });
   return columns;
 }
