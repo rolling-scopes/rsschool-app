6293e964b641142c8dc68f6ccb4ad4ced84c6527
artsiom aliakseyenka <alekseenkoart@gmail.com>
2022-06-30 09:55:17 +0200

    feat(notifications): add notification template helpers

--- nestjs/src/core/templates/index.ts
+++ nestjs/src/core/templates/index.ts
@@ -1,4 +1,8 @@
 import handlebars from 'handlebars';
+import * as dayjs from 'dayjs';
+import * as utc from 'dayjs/plugin/utc';
+
+dayjs.extend(utc);
 
 handlebars.registerHelper('truncate', (string: string, options: { maxLength: number }) => {
   const { maxLength = 50 } = options || {};
@@ -6,4 +10,19 @@ handlebars.registerHelper('truncate', (string: string, options: { maxLength: num
     return `${string.slice(0, maxLength)}...`;
   }
   return string;
+});
+
+handlebars.registerHelper('capitalize', (string: string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+});
+
+handlebars.registerHelper('ifEquals', function (arg1, arg2, options) {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  return arg1 == arg2 ? options.fn(this) : options.inverse(this);
+});
+
+handlebars.registerHelper('formatDateTime', (dateTime: string) => {
+  if (!dateTime) return '';
+  return dayjs.utc(dateTime).format('YYYY-MM-DD HH:mm');
 });
