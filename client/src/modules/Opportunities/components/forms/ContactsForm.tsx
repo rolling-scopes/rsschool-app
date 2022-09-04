d33deea4b53d26c30cae3a1d7cce397fec6392e0
Maksim Shylau <shilaums@gmail.com>
2022-09-04 15:06:19 +0200

    refactor: update forms

--- client/src/modules/Opportunities/components/forms/ContactsForm.tsx
+++ client/src/modules/Opportunities/components/forms/ContactsForm.tsx
@@ -1,10 +1,11 @@
 import { forwardRef, useEffect, ForwardedRef } from 'react';
-import { Form, Input, Card, FormInstance } from 'antd';
+import { Form, Input, Card, FormInstance, Typography } from 'antd';
 import { Contacts } from 'modules/Opportunities/models';
 import { contactsValidationRules as validationRules } from './form-validation';
 
 const { Item } = Form;
 const { TextArea } = Input;
+const { Text } = Typography;
 
 type Props = {
   contactsList: Contacts;
@@ -20,95 +21,47 @@ const ContactsForm = forwardRef((props: Props, ref: ForwardedRef<FormInstance>) 
     form.validateFields();
   }, [contactsList]);
 
-  const itemStyle = {
-    maxWidth: '310px',
+  const inputStyle = {
+    maxWidth: '400px',
   };
 
   return (
-    <Card title="Contacts">
-      <Form form={form} ref={ref} name="contacts">
-        <Item
-          style={itemStyle}
-          label="Phone (international format)"
-          wrapperCol={{ span: 24 }}
-          labelCol={{ span: 24 }}
-          name="phone"
-          rules={[...validationRules['phone']]}
-        >
-          <Input placeholder="+12025550111" />
+    <Card title={<Text strong>Contacts</Text>} style={{ width: '70vw' }}>
+      <Form
+        form={form}
+        ref={ref}
+        name="contacts"
+        labelCol={{ span: 9 }}
+        wrapperCol={{ span: 10 }}
+        style={{ width: '100%' }}
+      >
+        <Item label="Phone (international format)" name="phone" rules={[...validationRules['phone']]}>
+          <Input style={inputStyle} placeholder="+12025550111" />
         </Item>
-        <Item
-          style={itemStyle}
-          label="Email"
-          wrapperCol={{ span: 24 }}
-          labelCol={{ span: 24 }}
-          name="email"
-          rules={[...validationRules['email']]}
-        >
-          <Input placeholder="Email" />
+        <Item label="Email" name="email" rules={[...validationRules['email']]}>
+          <Input style={inputStyle} placeholder="Email" />
         </Item>
-        <Item
-          style={itemStyle}
-          label="Skype id"
-          wrapperCol={{ span: 24 }}
-          labelCol={{ span: 24 }}
-          name="skype"
-          rules={[...validationRules['skype']]}
-        >
-          <Input placeholder="Skype id" />
+        <Item label="Skype id" name="skype" rules={[...validationRules['skype']]}>
+          <Input style={inputStyle} placeholder="Skype id" />
         </Item>
-        <Item
-          style={itemStyle}
-          label="Telegram public name"
-          wrapperCol={{ span: 24 }}
-          labelCol={{ span: 24 }}
-          name="telegram"
-          rules={[...validationRules['telegram']]}
-        >
-          <Input placeholder="Telegram public name" />
+        <Item label="Telegram public name" name="telegram" rules={[...validationRules['telegram']]}>
+          <Input style={inputStyle} placeholder="Telegram public name" />
         </Item>
-        <Item
-          style={itemStyle}
-          label="LinkedIn Profile"
-          wrapperCol={{ span: 24 }}
-          labelCol={{ span: 24 }}
-          name="linkedin"
-          rules={[...validationRules['linkedin']]}
-        >
-          <Input placeholder="LinkedIn username" />
+        <Item label="LinkedIn Profile" name="linkedin" rules={[...validationRules['linkedin']]}>
+          <Input style={inputStyle} placeholder="LinkedIn username" />
         </Item>
-        <Item
-          style={itemStyle}
-          label="Locations (each from new line)"
-          wrapperCol={{ span: 24 }}
-          labelCol={{ span: 24 }}
-          name="locations"
-          rules={[...validationRules['locations']]}
-        >
+        <Item label="Locations (each from new line)" name="locations" rules={[...validationRules['locations']]}>
           <TextArea
+            style={inputStyle}
             rows={4}
             placeholder="The locations in which you want to work (maximum 3, separated by semicolons)"
           />
         </Item>
-        <Item
-          style={itemStyle}
-          label="Github username"
-          wrapperCol={{ span: 24 }}
-          labelCol={{ span: 24 }}
-          name="github"
-          rules={[...validationRules['github']]}
-        >
-          <Input placeholder="Github username" />
+        <Item label="Github username" name="github" rules={[...validationRules['github']]}>
+          <Input style={inputStyle} placeholder="Github username" />
         </Item>
-        <Item
-          style={itemStyle}
-          label="Website"
-          wrapperCol={{ span: 24 }}
-          labelCol={{ span: 24 }}
-          name="website"
-          rules={[...validationRules['website']]}
-        >
-          <Input placeholder="For example, a link to a portfolio or something like that" />
+        <Item label="Website" name="website" rules={[...validationRules['website']]}>
+          <Input style={inputStyle} placeholder="For example, a link to a portfolio or something like that" />
         </Item>
       </Form>
     </Card>
