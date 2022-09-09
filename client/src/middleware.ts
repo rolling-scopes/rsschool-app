b5d9742bdb010ca52e00e17de6240d66371b1b57
Andrei <andrei@palchys.com>
2022-09-09 10:39:19 +0300

    fix: update

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,13 +1,12 @@
-// import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   // eslint-disable-next-line no-console
   console.info('Middleware', request.nextUrl);
-  // if (request.nextUrl.pathname.includes('/course/score')) {
-  //   return NextResponse.rewrite(new URL(`/course/schedule${request.nextUrl.search}`, request.nextUrl.origin));
-  // }
+  if (request.nextUrl.pathname.includes('/course/score')) {
+    return NextResponse.rewrite(new URL(`/course/schedule${request.nextUrl.search}`, request.nextUrl.origin));
+  }
 }
 
 export const config = {
