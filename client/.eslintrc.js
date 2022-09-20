2d80df605da6b9433f11dbae665a40a0df8f9955
Natalia Gulko <Natalia_Gulko1@epam.com>
2022-09-20 11:32:10 +0300

    refactor(tests): add eslint for testing-library and jest-dom

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'testing-library'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@next/next/recommended',
+    'plugin:jest-dom/recommended',
+    'plugin:testing-library/dom',
   ],
   env: {
     browser: true,
@@ -19,4 +21,11 @@ module.exports = {
     'no-console': ['error', { allow: ['warn', 'error'] }],
     '@next/next/no-img-element': 0,
   },
+  overrides: [
+    {
+      // Enable eslint-plugin-testing-library rules or preset only for matching testing files
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
 };
