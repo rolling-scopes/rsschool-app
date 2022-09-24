7f31c9f99603a8db59f606203a8ea946a87d8437
Natalia Gulko <Natalia_Gulko1@epam.com>
2022-09-24 13:06:20 +0300

    fix(filtered-tags): update margin

--- client/src/modules/Schedule/components/FilteredTags/FilteredTags.tsx
+++ client/src/modules/Schedule/components/FilteredTags/FilteredTags.tsx
@@ -11,7 +11,7 @@ type FilteredTagsProps = {
 export const FilteredTags = ({ tagFilter, onTagClose }: FilteredTagsProps) =>
   tagFilter?.length > 0 ? (
     <div style={{ marginBottom: 16 }}>
-      <FilterFilled style={{ color: 'rgba(0, 0, 0, 0.25)', marginRight: 10 }} />
+      <FilterFilled style={{ color: 'rgba(0, 0, 0, 0.25)', marginRight: 8 }} />
       {tagFilter.map(tag => (
         <Tag key={tag} closable onClose={() => onTagClose(tag)}>{`Tag: ${
           TAG_NAME_MAP[tag as CourseScheduleItemDto['tag']] || tag
