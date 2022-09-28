8bbe18ef39c2833a32ebfcf0d65b5f40b6e9fefb
Maksim Shylau <shilaums@gmail.com>
2022-09-28 17:54:01 +0200

    test: migrate from table tests

--- client/src/modules/Opportunities/components/EditCv/ContactsForm/index.test.tsx
+++ client/src/modules/Opportunities/components/EditCv/ContactsForm/index.test.tsx
@@ -1,4 +1,4 @@
-import { cleanup, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { ContactsForm } from './index';
 
 const mockContactsList = {
@@ -12,50 +12,63 @@ const mockContactsList = {
 };
 
 describe('ContactsForm', () => {
-  // eslint-disable-next-line testing-library/no-render-in-setup
-  beforeEach(() => render(<ContactsForm contactsList={mockContactsList} />));
+  test('form fields should have proper values', async () => {
+    render(<ContactsForm contactsList={mockContactsList} />);
 
-  afterEach(cleanup);
+    const email = await screen.findByDisplayValue(mockContactsList.email);
+    const githubUsername = await screen.findByDisplayValue(mockContactsList.githubUsername);
+    const linkedin = await screen.findByDisplayValue(mockContactsList.linkedin);
+    const phone = await screen.findByDisplayValue(mockContactsList.phone);
+    const skype = await screen.findByDisplayValue(mockContactsList.skype);
+    const telegram = await screen.findByDisplayValue(mockContactsList.telegram);
+    const website = await screen.findByDisplayValue(mockContactsList.website);
 
-  test.each`
-    contactName   | expectedValue
-    ${'Email'}    | ${mockContactsList.email}
-    ${'Github'}   | ${mockContactsList.githubUsername}
-    ${'Linkedin'} | ${mockContactsList.linkedin}
-    ${'Phone'}    | ${mockContactsList.phone}
-    ${'Skype'}    | ${mockContactsList.skype}
-    ${'Telegram'} | ${mockContactsList.telegram}
-    ${'Website'}  | ${mockContactsList.website}
-  `('should render "$contactName" field with proper value', ({ expectedValue }) => {
-    const input = screen.getByDisplayValue(expectedValue);
-    expect(input).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+    expect(githubUsername).toBeInTheDocument();
+    expect(linkedin).toBeInTheDocument();
+    expect(phone).toBeInTheDocument();
+    expect(skype).toBeInTheDocument();
+    expect(telegram).toBeInTheDocument();
+    expect(website).toBeInTheDocument();
   });
 
-  test.each`
-    contactName   | placeholderText
-    ${'Email'}    | ${'Email'}
-    ${'Github'}   | ${'Github username'}
-    ${'Linkedin'} | ${'LinkedIn username'}
-    ${'Phone'}    | ${'+12025550111'}
-    ${'Skype'}    | ${'Skype id'}
-    ${'Telegram'} | ${'Telegram public name'}
-    ${'Website'}  | ${'Enter your website URL'}
-  `('should render "$contactName" field with proper placeholder', ({ placeholderText }) => {
-    const input = screen.getByPlaceholderText(placeholderText);
-    expect(input).toBeInTheDocument();
+  test('form fields should have proper placeholders', async () => {
+    render(<ContactsForm contactsList={mockContactsList} />);
+
+    const email = await screen.findByPlaceholderText('Email');
+    const githubUsername = await screen.findByPlaceholderText('Github username');
+    const linkedin = await screen.findByPlaceholderText('LinkedIn username');
+    const phone = await screen.findByPlaceholderText('+12025550111');
+    const skype = await screen.findByPlaceholderText('Skype id');
+    const telegram = await screen.findByPlaceholderText('Telegram public name');
+    const website = await screen.findByPlaceholderText('Enter your website URL');
+
+    expect(email).toBeInTheDocument();
+    expect(githubUsername).toBeInTheDocument();
+    expect(linkedin).toBeInTheDocument();
+    expect(phone).toBeInTheDocument();
+    expect(skype).toBeInTheDocument();
+    expect(telegram).toBeInTheDocument();
+    expect(website).toBeInTheDocument();
   });
 
-  test.each`
-    contactName   | labelText
-    ${'Email'}    | ${'Email'}
-    ${'Github'}   | ${'Github'}
-    ${'Linkedin'} | ${'LinkedIn'}
-    ${'Phone'}    | ${'Phone'}
-    ${'Skype'}    | ${'Skype'}
-    ${'Telegram'} | ${'Telegram'}
-    ${'Website'}  | ${'Website'}
-  `('should render "$contactName" field with proper Label', ({ labelText }) => {
-    const input = screen.getByLabelText(labelText);
-    expect(input).toBeInTheDocument();
+  test('form fields should have proper labels', async () => {
+    render(<ContactsForm contactsList={mockContactsList} />);
+
+    const email = await screen.findByLabelText('Email');
+    const githubUsername = await screen.findByLabelText('Github');
+    const linkedin = await screen.findByLabelText('LinkedIn');
+    const phone = await screen.findByLabelText('Phone');
+    const skype = await screen.findByLabelText('Skype');
+    const telegram = await screen.findByLabelText('Telegram');
+    const website = await screen.findByLabelText('Website');
+
+    expect(email).toBeInTheDocument();
+    expect(githubUsername).toBeInTheDocument();
+    expect(linkedin).toBeInTheDocument();
+    expect(phone).toBeInTheDocument();
+    expect(skype).toBeInTheDocument();
+    expect(telegram).toBeInTheDocument();
+    expect(website).toBeInTheDocument();
   });
 });
