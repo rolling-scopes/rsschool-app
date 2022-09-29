34b6190d7bdb0bacd7f3e30cac4d8038235bc4b3
Maksim Shylau <shilaums@gmail.com>
2022-09-29 14:25:37 +0200

    test: add ContactsSection tests

---
+++ client/src/modules/Opportunities/components/ViewCv/ContactsSection/index.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import { Contacts } from 'modules/Opportunities/models';
+import { ContactsSection } from './index';
+
+jest.mock('./ContactsList', () => ({
+    ContactsList: () => <div>Mock Contacts</div>,
+}));
+
+describe('ContactsSection', () => {
+    test('should display nothing if contacts are not provided', () => {
+        const { container } = render(<ContactsSection contacts={null} />);
+        expect(container).toBeEmptyDOMElement();
+    });
+
+    test('should display section if contacts are provided', () => {
+        render(<ContactsSection contacts={{} as Contacts} />);
+        const contactsList = screen.getByText('Mock Contacts');
+        expect(contactsList).toBeInTheDocument();
+    });
+});
