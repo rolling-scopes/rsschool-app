844240a781055cf642e268dc13db7ccc2012d644
Maksim Shylau <shilaums@gmail.com>
2022-10-03 14:35:47 +0200

    test: refactor test cases

--- client/src/modules/Opportunities/components/EditCv/ContactsForm/index.test.tsx
+++ client/src/modules/Opportunities/components/EditCv/ContactsForm/index.test.tsx
@@ -12,63 +12,24 @@ const mockContactsList = {
 };
 
 describe('ContactsForm', () => {
-  test('form fields should have proper values', async () => {
+  test.each`
+    value                              | placeholder                 | labelText
+    ${mockContactsList.email}          | ${'Email'}                  | ${'Email'}
+    ${mockContactsList.githubUsername} | ${'Github username'}        | ${'Github'}
+    ${mockContactsList.linkedin}       | ${'LinkedIn username'}      | ${'LinkedIn'}
+    ${mockContactsList.phone}          | ${'+12025550111'}           | ${'Phone'}
+    ${mockContactsList.skype}          | ${'Skype id'}               | ${'Skype'}
+    ${mockContactsList.telegram}       | ${'Telegram public name'}   | ${'Telegram'}
+    ${mockContactsList.website}        | ${'Enter your website URL'} | ${'Website'}
+  `('form field should have proper value, placeholder and label', async ({ value, placeholder, labelText }) => {
     render(<ContactsForm contactsList={mockContactsList} />);
 
-    const email = await screen.findByDisplayValue(mockContactsList.email);
-    const githubUsername = await screen.findByDisplayValue(mockContactsList.githubUsername);
-    const linkedin = await screen.findByDisplayValue(mockContactsList.linkedin);
-    const phone = await screen.findByDisplayValue(mockContactsList.phone);
-    const skype = await screen.findByDisplayValue(mockContactsList.skype);
-    const telegram = await screen.findByDisplayValue(mockContactsList.telegram);
-    const website = await screen.findByDisplayValue(mockContactsList.website);
+    const fieldDisplayedValue = await screen.findByDisplayValue(value);
+    const fieldPlaceholder = await screen.findByPlaceholderText(placeholder);
+    const fieldLabel = await screen.findByLabelText(labelText);
 
-    expect(email).toBeInTheDocument();
-    expect(githubUsername).toBeInTheDocument();
-    expect(linkedin).toBeInTheDocument();
-    expect(phone).toBeInTheDocument();
-    expect(skype).toBeInTheDocument();
-    expect(telegram).toBeInTheDocument();
-    expect(website).toBeInTheDocument();
-  });
-
-  test('form fields should have proper placeholders', async () => {
-    render(<ContactsForm contactsList={mockContactsList} />);
-
-    const email = await screen.findByPlaceholderText('Email');
-    const githubUsername = await screen.findByPlaceholderText('Github username');
-    const linkedin = await screen.findByPlaceholderText('LinkedIn username');
-    const phone = await screen.findByPlaceholderText('+12025550111');
-    const skype = await screen.findByPlaceholderText('Skype id');
-    const telegram = await screen.findByPlaceholderText('Telegram public name');
-    const website = await screen.findByPlaceholderText('Enter your website URL');
-
-    expect(email).toBeInTheDocument();
-    expect(githubUsername).toBeInTheDocument();
-    expect(linkedin).toBeInTheDocument();
-    expect(phone).toBeInTheDocument();
-    expect(skype).toBeInTheDocument();
-    expect(telegram).toBeInTheDocument();
-    expect(website).toBeInTheDocument();
-  });
-
-  test('form fields should have proper labels', async () => {
-    render(<ContactsForm contactsList={mockContactsList} />);
-
-    const email = await screen.findByLabelText('Email');
-    const githubUsername = await screen.findByLabelText('Github');
-    const linkedin = await screen.findByLabelText('LinkedIn');
-    const phone = await screen.findByLabelText('Phone');
-    const skype = await screen.findByLabelText('Skype');
-    const telegram = await screen.findByLabelText('Telegram');
-    const website = await screen.findByLabelText('Website');
-
-    expect(email).toBeInTheDocument();
-    expect(githubUsername).toBeInTheDocument();
-    expect(linkedin).toBeInTheDocument();
-    expect(phone).toBeInTheDocument();
-    expect(skype).toBeInTheDocument();
-    expect(telegram).toBeInTheDocument();
-    expect(website).toBeInTheDocument();
+    expect(fieldDisplayedValue).toBeInTheDocument();
+    expect(fieldPlaceholder).toBeInTheDocument();
+    expect(fieldLabel).toBeInTheDocument();
   });
 });
