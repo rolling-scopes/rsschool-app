ad5caeb99b34c6df81ac4ace0457d612bfe6720a
Natalia Gulko <Natalia_Gulko1@epam.com>
2022-10-27 19:54:59 +0300

    feat(mentor-dashboard): responsive instructions

--- client/src/modules/Mentor/components/Instructions/Instructions.tsx
+++ client/src/modules/Mentor/components/Instructions/Instructions.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Badge, Card, Space, Typography } from 'antd';
+import { Badge, Card, Col, Divider, Row, Space, Typography } from 'antd';
 import { INSTRUCTIONS_TEXT, renderDescription, renderSocialLinks } from '.';
 
 const { Meta, Grid } = Card;
@@ -13,24 +13,28 @@ function Instructions() {
       <Grid style={{ width: '100%' }} hoverable={false}>
         <Meta title={title} description={<Text>{description}</Text>} />
       </Grid>
-      {steps.map((s, idx) => (
-        <Grid key={s.title} hoverable={false} style={{ width: '33.33333%' }}>
-          <Meta
-            title={
-              <Space>
-                <Badge count={idx + 1} style={{ backgroundColor: '#1890FF' }} />
-                {s.title}
-              </Space>
-            }
-            description={
-              <Space direction="vertical" size="middle">
-                {renderDescription(s.html)}
-                {s.links ? renderSocialLinks(s.links) : null}
-              </Space>
-            }
-          />
-        </Grid>
-      ))}
+      <Row>
+        {steps.map((s, idx) => (
+          <Col key={s.title} xs={24} sm={24} md={8}>
+            <Grid hoverable={false} style={{ width: '100%', height: '100%' }}>
+              <Meta
+                title={
+                  <Space>
+                    <Badge count={idx + 1} style={{ backgroundColor: '#1890FF' }} />
+                    {s.title}
+                  </Space>
+                }
+                description={
+                  <Space direction="vertical" size="middle">
+                    {renderDescription(s.html)}
+                    {s.links ? renderSocialLinks(s.links) : null}
+                  </Space>
+                }
+              />
+            </Grid>
+          </Col>
+        ))}
+      </Row>
     </Card>
   );
 }
