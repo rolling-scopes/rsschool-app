6cbed5baa600c93dc51b375ba6c2c7b343137ab6
Natalia Gulko <Natalia_Gulko1@epam.com>
2022-11-05 16:08:56 +0300

    feat(mentor-dashboard): submit review modal (#1821)
    * fea(mentor-dashboard): get students count
    
    * refactor(solutions-table): set loading on table
    
    * feat(mentor-dashboard): add link
    
    * refactor(modal-submit-form): destructure props
    
    * feat(mentor-dashboard): remove githubId
    
    * feat(mentor-dashboard): add modal window
    
    * Revert "feat(mentor-dashboard): remove githubId"
    
    This reverts commit c6271d3e0ef5e17c309e75758a62a97f91a9b40f.
    
    * feat(mentor-dashboard): send review from modal
    
    * feat(mentor-dashboard): modal layout
    
    * test(mentor-dashboard): table data
    
    * test(mentor-dashboard): should render table data
    
    * test(mentor-dashboard): should render instructions
    
    * test(submit-review-modal): should render fields
    
    * test: mock axios
    
    * test(submit-review-modal): should render succes/error message
    
    * test(modal-submit-form): should render
    
    * chore: format
    
    * refactor(submit-review-modal): use finally

---
+++ client/src/modules/Mentor/components/SubmitReviewModal/SubmitReviewModal.test.tsx
@@ -0,0 +1,98 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { MODAL_TITLE, SubmitReviewModal, SubmitReviewModalProps } from '.';
+import { SUCCESS_MESSAGE } from './SubmitReviewModal';
+import mockAxios from 'jest-mock-axios';
+
+const MODAL_DATA_MOCK = {
+  courseTaskId: 1,
+  maxScore: 100,
+  resultScore: 20,
+  solutionUrl: `solution-url`,
+  studentGithubId: `student-github`,
+  studentName: `Student Name`,
+  taskDescriptionUrl: `task-url`,
+  taskName: `Task Name`,
+};
+
+const PROPS_MOCK: SubmitReviewModalProps = {
+  data: MODAL_DATA_MOCK,
+  courseId: 1,
+  onClose: jest.fn(),
+  onSubmit: jest.fn(),
+};
+
+describe('SubmitReviewModal', () => {
+  afterEach(() => {
+    mockAxios.reset();
+  });
+
+  it.each`
+    text                           | role
+    ${MODAL_DATA_MOCK.taskName}    | ${'link'}
+    ${MODAL_DATA_MOCK.solutionUrl} | ${'link'}
+    ${'Submit'}                    | ${'button'}
+    ${'Cancel'}                    | ${'button'}
+  `('should render $role "$text"', ({ text, role }: { text: string; role: string }) => {
+    render(<SubmitReviewModal {...PROPS_MOCK} />);
+
+    const element = screen.getByRole(role, { name: new RegExp(text) });
+
+    expect(element).toBeInTheDocument();
+  });
+
+  it.each`
+    text
+    ${MODAL_DATA_MOCK.maxScore}
+    ${MODAL_DATA_MOCK.studentName}
+  `('should render field "$text"', ({ text }: { text: string }) => {
+    render(<SubmitReviewModal {...PROPS_MOCK} />);
+
+    const element = screen.getByText(new RegExp(text));
+
+    expect(element).toBeInTheDocument();
+  });
+
+  it('should not render fields when data was not provided', () => {
+    render(<SubmitReviewModal {...PROPS_MOCK} data={null} />);
+
+    const modal = screen.queryByText(new RegExp(MODAL_TITLE));
+
+    expect(modal).not.toBeInTheDocument();
+  });
+
+  it('should render success message on submit', async () => {
+    mockAxios.post.mockResolvedValueOnce({ data: true });
+    render(<SubmitReviewModal {...PROPS_MOCK} />);
+    const scoreInput = screen.getByRole('spinbutton', { name: /score \(max 100 points\)/i });
+    fireEvent.change(scoreInput, { target: { value: 10 } });
+
+    const submitBtn = screen.getByRole('button', { name: 'Submit' });
+    fireEvent.click(submitBtn);
+
+    const message = await screen.findByText(SUCCESS_MESSAGE);
+    expect(message).toBeInTheDocument();
+  });
+
+  it('should render error message when error has occurred on submit', async () => {
+    const errorMessage = 'Network error';
+    mockAxios.post.mockRejectedValueOnce(new Error(errorMessage));
+    render(<SubmitReviewModal {...PROPS_MOCK} />);
+    const scoreInput = screen.getByRole('spinbutton', { name: /score \(max 100 points\)/i });
+    fireEvent.change(scoreInput, { target: { value: 10 } });
+
+    const submitBtn = screen.getByRole('button', { name: 'Submit' });
+    fireEvent.click(submitBtn);
+
+    const message = await screen.findByText(errorMessage);
+    expect(message).toBeInTheDocument();
+  });
+
+  it('should call onClose when "Cancel" button was clicked', async () => {
+    render(<SubmitReviewModal {...PROPS_MOCK} />);
+    const cancelBtn = screen.getByRole('button', { name: 'Cancel' });
+
+    fireEvent.click(cancelBtn);
+
+    expect(PROPS_MOCK.onClose).toHaveBeenCalledWith(null);
+  });
+});
--- client/src/modules/Mentor/hooks/useMentorStudentsCount.tsx
+++ client/src/modules/Mentor/hooks/useMentorStudentsCount.tsx
@@ -2,13 +2,13 @@ import { MentorsApi } from 'api';
 import { useMemo } from 'react';
 import { useAsync } from 'react-use';
 
-export function useMentorDashboard(mentorId: number | null, courseId: number, hasChanged: boolean) {
+export function useMentorStudentsCount(mentorId: number | null, courseId: number) {
   const service = useMemo(() => new MentorsApi(), []);
 
   const { value: data, loading } = useAsync(async () => {
-    const { data = [] } = mentorId ? await service.getMentorDashboardData(mentorId, courseId) : { data: [] };
+    const { data = [] } = mentorId ? await service.getCourseStudentsCount(mentorId, courseId) : { data: [] };
     return data;
-  }, [hasChanged]);
+  }, []);
 
   return [data, loading] as const;
 }
