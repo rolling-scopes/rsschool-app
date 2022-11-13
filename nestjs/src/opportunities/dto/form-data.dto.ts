5a995958f26a228679142f13e8a49a6231aed65b
Maksim Shylau <shilaums@gmail.com>
2022-11-13 20:16:00 +0100

    feat: add validation to dto

--- nestjs/src/opportunities/dto/form-data.dto.ts
+++ nestjs/src/opportunities/dto/form-data.dto.ts
@@ -1,59 +1,93 @@
 import { LanguageLevel } from '@entities/data';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsBoolean, IsString, ValidateIf } from 'class-validator';
 
-//type FormData = Omit<Resume, 'uuid' | 'updatedDate' | 'user' | 'userId' | 'isHidden' | 'githubId' | 'expires' | 'id'>;
 export class FormDataDto {
+  @IsString()
+  @ValidateIf(value => value === null)
   @ApiProperty({ nullable: true, type: String })
   public avatarLink: string | null;
 
+  @IsArray()
   @ApiProperty({ type: [Number] })
   public visibleCourses: number[];
 
+  @IsString()
+  @ValidateIf(value => value === null)
   @ApiProperty({ nullable: true, type: String })
   public desiredPosition: string | null;
 
+  @IsString()
+  @ValidateIf(value => value === null)
   @ApiProperty({ nullable: true, type: String })
   public email: string | null;
 
+  @IsString()
+  @ValidateIf(value => value === null)
   @ApiProperty({ enum: LanguageLevel, nullable: true })
   public englishLevel: LanguageLevel | null;
 
+  @IsBoolean()
   @ApiProperty()
   public fullTime: boolean;
 
+  @IsString()
+  @ValidateIf(value => value === null)
   @ApiProperty({ nullable: true, type: String })
   public githubUsername: string | null;
 
+  @IsString()
+  @ValidateIf(value => value === null)
   @ApiProperty({ nullable: true, type: String })
   public linkedin: string | null;
 
+  @IsString()
+  @ValidateIf(value => value === null)
   @ApiProperty({ nullable: true, type: String })
   public locations: string | null;
 
+  @IsString()
+  @ValidateIf(value => value === null)
   @ApiProperty({ enum: ['served', 'liable', 'notLiable'], nullable: true })
   public militaryService: string | null;
 
+  @IsString()
+  @ValidateIf(value => value === null)
   @ApiProperty({ nullable: true, type: String })
   public name: string | null;
 
+  @IsString()
+  @ValidateIf(value => value === null)
   @ApiProperty({ nullable: true, type: String })
   public notes: string | null;
 
+  @IsString()
+  @ValidateIf(value => value === null)
   @ApiProperty({ nullable: true, type: String })
   public phone: string | null;
 
+  @IsString()
+  @ValidateIf(value => value === null)
   @ApiProperty({ nullable: true, type: String })
   public selfIntroLink: string | null;
 
+  @IsString()
+  @ValidateIf(value => value === null)
   @ApiProperty({ nullable: true, type: String })
   public skype: string | null;
 
+  @IsString()
+  @ValidateIf(value => value === null)
   @ApiProperty({ nullable: true, type: String })
   public startFrom: string | null;
 
+  @IsString()
+  @ValidateIf(value => value === null)
   @ApiProperty({ nullable: true, type: String })
   public telegram: string | null;
 
+  @IsString()
+  @ValidateIf(value => value === null)
   @ApiProperty({ nullable: true, type: String })
   public website: string | null;
 }
