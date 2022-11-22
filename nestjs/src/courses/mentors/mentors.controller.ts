500618ac2b3b8b9b84520cf2aa34994ac1cf804d
Natalia Gulko <Natalia_Gulko1@epam.com>
2022-11-22 14:03:21 +0300

    refactor: add required roles

--- nestjs/src/courses/mentors/mentors.controller.ts
+++ nestjs/src/courses/mentors/mentors.controller.ts
@@ -1,8 +1,7 @@
 import { Controller, Get, Param, ParseIntPipe, Req, UseGuards } from '@nestjs/common';
 import { ApiBadRequestResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { InsertResult } from 'typeorm';
 import { MentorsService } from '.';
-import { CurrentRequest, DefaultGuard, RoleGuard } from '../../auth';
+import { CourseRole, CurrentRequest, DefaultGuard, RequiredRoles, RoleGuard } from '../../auth';
 import { MentorDashboardDto } from './dto/mentor-dashboard.dto';
 import { MentorStudentDto } from './dto/mentor-student.dto';
 
@@ -16,6 +15,7 @@ export class MentorsController {
   @ApiOperation({ operationId: 'getMentorStudents' })
   @ApiOkResponse({ type: [MentorStudentDto] })
   @ApiBadRequestResponse()
+  @RequiredRoles([CourseRole.Mentor, CourseRole.Supervisor, CourseRole.Manager])
   public async getMentorStudents(@Param('mentorId', ParseIntPipe) mentorId: number, @Req() req: CurrentRequest) {
     const items = await this.mentorsService.getStudents(mentorId);
     return items.map(item => new MentorStudentDto(item));
@@ -25,10 +25,10 @@ export class MentorsController {
   @ApiOperation({ operationId: 'getCourseStudentsCount' })
   @ApiOkResponse({ type: Number })
   @ApiBadRequestResponse()
+  @RequiredRoles([CourseRole.Mentor, CourseRole.Supervisor, CourseRole.Manager])
   public async getCourseStudentsCount(
     @Param('mentorId', ParseIntPipe) mentorId: number,
     @Param('courseId', ParseIntPipe) courseId: number,
-    @Req() req: CurrentRequest,
   ) {
     return await this.mentorsService.getCourseStudentsCount(mentorId, courseId);
   }
@@ -37,23 +37,23 @@ export class MentorsController {
   @ApiOperation({ operationId: 'getMentorDashboardData' })
   @ApiOkResponse({ type: [MentorDashboardDto] })
   @ApiBadRequestResponse()
+  @RequiredRoles([CourseRole.Mentor, CourseRole.Supervisor, CourseRole.Manager])
   public async getMentorDashboardData(
     @Param('mentorId', ParseIntPipe) mentorId: number,
     @Param('courseId', ParseIntPipe) courseId: number,
-    @Req() req: CurrentRequest,
   ): Promise<MentorDashboardDto[]> {
     return await this.mentorsService.getStudentsTasks(mentorId, courseId);
   }
 
   @Get('/:mentorId/course/:courseId/random-task')
   @ApiOperation({ operationId: 'getRandomTask' })
-  @ApiOkResponse({ type: InsertResult })
+  @ApiOkResponse({})
   @ApiBadRequestResponse()
+  @RequiredRoles([CourseRole.Mentor, CourseRole.Supervisor, CourseRole.Manager])
   public async getRandomTask(
     @Param('mentorId', ParseIntPipe) mentorId: number,
     @Param('courseId', ParseIntPipe) courseId: number,
-    @Req() req: CurrentRequest,
-  ): Promise<InsertResult> {
+  ) {
     return await this.mentorsService.getRandomTask(mentorId, courseId);
   }
 }
