44500c38917978f2f0d40cc972c0bdf70aa8b12b
Natalia Gulko <Natalia_Gulko1@epam.com>
2022-12-13 12:21:24 +0300

    chore: format

--- client/src/modules/AutoTest/components/SelfEducation/SelfEducation.tsx
+++ client/src/modules/AutoTest/components/SelfEducation/SelfEducation.tsx
@@ -67,83 +67,85 @@ function SelfEducation({ courseTask }: SelfEducationProps) {
   return (
     <>
       {renderInfo()}
-      {randomQuestions?.map(({ question, answers, multiple, questionImage, answersType, index: questionIndex }, idx) => {
-        const questionNumber = idx + 1;
-        return (
-          <Form.Item
-            key={questionIndex}
-            label={
-              <Row>
-                <Title level={5}>
-                  {questionNumber}. {question}
-                </Title>
-                {questionImage && (
-                  <img
-                    src={questionImage}
-                    style={{
-                      width: '100%',
-                      maxWidth: '700px',
-                      marginBottom: '10px',
-                    }}
-                  />
-                )}
-              </Row>
-            }
-            name={`answer-${questionIndex}`}
-            rules={[{ required: true, message: 'Please answer the question' }]}
-          >
-            {multiple ? (
-              <Checkbox.Group>
-                {answers?.map((answer, answerIndex) => (
-                  <Row key={answerIndex}>
-                    <Checkbox value={answerIndex}>
-                      {answersType === 'image' ? (
-                        <>
-                          ({answerIndex + 1}){' '}
-                          <img
-                            src={answer}
-                            style={{
-                              width: '100%',
-                              maxWidth: '400px',
-                              marginBottom: '10px',
-                            }}
-                          />
-                        </>
-                      ) : (
-                        answer
-                      )}
-                    </Checkbox>
-                  </Row>
-                ))}
-              </Checkbox.Group>
-            ) : (
-              <Radio.Group>
-                {answers?.map((answer, index) => (
-                  <Row key={index}>
-                    <Radio value={index}>
-                      {answersType === 'image' ? (
-                        <>
-                          ({index + 1}){' '}
-                          <img
-                            src={answer}
-                            style={{
-                              width: '100%',
-                              maxWidth: '400px',
-                              marginBottom: '10px',
-                            }}
-                          />
-                        </>
-                      ) : (
-                        answer
-                      )}
-                    </Radio>
-                  </Row>
-                ))}
-              </Radio.Group>
-            )}
-          </Form.Item>
-        );
-      })}
+      {randomQuestions?.map(
+        ({ question, answers, multiple, questionImage, answersType, index: questionIndex }, idx) => {
+          const questionNumber = idx + 1;
+          return (
+            <Form.Item
+              key={questionIndex}
+              label={
+                <Row>
+                  <Title level={5}>
+                    {questionNumber}. {question}
+                  </Title>
+                  {questionImage && (
+                    <img
+                      src={questionImage}
+                      style={{
+                        width: '100%',
+                        maxWidth: '700px',
+                        marginBottom: '10px',
+                      }}
+                    />
+                  )}
+                </Row>
+              }
+              name={`answer-${questionIndex}`}
+              rules={[{ required: true, message: 'Please answer the question' }]}
+            >
+              {multiple ? (
+                <Checkbox.Group>
+                  {answers?.map((answer, answerIndex) => (
+                    <Row key={answerIndex}>
+                      <Checkbox value={answerIndex}>
+                        {answersType === 'image' ? (
+                          <>
+                            ({answerIndex + 1}){' '}
+                            <img
+                              src={answer}
+                              style={{
+                                width: '100%',
+                                maxWidth: '400px',
+                                marginBottom: '10px',
+                              }}
+                            />
+                          </>
+                        ) : (
+                          answer
+                        )}
+                      </Checkbox>
+                    </Row>
+                  ))}
+                </Checkbox.Group>
+              ) : (
+                <Radio.Group>
+                  {answers?.map((answer, index) => (
+                    <Row key={index}>
+                      <Radio value={index}>
+                        {answersType === 'image' ? (
+                          <>
+                            ({index + 1}){' '}
+                            <img
+                              src={answer}
+                              style={{
+                                width: '100%',
+                                maxWidth: '400px',
+                                marginBottom: '10px',
+                              }}
+                            />
+                          </>
+                        ) : (
+                          answer
+                        )}
+                      </Radio>
+                    </Row>
+                  ))}
+                </Radio.Group>
+              )}
+            </Form.Item>
+          );
+        },
+      )}
     </>
   );
 }
