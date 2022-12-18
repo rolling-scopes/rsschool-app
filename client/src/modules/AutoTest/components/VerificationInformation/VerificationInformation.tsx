ab907df6c187e325744da85beab1deb8dab4b89e
Natalia Gulko <Natalia_Gulko1@epam.com>
2022-12-18 17:20:05 +0300

    feat(auto-test): add refresh button

--- client/src/modules/AutoTest/components/VerificationInformation/VerificationInformation.tsx
+++ client/src/modules/AutoTest/components/VerificationInformation/VerificationInformation.tsx
@@ -2,12 +2,14 @@ import { Row, Col, Space, Button, Alert, Typography } from 'antd';
 import { VerificationsTable } from 'modules/AutoTest/components';
 import { useAttemptsMessage } from 'modules/AutoTest/hooks';
 import { CourseTaskVerifications } from 'modules/AutoTest/types';
+import { ReloadOutlined } from '@ant-design/icons';
 
 type VerificationInformationProps = {
   courseTask: CourseTaskVerifications;
   loading: boolean;
   isTableVisible: boolean;
   startTask: () => void;
+  reload: () => void;
 };
 
 const { Text } = Typography;
@@ -17,38 +19,46 @@ function VerificationInformation({
   loading,
   isTableVisible,
   startTask,
+  reload,
 }: VerificationInformationProps): any {
   const { maxScore, verifications } = courseTask;
   const { explanation, attemptsLeftMessage, allowStartTask } = useAttemptsMessage(courseTask);
 
   return (
-    <Row style={{ background: 'white', padding: 24 }} gutter={[0, 24]} justify="center">
-      <Col span={24}>
-        <Alert showIcon type="info" message={explanation} />
-      </Col>
-      {attemptsLeftMessage && (
+    <>
+      <Row style={{ background: 'white', padding: '24px 24px 12px' }} gutter={[0, 24]} justify="end">
         <Col span={24}>
-          <Space>
-            <Text type="secondary">Attempts:</Text>
-            <Text>{attemptsLeftMessage}</Text>
-          </Space>
+          <Alert showIcon type="info" message={explanation} />
         </Col>
-      )}
+        {attemptsLeftMessage && (
+          <Col flex="auto">
+            <Space>
+              <Text type="secondary">Attempts:</Text>
+              <Text>{attemptsLeftMessage}</Text>
+            </Space>
+          </Col>
+        )}
+        {isTableVisible && (
+          <Col flex="none">
+            <Button icon={<ReloadOutlined />} onClick={reload}>
+              Refresh
+            </Button>
+          </Col>
+        )}
+      </Row>
       {isTableVisible && (
-        <>
+        <Row style={{ background: 'white', padding: '0 24px 24px' }} gutter={[0, 24]} justify="center">
           <Col span={24}>
             <VerificationsTable maxScore={maxScore} verifications={verifications} loading={loading} />
           </Col>
           <Col>
-            <Space>
-              <Button type="primary" onClick={startTask} disabled={!allowStartTask}>
-                Start test
-              </Button>
-            </Space>
+            <Button type="primary" onClick={startTask} disabled={!allowStartTask}>
+              Start test
+            </Button>
           </Col>
-        </>
+        </Row>
       )}
-    </Row>
+    </>
   );
 }
 
