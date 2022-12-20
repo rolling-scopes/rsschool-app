3e8b595b34f1c8cc679e5d9f0e4eaa976ded0255
Valery <valerydluski@gmail.com>
2022-12-20 20:50:30 +0100

    fix: prettier

--- server/src/migrations/1671565186190-Event.ts
+++ server/src/migrations/1671565186190-Event.ts
@@ -1,14 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Event1671565186190 implements MigrationInterface {
-    name = 'Event1671565186190'
+  name = 'Event1671565186190';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "event" ADD "deletedDate" TIMESTAMP`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "event" ADD "deletedDate" TIMESTAMP`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "event" DROP COLUMN "deletedDate"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "event" DROP COLUMN "deletedDate"`);
+  }
 }
