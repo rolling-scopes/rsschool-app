f6fd66978de4ae7edd41938dfdf1104b55dc3f74
Valery <valerydluski@gmail.com>
2023-01-02 09:48:16 +0100

    fix: update mobile view

--- client/src/modules/TeamDistribution/components/WelcomeCard/WelcomeCard.tsx
+++ client/src/modules/TeamDistribution/components/WelcomeCard/WelcomeCard.tsx
@@ -1,15 +1,17 @@
 import { Card, Col, Row, Typography } from 'antd';
+import { useMedia } from 'react-use';
 
 const { Text } = Typography;
 
 export default function WelcomeCard() {
+  const mobileView = useMedia('(max-width: 720px)');
   return (
     <Card
       title={<Text style={{ color: '#fff' }}>Become a member of the team!</Text>}
       style={{ marginTop: 24, backgroundColor: '#ff7875' }}
     >
       <Row gutter={24} style={{ minHeight: '100px' }}>
-        <Col span={12}>
+        <Col span={!mobileView ? 12 : 24}>
           <Text style={{ color: '#fff' }}>
             Group task – it is a possibility to unite with your colleagues to develop the best solutions, and to gain
             knowledge and skills.
@@ -17,20 +19,22 @@ export default function WelcomeCard() {
             choose to do either of these, you will be added to a team automatically.
           </Text>
         </Col>
-        <Col span={12} style={{ position: 'relative', display: 'flex', justifyContent: 'end', paddingRight: '24px' }}>
-          <div
-            style={{
-              backgroundImage: `url(https://cdn.rs.school/sloths/stickers/welcome/image.png)`,
-              position: 'absolute',
-              top: '-120px',
-              backgroundSize: 'contain',
-              backgroundRepeat: 'no-repeat',
-              backgroundPosition: 'center',
-              height: '240px',
-              width: '240px',
-            }}
-          />
-        </Col>
+        {!mobileView && (
+          <Col span={12} style={{ position: 'relative', display: 'flex', justifyContent: 'end', paddingRight: '24px' }}>
+            <div
+              style={{
+                backgroundImage: `url(https://cdn.rs.school/sloths/stickers/welcome/image.png)`,
+                position: 'absolute',
+                top: '-120px',
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                height: '240px',
+                width: '240px',
+              }}
+            />
+          </Col>
+        )}
       </Row>
     </Card>
   );
