cc3a00e333deda078a42e82f871e832c25b5293b
Valery <valerydluski@gmail.com>
2023-01-18 12:54:03 +0100

    fix: remove tooltip

--- client/src/modules/CourseManagement/components/CourseEventModal/index.tsx
+++ client/src/modules/CourseManagement/components/CourseEventModal/index.tsx
@@ -95,12 +95,7 @@ export function CourseEventModal({ data, onCancel, courseId, onSubmit }: Props) 
       {data.event?.id ? (
         <Title level={4}>{data.event.name}</Title>
       ) : (
-        <Form.Item
-          name="event"
-          label="Event"
-          rules={[{ required: true, message: 'Please select an event' }]}
-          tooltip="sdfasfa"
-        >
+        <Form.Item name="event" label="Event" rules={[{ required: true, message: 'Please select an event' }]}>
           <Select
             mode="tags"
             maxTagCount={1}
