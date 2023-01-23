130909355388a9aad01c4b768c4bb79c0b54b525
Valery <valerydluski@gmail.com>
2023-01-23 11:00:13 +0100

    feat: update interview template

--- client/src/data/interviews/corejs2.ts
+++ client/src/data/interviews/corejs2.ts
@@ -7,34 +7,25 @@ export const corejs2Template: InterviewTemplate = {
   During the interview, students can score up to 100 points. Each topic has its value in points.<br/>
   Please see the details below.
   <ul>
-  <li>Advanced Expressions - up to 10 points</li>
   <li>Objects Built-in methods - up to 5 points</li>
-  <li>Arrays Built-in methods - up to 5 points</li>
+  <li>Objects props - up to 5 points</li>
   <li>Functional Scope - up to 10 points</li>
-  <li>ECMAScript Intermediate - up to 10 points</li>
   <li>Advanced Functions - up to 15 points</li>
   <li>Functional Patterns - up to 10 points</li>
   <li>Closures Advanced - up to 15 points</li>
   <li>Prototypal Inheritance Basics - up to 10 points</li>
   <li>ECMAScript Classes - up to 5 points</li>
+  <li>ECMAScript Intermediate - up to 10 points</li>
   <li>ECMAScript Data Types & Expressions - up to 5 points</li>
   <li>ECMAScript Advanced - up to 10 points</li>
+  <li>Network requests - up to 10 points</li>
   <li>Page Lifecycle - up to 5 points</li>
-  <li>Events Propagation / Preventing - up to 5 points</li>
   <li>Web Storage API & cookies - up to 5 points</li>
+  <li>Typescript - up to 10 points</li>
   </ul>
   <br/>
   `,
   categories: [
-    {
-      id: 2000,
-      name: 'Advanced Expressions',
-      description: '10 points',
-      questions: [
-        { id: 2001, name: 'Hoisting' },
-        { id: 2002, name: 'Temporal dead zone' },
-      ],
-    },
     {
       id: 2010,
       name: 'Objects Built-in methods',
@@ -46,11 +37,11 @@ export const corejs2Template: InterviewTemplate = {
     },
     {
       id: 2020,
-      name: 'Arrays Built-in methods',
+      name: 'Objects props',
       description: '5 points',
       questions: [
-        { id: 2021, name: 'Know how to copy array part' },
-        { id: 2022, name: 'Know how to flatten nested array' },
+        { id: 2021, name: 'Object computed props' },
+        { id: 2022, name: 'Be able to loop through Object keys' },
       ],
     },
     {
@@ -65,29 +56,13 @@ export const corejs2Template: InterviewTemplate = {
     },
     {
       id: 2040,
-      name: 'ECMAScript Intermediate',
-      description: '10 points',
-      questions: [
-        { id: 2041, name: 'Function default parameters' },
-        { id: 2042, name: 'Know how to use spread operator for Function arguments' },
-        { id: 2043, name: 'Spread operator for Array' },
-        {
-          id: 2044,
-          name: 'Understand and able to use spread operator for Array concatenation Destructuring assignment',
-        },
-        { id: 2045, name: 'Be able to discover destructuring assignment concept' },
-        { id: 2046, name: 'String templates' },
-      ],
-    },
-    {
-      id: 2050,
       name: 'Advanced Functions',
       description: '15 points',
       questions: [
-        { id: 2051, name: 'this scope' },
-        { id: 2052, name: 'Reference Type & losing this' },
-        { id: 2053, name: 'Be able to replace this scope' },
-        { id: 2054, name: 'Be able to use call and apply Function built-in methods' },
+        { id: 2041, name: 'this scope' },
+        { id: 2042, name: 'Reference Type & losing this' },
+        { id: 2043, name: 'Be able to replace this scope' },
+        { id: 2044, name: 'Be able to use call and apply Function built-in methods' },
       ],
     },
     {
@@ -133,6 +108,22 @@ export const corejs2Template: InterviewTemplate = {
       ],
     },
     {
+      id: 2050,
+      name: 'ECMAScript Intermediate',
+      description: '10 points',
+      questions: [
+        { id: 2051, name: 'Function default parameters' },
+        { id: 2052, name: 'Know how to use spread operator for Function arguments' },
+        { id: 2053, name: 'Spread operator for Array' },
+        {
+          id: 2054,
+          name: 'Understand and able to use spread operator for Array concatenation Destructuring assignment',
+        },
+        { id: 2055, name: 'Be able to discover destructuring assignment concept' },
+        { id: 2056, name: 'String templates' },
+      ],
+    },
+    {
       id: 2100,
       name: 'ECMAScript Data Types & Expressions',
       description: '5 points',
@@ -146,32 +137,31 @@ export const corejs2Template: InterviewTemplate = {
       name: 'ECMAScript Advanced',
       description: '10 points',
       questions: [
+        { id: 2115, name: 'event loop' },
         { id: 2111, name: 'Promises' },
         { id: 2112, name: 'Promise Chaining' },
         { id: 2113, name: 'Promise static methods' },
         { id: 2114, name: 'Be able to handle errors in promises' },
-        { id: 2115, name: 'event loop' },
         { id: 2116, name: 'async/await' },
       ],
     },
     {
       id: 2120,
-      name: 'Page Lifecycle',
+      name: 'Network requests',
       description: '5 points',
       questions: [
-        { id: 2121, name: 'Parsing' },
-        { id: 2122, name: 'Reflow' },
-        { id: 2123, name: 'Repaint' },
+        { id: 2121, name: 'Fetch' },
+        { id: 2122, name: 'XMLHTTPRequest (concept) (optional)' },
       ],
     },
     {
       id: 2130,
-      name: 'Events Propagation / Preventing',
+      name: 'Page Lifecycle',
       description: '5 points',
       questions: [
-        { id: 2131, name: 'Know how to stop Event propagation' },
-        { id: 2132, name: 'Know how to prevent Event default browser behavior' },
-        { id: 2133, name: 'Understand Event delegating concept' },
+        { id: 2131, name: 'Parsing' },
+        { id: 2132, name: 'Reflow' },
+        { id: 2133, name: 'Repaint' },
       ],
     },
     {
@@ -179,6 +169,20 @@ export const corejs2Template: InterviewTemplate = {
       name: 'Web Storage API & cookies',
       description: '5 points',
       questions: [{ id: 2141, name: 'Difference between localStorage, sessionStorage, session and cookies' }],
+    },
+    {
+      id: 2150,
+      name: 'Typescript',
+      description: '10 points',
+      questions: [
+        { id: 2151, name: 'basic types' },
+        { id: 2152, name: 'enums' },
+        { id: 2153, name: 'type / interface, differences between them' },
+        { id: 2154, name: 'function types' },
+        { id: 2155, name: 'generic types (concept)' },
+        { id: 2156, name: 'utitily types (optional)' },
+        { id: 2157, name: 'typeguards (optional)' },
+      ],
     },
   ],
 };
