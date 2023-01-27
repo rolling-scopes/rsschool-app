24c702766b35360a612d9d0800b3183d1c50d9d6
nataliagulko <natalia_gulko1@epam.com>
2023-01-27 09:59:25 +0300

    refactor: add eslint for react and hooks

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
     'plugin:jest-dom/recommended',
     'plugin:testing-library/dom',
     'plugin:jest/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
   ],
   env: {
     browser: true,
@@ -21,6 +23,9 @@ module.exports = {
     '@typescript-eslint/no-non-null-assertion': 0,
     'no-console': ['error', { allow: ['warn', 'error'] }],
     '@next/next/no-img-element': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/display-name': 0,
+    'react/no-unescaped-entities': 0,
   },
   overrides: [
     {
@@ -29,4 +34,9 @@ module.exports = {
       extends: ['plugin:testing-library/react'],
     },
   ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
