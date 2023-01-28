446a45168437342ff9525d8f98dc07d6d684b51a
nataliagulko <natalia_gulko1@epam.com>
2023-01-28 11:09:26 +0300

    refactor: tasks

--- client/src/pages/admin/tasks.tsx
+++ client/src/pages/admin/tasks.tsx
@@ -2,7 +2,7 @@ import { Button, Checkbox, Form, Row, Col, Input, Collapse, Layout, message, Sel
 import withSession, { Session } from 'components/withSession';
 import { boolIconRenderer, stringSorter, tagsRenderer, getColumnSearchProps } from 'components/Table';
 import { union } from 'lodash';
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { useAsync } from 'react-use';
 import { getCoursesProps as getServerSideProps } from 'modules/Course/data/getCourseProps';
 import { githubRepoUrl, urlPattern } from 'services/validators';
@@ -91,162 +91,6 @@ function Page(props: Props) {
     [modalData, modalAction, modalLoading, dataCriteria],
   );
 
-  const renderModal = useCallback(() => {
-    const addCriteria = (criteria: CriteriaDto) => {
-      const newDataCriteria = [...dataCriteria, criteria];
-      setDataCriteria(addKeyAndIndex(newDataCriteria));
-    };
-
-    const addJSONtoCriteria = (criteria: CriteriaDto[]) => {
-      const oldCriteria = dataCriteria;
-      const newCriteria = [...oldCriteria, ...criteria];
-      setDataCriteria(addKeyAndIndex(newCriteria));
-    };
-
-    const allTags = union(...data.map(d => d.tags || []));
-    const allSkills = union(
-      data
-        .map(d => d.skills || [])
-        .concat(SKILLS)
-        .flat()
-        .sort(),
-    );
-    return (
-      <ModalForm
-        data={modalData}
-        title="Task"
-        submit={handleModalSubmit}
-        cancel={() => {
-          setModalData(null);
-          setDataCriteria([]);
-        }}
-        onChange={setModalValues}
-        getInitialValues={getInitialValues}
-        loading={modalLoading}
-      >
-        <Row gutter={24}>
-          <Col span={12}>
-            <Form.Item name="name" label="Name" rules={[{ required: true, message: 'Please enter task name' }]}>
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item name="type" label="Task Type" rules={[{ required: true, message: 'Please select a type' }]}>
-              <Select>
-                {TASK_TYPES.map(({ id, name }) => (
-                  <Select.Option key={id} value={id}>
-                    {name}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row gutter={24}>
-          <Col span={12}>
-            <Form.Item
-              name="discipline"
-              label="Discipline"
-              rules={[{ required: true, message: 'Please select a discipline' }]}
-            >
-              <Select>
-                {disciplines.map(({ id, name }) => (
-                  <Select.Option key={id} value={id}>
-                    {name}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item name="tags" label="Tags">
-              <Select mode="tags">
-                {allTags.map(tag => (
-                  <Select.Option key={tag} value={tag}>
-                    {tag}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Form.Item>
-          </Col>
-        </Row>
-
-        <Form.Item
-          name="descriptionUrl"
-          label="Description URL"
-          rules={[
-            {
-              required: true,
-              message: 'Please enter description URL',
-            },
-            {
-              message: 'Please enter valid URL',
-              pattern: urlPattern,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item name="description" label="Summary">
-          <Input />
-        </Form.Item>
-
-        <Row gutter={24}>
-          <Col span={24}>
-            <Form.Item name="skills" label="Skills">
-              <Select mode="tags">
-                {allSkills.map(skill => (
-                  <Select.Option key={skill} value={skill}>
-                    {skill}
-                  </Select.Option>
-                ))}
-              </Select>
-            </Form.Item>
-          </Col>
-        </Row>
-        <Collapse>
-          <Collapse.Panel header="Criteria For Cross-Check Task" key="1" forceRender>
-            <Form.Item label="Criteria For Cross-Check">
-              <UploadCriteriaJSON onLoad={addJSONtoCriteria} />
-            </Form.Item>
-            <AddCriteriaForCrossCheck onCreate={addCriteria} />
-            {dataCriteria.length !== 0 ? (
-              <>
-                <Divider />
-                <EditableTable dataCriteria={dataCriteria} setDataCriteria={setDataCriteria} />
-                <ExportJSONButton dataCriteria={dataCriteria} />
-              </>
-            ) : null}
-          </Collapse.Panel>
-          <Collapse.Panel header="Github" key="2" forceRender>
-            <Form.Item name="githubPrRequired" label="" valuePropName="checked">
-              <Checkbox>Pull Request required</Checkbox>
-            </Form.Item>
-            <Form.Item name="sourceGithubRepoUrl" label="Source Repo Url" rules={[{ pattern: githubRepoUrl }]}>
-              <Input placeholder="https://github.com/rolling-scopes-school/task1" />
-            </Form.Item>
-            <Form.Item name="githubRepoName" label="Expected Repo Name">
-              <Input placeholder="task1" />
-            </Form.Item>
-          </Collapse.Panel>
-          <Collapse.Panel header="JSON Attributes" key="3" forceRender>
-            <Form.Item
-              name="attributes"
-              rules={[
-                {
-                  validator: async (_, value: string) => (value ? JSON.parse(value) : Promise.resolve()),
-                  message: 'Invalid json',
-                },
-              ]}
-            >
-              <Input.TextArea rows={6} />
-            </Form.Item>
-          </Collapse.Panel>
-        </Collapse>
-      </ModalForm>
-    );
-  }, [data, modalData, handleModalSubmit, modalLoading, disciplines, dataCriteria]);
-
   return (
     <AdminPageLayout title="Manage Tasks" session={props.session} loading={loading} courses={props.courses}>
       <Content style={{ margin: 8 }}>
@@ -262,7 +106,17 @@ function Page(props: Props) {
           columns={getColumns(handleEditItem)}
         />
       </Content>
-      {renderModal()}
+      <TaskModal
+        data={data}
+        dataCriteria={dataCriteria}
+        disciplines={disciplines}
+        handleModalSubmit={handleModalSubmit}
+        modalData={modalData}
+        modalLoading={modalLoading}
+        setDataCriteria={setDataCriteria}
+        setModalData={setModalData}
+        setModalValues={setModalValues}
+      />
     </AdminPageLayout>
   );
 }
@@ -353,6 +207,188 @@ function getColumns(handleEditItem: any) {
       render: (_: any, record: TaskDto) => <a onClick={() => handleEditItem(record)}>Edit</a>,
     },
   ];
+}
+
+type ModalProps = {
+  data: TaskDto[];
+  modalData: ModalData;
+  dataCriteria: CriteriaDto[];
+  modalLoading: boolean;
+  disciplines: DisciplineDto[];
+  setDataCriteria: (c: CriteriaDto[]) => void;
+  handleModalSubmit: (values: any) => Promise<void>;
+  setModalData: (d: ModalData) => void;
+  setModalValues: (v: any) => void;
+};
+
+function TaskModal({
+  data,
+  dataCriteria,
+  modalData,
+  modalLoading,
+  disciplines,
+  setDataCriteria,
+  handleModalSubmit,
+  setModalData,
+  setModalValues,
+}: ModalProps) {
+  const addCriteria = (criteria: CriteriaDto) => {
+    const newDataCriteria = [...dataCriteria, criteria];
+    setDataCriteria(addKeyAndIndex(newDataCriteria));
+  };
+
+  const addJSONtoCriteria = (criteria: CriteriaDto[]) => {
+    const oldCriteria = dataCriteria;
+    const newCriteria = [...oldCriteria, ...criteria];
+    setDataCriteria(addKeyAndIndex(newCriteria));
+  };
+
+  const allTags = useMemo(() => union(...data.map(d => d.tags || [])), [data]);
+  const allSkills = useMemo(
+    () =>
+      union(
+        data
+          .map(d => d.skills || [])
+          .concat(SKILLS)
+          .flat()
+          .sort(),
+      ),
+    [data],
+  );
+  return (
+    <ModalForm
+      data={modalData}
+      title="Task"
+      submit={handleModalSubmit}
+      cancel={() => {
+        setModalData(null);
+        setDataCriteria([]);
+      }}
+      onChange={setModalValues}
+      getInitialValues={getInitialValues}
+      loading={modalLoading}
+    >
+      <Row gutter={24}>
+        <Col span={12}>
+          <Form.Item name="name" label="Name" rules={[{ required: true, message: 'Please enter task name' }]}>
+            <Input />
+          </Form.Item>
+        </Col>
+        <Col span={12}>
+          <Form.Item name="type" label="Task Type" rules={[{ required: true, message: 'Please select a type' }]}>
+            <Select>
+              {TASK_TYPES.map(({ id, name }) => (
+                <Select.Option key={id} value={id}>
+                  {name}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row gutter={24}>
+        <Col span={12}>
+          <Form.Item
+            name="discipline"
+            label="Discipline"
+            rules={[{ required: true, message: 'Please select a discipline' }]}
+          >
+            <Select>
+              {disciplines.map(({ id, name }) => (
+                <Select.Option key={id} value={id}>
+                  {name}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Col>
+        <Col span={12}>
+          <Form.Item name="tags" label="Tags">
+            <Select mode="tags">
+              {allTags.map(tag => (
+                <Select.Option key={tag} value={tag}>
+                  {tag}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Form.Item
+        name="descriptionUrl"
+        label="Description URL"
+        rules={[
+          {
+            required: true,
+            message: 'Please enter description URL',
+          },
+          {
+            message: 'Please enter valid URL',
+            pattern: urlPattern,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item name="description" label="Summary">
+        <Input />
+      </Form.Item>
+
+      <Row gutter={24}>
+        <Col span={24}>
+          <Form.Item name="skills" label="Skills">
+            <Select mode="tags">
+              {allSkills.map(skill => (
+                <Select.Option key={skill} value={skill}>
+                  {skill}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Col>
+      </Row>
+      <Collapse>
+        <Collapse.Panel header="Criteria For Cross-Check Task" key="1" forceRender>
+          <Form.Item label="Criteria For Cross-Check">
+            <UploadCriteriaJSON onLoad={addJSONtoCriteria} />
+          </Form.Item>
+          <AddCriteriaForCrossCheck onCreate={addCriteria} />
+          {dataCriteria.length !== 0 ? (
+            <>
+              <Divider />
+              <EditableTable dataCriteria={dataCriteria} setDataCriteria={setDataCriteria} />
+              <ExportJSONButton dataCriteria={dataCriteria} />
+            </>
+          ) : null}
+        </Collapse.Panel>
+        <Collapse.Panel header="Github" key="2" forceRender>
+          <Form.Item name="githubPrRequired" label="" valuePropName="checked">
+            <Checkbox>Pull Request required</Checkbox>
+          </Form.Item>
+          <Form.Item name="sourceGithubRepoUrl" label="Source Repo Url" rules={[{ pattern: githubRepoUrl }]}>
+            <Input placeholder="https://github.com/rolling-scopes-school/task1" />
+          </Form.Item>
+          <Form.Item name="githubRepoName" label="Expected Repo Name">
+            <Input placeholder="task1" />
+          </Form.Item>
+        </Collapse.Panel>
+        <Collapse.Panel header="JSON Attributes" key="3" forceRender>
+          <Form.Item
+            name="attributes"
+            rules={[
+              {
+                validator: async (_, value: string) => (value ? JSON.parse(value) : Promise.resolve()),
+                message: 'Invalid json',
+              },
+            ]}
+          >
+            <Input.TextArea rows={6} />
+          </Form.Item>
+        </Collapse.Panel>
+      </Collapse>
+    </ModalForm>
+  );
 }
 
 function getInitialValues(modalData: Partial<TaskDto>) {
