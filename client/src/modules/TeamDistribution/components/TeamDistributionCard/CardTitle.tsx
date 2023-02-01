2c51ca0ffc102ae7c2b11b2ee7c71fbe8cac9614
Valery <valerydluski@gmail.com>
2023-02-01 12:30:50 +0100

    feat: add status badge

--- client/src/modules/TeamDistribution/components/TeamDistributionCard/CardTitle.tsx
+++ client/src/modules/TeamDistribution/components/TeamDistributionCard/CardTitle.tsx
@@ -1,7 +1,6 @@
-import { Col, Row, Space, Typography } from 'antd';
-import { TeamOutlined } from '@ant-design/icons';
-
-import { TeamDistributionDto } from 'api';
+import { Col, Row, Space, Tag, Typography } from 'antd';
+import { TeamOutlined, ClockCircleOutlined } from '@ant-design/icons';
+import { TeamDistributionDto, TeamDistributionDtoRegistrationStatusEnum } from 'api';
 import { DistributionPeriod } from './DistributionPeriod';
 
 type Props = {
@@ -11,6 +10,21 @@ type Props = {
 const { Text, Title } = Typography;
 
 export function CardTitle({ distribution }: Props) {
+  const renderTag = () => {
+    switch (distribution.registrationStatus) {
+      case TeamDistributionDtoRegistrationStatusEnum.Distributed:
+        return (
+          <Tag icon={<ClockCircleOutlined />} color="green">
+            distributed
+          </Tag>
+        );
+      case TeamDistributionDtoRegistrationStatusEnum.Completed:
+        return <Tag icon={<ClockCircleOutlined />}>without team</Tag>;
+      default:
+        return null;
+    }
+  };
+
   return (
     <Row gutter={24} wrap justify="space-between">
       <Col>
@@ -18,11 +32,14 @@ export function CardTitle({ distribution }: Props) {
       </Col>
       <Col>
         <Space size="middle" wrap>
-          {distribution.minTotalScore !== 0 && (
-            <Text style={{ fontSize: 14 }} type="secondary">
-              Min score {distribution.minTotalScore}
-            </Text>
-          )}
+          {distribution.registrationStatus === TeamDistributionDtoRegistrationStatusEnum.Completed ||
+          distribution.registrationStatus === TeamDistributionDtoRegistrationStatusEnum.Distributed
+            ? renderTag()
+            : distribution.minTotalScore !== 0 && (
+                <Text style={{ fontSize: 14 }} type="secondary">
+                  Min score {distribution.minTotalScore}
+                </Text>
+              )}
           <Text style={{ fontSize: 14 }} type="secondary">
             <TeamOutlined style={{ marginRight: 8 }} />
             {distribution.strictTeamSize} members
