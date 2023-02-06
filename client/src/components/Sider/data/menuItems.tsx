b016f6b1ed4fcba5a6753a8b2070ccdae43ce7de
Valery <valerydluski@gmail.com>
2023-02-06 13:28:05 +0100

    fix: restriction of access to the page of users and tasks

--- client/src/components/Sider/data/menuItems.tsx
+++ client/src/components/Sider/data/menuItems.tsx
@@ -163,7 +163,7 @@ const courseManagementMenuItems: CourseManagementMenuItemsData[] = [
     name: 'Course Users',
     key: 'courseUsers',
     getUrl: (course: Course) => `/course/admin/users?course=${course.alias}`,
-    courseAccess: isAdmin,
+    courseAccess: some(isCourseManager, isAdmin),
   },
   {
     name: 'Cross-Сheck Table',
