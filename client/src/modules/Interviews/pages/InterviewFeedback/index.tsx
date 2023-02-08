6a491d6e696ae94f00eb321ef09b06bbdd169e12
Valery <valerydluski@gmail.com>
2023-02-08 19:26:57 +0100

    fix: remove useeffect

--- client/src/modules/Interviews/pages/InterviewFeedback/index.tsx
+++ client/src/modules/Interviews/pages/InterviewFeedback/index.tsx
@@ -7,7 +7,7 @@ import { InputType, templates } from 'data/interviews';
 import _ from 'lodash';
 import { SessionContext } from 'modules/Course/contexts';
 import { useRouter } from 'next/router';
-import { Fragment, useContext, useEffect, useMemo, useState } from 'react';
+import { Fragment, useContext, useMemo, useState } from 'react';
 import { CourseService } from 'services/course';
 import type { PageProps } from './getServerSideProps';
 
