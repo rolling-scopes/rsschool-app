93dfb09ae95357c4c487637a4db2c1471a014d24
Oleg Vaskevich <oleg.vaskevich.it@gmail.com>
2023-02-11 23:40:30 +0300

    fix: add epam tooltip

--- client/src/modules/Registry/constants/index.ts
+++ client/src/modules/Registry/constants/index.ts
@@ -5,6 +5,8 @@ export const DEFAULT_DOUBLE_COLUMN_SIZES = { xs: 24, sm: 24, md: 12, lg: 12, xl:
 export const DEFAULT_ROW_GUTTER = 24;
 
 export const TEXT_EMAIL_TOOLTIP = 'No spam e-mails. Only for course purposes.';
+export const TEXT_EPAM_EMAIL_TOOLTIP =
+  'If you are EPAM employee, please specify your email to avoid some manual processes later';
 export const TEXT_LOCATION_STUDENT_TOOLTIP =
   'We need your location for understanding the audience and for mentor distribution.';
 export const RSSCHOOL_BOT_LINK = 'https://t.me/rsschool_bot?start';
