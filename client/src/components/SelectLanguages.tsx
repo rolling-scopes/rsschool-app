a0af6d3e84bfa0bfbc26a8b05b232a78ba14c46b
Oleg Vaskevich <oleg.vaskevich.it@gmail.com>
2023-03-15 12:48:20 +0300

    fix: sort langauges by name

--- client/src/components/SelectLanguages.tsx
+++ client/src/components/SelectLanguages.tsx
@@ -3,7 +3,7 @@ import { useMemo } from 'react';
 import { UpdateUserDtoLanguagesEnum } from 'api';
 
 export function SelectLanguages({ placeholder = 'Select languages', ...props }: SelectProps) {
-  const languages = useMemo(() => Object.values(UpdateUserDtoLanguagesEnum).sort(), []);
+  const languages = useMemo(() => Object.values(UpdateUserDtoLanguagesEnum).sort(languagesSorter), []);
 
   return (
     <Select
@@ -22,4 +22,15 @@ export function getLanguageName(language: UpdateUserDtoLanguagesEnum) {
   const languageNames = new Intl.DisplayNames(['en'], { type: 'language' });
 
   return languageNames.of(language);
+}
+
+function languagesSorter(a: UpdateUserDtoLanguagesEnum, b: UpdateUserDtoLanguagesEnum) {
+  const aName = getLanguageName(a);
+  const bName = getLanguageName(b);
+
+  if (!aName || !bName) {
+    return 0;
+  }
+
+  return aName.localeCompare(bName, 'en');
 }
