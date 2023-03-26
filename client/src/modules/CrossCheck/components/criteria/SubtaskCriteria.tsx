12ac60f4e8313ab6065781484e9e95b77753dac4
Dasha Litvinova <86688910+andreikovna@users.noreply.github.com>
2023-03-26 23:04:42 +0300

    Feat/update cross check form  (#2082)
    * feat: refactor and change stars to slider
    
    * refactor: according to review
    
    ---------
    
    Co-authored-by: = <litvinonada@solit.by>
    Co-authored-by: Valery <57412523+valerydluski@users.noreply.github.com>

--- client/src/modules/CrossCheck/components/criteria/SubtaskCriteria.tsx
+++ client/src/modules/CrossCheck/components/criteria/SubtaskCriteria.tsx
@@ -1,76 +1,81 @@
-import { Input, Rate, Typography } from 'antd';
+import { Input, Typography, InputNumber, Slider } from 'antd';
+import { useMemo } from 'react';
 import { CrossCheckCriteriaData } from 'services/course';
 
-import { CommentState, CountState } from '../CrossCheckCriteriaForm';
-
 const { TextArea } = Input;
+const { Text } = Typography;
+
 interface SubtaskCriteriaProps {
-  criteriaComment: CommentState[];
   subtaskData: CrossCheckCriteriaData;
-  countStar: CountState[];
-  updateCountStar: (event: number, max: number, key: string) => void;
-  updateComment: (value: string, key: string) => void;
+  updateCriteriaData: (updatedEntry: CrossCheckCriteriaData) => void;
 }
 
-const NUMBER_OF_STARS = 5;
+export function SubtaskCriteria({ subtaskData, updateCriteriaData }: SubtaskCriteriaProps) {
+  const maxScore = subtaskData.max;
+  const comment = subtaskData.textComment;
+  const criteriaScore = subtaskData.point;
 
-export default function SubtaskCriteria({
-  criteriaComment,
-  subtaskData,
-  countStar,
-  updateCountStar,
-  updateComment,
-}: SubtaskCriteriaProps) {
-  const criteriaScore = (countStar.find(item => item.key === subtaskData.key)?.point as number) ?? 0;
+  const updateSubtaskData = (newData: number | string) => {
+    const updatedEntry =
+      typeof newData === 'string' ? { ...subtaskData, textComment: newData } : { ...subtaskData, point: newData };
+    updateCriteriaData(updatedEntry);
+  };
+
+  const statusCommentRequired = useMemo(() => {
+    if (criteriaScore !== undefined && maxScore) {
+      const commentNotMatchRules = comment ? comment.length < 10 : true;
+      return criteriaScore < maxScore && commentNotMatchRules;
+    }
+    return false;
+  }, [criteriaScore, comment, maxScore]);
 
   return (
-    <div style={{ border: '1px solid #F5F5F5', margin: '24px 0' }} key={subtaskData.key}>
+    <div style={{ border: '1px solid #F5F5F5', margin: '24px 0' }}>
       <div
         style={{
-          display: 'block',
-          margin: '0',
-          fontSize: '14px',
           background: '#FAFAFA',
           borderBottom: '1px solid #F5F5F5',
           padding: '14px 12px',
         }}
       >
-        <Typography.Text>{subtaskData.text}</Typography.Text>
+        <Text>{subtaskData.text}</Text>
       </div>
 
       <div
         style={{
           display: 'flex',
           padding: '13px 12px',
-          fontSize: '14px',
           justifyContent: 'space-between',
           alignItems: 'center',
         }}
       >
-        <Typography.Text>Quality of execution:</Typography.Text>
-        <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
-          <Rate
-            value={(criteriaScore / (subtaskData.max as number)) * NUMBER_OF_STARS}
-            allowClear
-            allowHalf
-            defaultValue={0}
-            count={5}
-            onChange={event => {
-              updateCountStar(event, subtaskData.max as number, subtaskData.key);
-            }}
+        <Text>
+          Quality of execution:
+          <br />
+          (Max {maxScore} points for criteria)
+        </Text>
+        <div style={{ width: '60%', display: 'flex', gap: '10px' }}>
+          <Slider
+            style={{ width: '70%' }}
+            min={0}
+            max={maxScore}
+            onChange={updateSubtaskData}
+            value={criteriaScore ?? 0}
           />
-          <Typography.Text>
-            {criteriaScore} / {subtaskData.max}
-          </Typography.Text>
+          <InputNumber min={0} max={maxScore} value={criteriaScore ?? 0} onChange={updateSubtaskData} />
         </div>
       </div>
-      <TextArea
-        value={criteriaComment.find(item => item.key === subtaskData.key)?.textComment}
-        placeholder="Comment about this criteria for task"
-        rows={2}
-        style={{ width: 'calc(100% - 24px)', display: 'block', margin: '0 auto 16px' }}
-        onInput={event => updateComment((event.target as HTMLInputElement).value, subtaskData.key)}
-      />
+      <div style={{ padding: '0 12px' }}>
+        <TextArea
+          style={{ border: statusCommentRequired ? '1px red solid' : '' }}
+          value={subtaskData.textComment}
+          rows={2}
+          onInput={event => updateSubtaskData((event.target as HTMLInputElement).value)}
+        />
+        <div style={{ height: '20px' }}>
+          {statusCommentRequired && <Text type="danger">Please leave a detailed comment</Text>}
+        </div>
+      </div>
     </div>
   );
 }
