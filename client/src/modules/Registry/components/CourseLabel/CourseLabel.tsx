3e25ff2fbaf5df0629ec945985975eceae3a9b7d
Andrei Palchys <andrei@palchys.com>
2023-05-23 00:34:36 +0300

    fix: remove tag temporary

--- client/src/modules/Registry/components/CourseLabel/CourseLabel.tsx
+++ client/src/modules/Registry/components/CourseLabel/CourseLabel.tsx
@@ -1,4 +1,3 @@
-import { Tag } from 'antd';
 import { CourseDto } from 'api';
 import { CourseIcon } from 'components/Icons';
 import { formatMonthFriendly } from 'services/formatter';
@@ -8,16 +7,13 @@ type Props = {
 };
 
 export function CourseLabel({ course }: Props) {
-  const { planned, discipline, name, startDate } = course;
+  const { discipline, name, startDate } = course;
   const disciplineName = discipline?.name ? `${discipline.name}, ` : '';
   const courseInfo = ` ${name} (${disciplineName}${formatMonthFriendly(startDate)}) `;
-  const tag = planned ? <Tag color="orange">Planned</Tag> : <Tag color="green">In Progress</Tag>;
-
   return (
     <>
       <CourseIcon course={course} />
       {courseInfo}
-      {tag}
     </>
   );
 }
