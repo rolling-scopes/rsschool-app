2d579507d578ad47dd1880987aa83077376af988
Andrei Palchys <andrei@palchys.com>
2023-05-23 01:00:25 +0300

    feat: switch to neon database

--- server/src/dataSourceOptions.ts
+++ server/src/dataSourceOptions.ts
@@ -4,8 +4,9 @@ import { DataSourceOptions } from 'typeorm';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
+  ssl: process.env.RS_ENV === 'staging' ? true : undefined,
   host: process.env.RSSHCOOL_API_PG_HOST,
-  port: 5432,
+  port: process.env.RS_ENV !== 'staging' ? 5432 : undefined,
   username: process.env.RSSHCOOL_API_PG_USERNAME,
   password: process.env.RSSHCOOL_API_PG_PASSWORD,
   database: process.env.RSSHCOOL_API_PG_DATABASE,
