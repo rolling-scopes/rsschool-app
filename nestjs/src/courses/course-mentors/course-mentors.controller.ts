908593697275c956d2c42c135414237488268829
Vadzim Antonau <vanet@tut.by>
2023-08-08 21:15:15 +0300

    feat(course-mentors): add role guards to endpoints

--- nestjs/src/courses/course-mentors/course-mentors.controller.ts
+++ nestjs/src/courses/course-mentors/course-mentors.controller.ts
@@ -1,16 +1,21 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { CourseMentorsService } from './course-mentors.service';
+import { CourseRole, DefaultGuard, RequiredRoles, Role, RoleGuard } from 'src/auth';
 
 @Controller('course/:courseId/mentors')
 export class CourseMentorsController {
   constructor(private readonly courseMentorsService: CourseMentorsService) {}
 
   @Get()
+  @UseGuards(DefaultGuard, RoleGuard)
+  @RequiredRoles([Role.Admin, CourseRole.Manager, CourseRole.Supervisor])
   findAll(@Param('courseId') courseId: string) {
     return this.courseMentorsService.findAll(+courseId);
   }
 
   @Get('details')
+  @UseGuards(DefaultGuard, RoleGuard)
+  @RequiredRoles([Role.Admin, CourseRole.Manager, CourseRole.Supervisor])
   getMentorsDetails(@Param('courseId') courseId: string) {
     return this.courseMentorsService.getMentorsWithStats(+courseId);
   }
