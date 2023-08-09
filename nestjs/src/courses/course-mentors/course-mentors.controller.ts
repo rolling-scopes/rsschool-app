61484968eaee2a6a0201e39d711f4f4281776413
Vadzim Antonau <vanet@tut.by>
2023-08-09 10:02:02 +0300

    feat(course-mentors): migrate mentors/details/csv

--- nestjs/src/courses/course-mentors/course-mentors.controller.ts
+++ nestjs/src/courses/course-mentors/course-mentors.controller.ts
@@ -1,7 +1,9 @@
-import { CacheTTL, Controller, Get, Param, ParseIntPipe, UseGuards } from '@nestjs/common';
+import { CacheTTL, Controller, Get, Param, ParseIntPipe, Res, UseGuards } from '@nestjs/common';
 import { CourseMentorsService } from './course-mentors.service';
 import { CourseGuard, CourseRole, DefaultGuard, RequiredRoles, Role, RoleGuard } from '../../auth';
 import { DEFAULT_CACHE_TTL } from 'src/constants';
+import { parseAsync, transforms } from 'json2csv';
+import { Response } from 'express';
 
 @Controller('course/:courseId/mentors')
 export class CourseMentorsController {
@@ -12,6 +14,19 @@ export class CourseMentorsController {
   @RequiredRoles([Role.Admin, CourseRole.Manager, CourseRole.Supervisor])
   getMentorsDetails(@Param('courseId', ParseIntPipe) courseId: number) {
     return this.courseMentorsService.getMentorsWithStats(courseId);
+  }
+
+  @Get('details/csv')
+  @UseGuards(DefaultGuard, RoleGuard)
+  @RequiredRoles([Role.Admin, CourseRole.Manager, CourseRole.Supervisor])
+  async getMentorsDetailsCsv(@Param('courseId', ParseIntPipe) courseId: number, @Res() res: Response) {
+    const results = await this.courseMentorsService.getMentorsWithStats(courseId);
+    const parsedData = await parseAsync(results, { transforms: [transforms.flatten()] });
+
+    res.setHeader('Content-Type', 'text/csv');
+    res.setHeader('Content-disposition', `filename=mentors.csv`);
+
+    res.end(parsedData);
   }
 
   @Get('search/:searchText')
