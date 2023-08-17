7a80c13a68fc257cd7309db7328d18d6cd966bec
Vadzim Antonau <vanet@tut.by>
2023-08-17 11:15:23 +0300

    fix(server): delete unused import in mentor service

--- server/src/services/mentor.service.ts
+++ server/src/services/mentor.service.ts
@@ -6,7 +6,6 @@ import { createMentorStudentPairs } from '../rules/mentors';
 import { OperationResult } from './operationResult';
 import { Mentor } from '../models';
 import { ILogger } from '../logger';
-import { MentorDetails } from './course.service';
 import { sendNotification } from './notification.service';
 
 export class MentorService {
