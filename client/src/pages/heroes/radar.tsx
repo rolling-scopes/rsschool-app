543904c6afb80731d8d9be084a9c45ae6222c3fc
Vadzim Antonau <vanet@tut.by>
2023-08-17 10:06:54 +0300

    feat(heroes-radar): add data fetching and basic test layout

--- client/src/pages/heroes/radar.tsx
+++ client/src/pages/heroes/radar.tsx
@@ -1,6 +1,10 @@
 import { PageLayout } from 'components/PageLayout';
 import withSession, { Session } from 'components/withSession';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Masonry from 'react-masonry-css';
+import css from 'styled-jsx/css';
+import { GratitudesApi, HeroesRadarDto } from 'api';
+import { getFullName } from 'utils/text-utils';
 
 type Props = {
   session: Session;
@@ -8,12 +12,57 @@ type Props = {
 
 function Page(props: Props) {
   const [loading, setLoading] = useState(false);
+  const [heroes, setHeroes] = useState<HeroesRadarDto[]>([]);
+  const gratitudeApi = new GratitudesApi();
+
+  useEffect(() => {
+    const getHeroes = async () => {
+      setLoading(true);
+      const { data: heroes } = await gratitudeApi.getHeroesRadar();
+      setHeroes(heroes);
+      setLoading(false);
+    };
+    getHeroes();
+  }, []);
 
   return (
     <PageLayout loading={loading} title="Heroes Radar" githubId={props.session.githubId}>
-
+      <Masonry
+        breakpointCols={{
+          default: 4,
+          1100: 3,
+          700: 2,
+          500: 1,
+        }}
+        className={masonryClassName}
+        columnClassName={masonryColumnClassName}
+      >
+        {heroes.map(({ githubId, firstName, lastName, total, badges }) => (
+          <div key={githubId}>
+            <span>{getFullName({firstName, lastName, githubId})} - {total}</span>
+            {badges.map(badge => (<span key={badge.badgeId}>{badge.badgeId} {badge.badgeCount} </span>))}
+          </div>
+        ))}
+      </Masonry>
+      {masonryStyles}
+      {masonryColumnStyles}
     </PageLayout>
   );
 }
+
+const gapSize = 16;
+const { className: masonryClassName, styles: masonryStyles } = css.resolve`
+  div {
+    display: flex;
+    margin-left: -${gapSize}px;
+    width: auto;
+  }
+`;
+const { className: masonryColumnClassName, styles: masonryColumnStyles } = css.resolve`
+  div {
+    padding-left: ${gapSize}px;
+    background-clip: padding-box;
+  }
+`;
 
 export default withSession(Page);
