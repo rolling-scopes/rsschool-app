fa165cd1f27f6f8d26fc8b149b4f41cc5fcffefa
Vadzim Antonau <vanet@tut.by>
2023-08-18 21:33:29 +0300

    fix(nestjs): naming in UsedCourseDto

--- nestjs/src/courses/dto/used-course.dto.ts
+++ nestjs/src/courses/dto/used-course.dto.ts
@@ -2,9 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class UsedCourseDto {
-  constructor(obj: { name: string; isActive: boolean }) {
-    this.isActive = obj.isActive;
-    this.name = obj.name;
+  constructor(course: { name: string; isActive: boolean }) {
+    this.isActive = course.isActive;
+    this.name = course.name;
   }
 
   @IsString()
