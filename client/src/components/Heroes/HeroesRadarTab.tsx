a27377e2d4aed407a40d25351aad27f4606818d0
Vadzim Antonau <vanet@tut.by>
2023-08-29 17:46:28 +0300

    feat(heroes-radar): add countries select to the form

--- client/src/components/Heroes/HeroesRadarTab.tsx
+++ client/src/components/Heroes/HeroesRadarTab.tsx
@@ -1,6 +1,6 @@
 import { Button, Checkbox, Form, Select, Space, TableProps } from 'antd';
 import HeroesRadarTable from './HeroesRadarTable';
-import { HeroesRadarDto, GratitudesApi, HeroRadarDto } from 'api';
+import { HeroesRadarDto, GratitudesApi, HeroRadarDto, CountryDto } from 'api';
 import { IPaginationInfo } from 'common/types/pagination';
 import { useState, useEffect, useCallback } from 'react';
 import { Course } from 'services/models';
@@ -9,6 +9,7 @@ import { onlyDefined } from 'utils/onlyDefined';
 export type HeroesRadarFormProps = {
   courseId?: number;
   notActivist?: boolean;
+  countryName?: string;
 };
 
 type GetHeroesProps = HeroesRadarFormProps & Partial<IPaginationInfo>;
@@ -24,20 +25,27 @@ function HeroesRadarTab({ setLoading, courses }: { setLoading: (arg: boolean) =>
     content: [],
     pagination: { current: initialPage, pageSize: initialPageSize, itemCount: 0, total: 0, totalPages: 0 },
   });
+  const [countries, setCountries] = useState<CountryDto[]>([]);
   const [form] = Form.useForm();
   const [formData, setFormData] = useState<HeroesRadarFormProps>(form.getFieldsValue());
   const [formLayout, setFormLayout] = useState<LayoutType>('inline');
   const gratitudeApi = new GratitudesApi();
+
+  const getCountries = async () => {
+    const { data } = await gratitudeApi.getHeroesCountries();
+    setCountries(data)
+  }
 
   const getHeroes = async ({
     current = initialPage,
     pageSize = initialPageSize,
     courseId,
     notActivist,
+    countryName
   }: GetHeroesProps) => {
     try {
       setLoading(true);
-      const { data } = await gratitudeApi.getHeroesRadar(current, pageSize, courseId, notActivist);
+      const { data } = await gratitudeApi.getHeroesRadar(current, pageSize, courseId, notActivist, countryName);
       setHeroes(data);
     } finally {
       setLoading(false);
@@ -46,6 +54,7 @@ function HeroesRadarTab({ setLoading, courses }: { setLoading: (arg: boolean) =>
 
   useEffect(() => {
     getHeroes(initialQueryParams);
+    getCountries();
   }, []);
 
   const handleSubmit = useCallback(async (formData: HeroesRadarFormProps) => {
@@ -74,6 +83,15 @@ function HeroesRadarTab({ setLoading, courses }: { setLoading: (arg: boolean) =>
       <Form layout={formLayout} form={form} onFinish={handleSubmit} style={{ marginBottom: 24 }}>
         <Form.Item name={'courseId'} label="Courses" style={{ minWidth: 260, marginBottom: 16 }}>
           <Select options={courses.map(({ id, name }) => ({ value: id, label: name }))} />
+        </Form.Item>
+        <Form.Item name={'countryName'} label="Countries" style={{ minWidth: 260, marginBottom: 16 }}>
+          <Select>
+            {countries.map(({countryName}) => (
+              <Select.Option key={countryName} value={countryName}>
+                {countryName}
+              </Select.Option>
+            ))}
+          </Select>
         </Form.Item>
         <Form.Item name={'notActivist'} valuePropName="checked" style={{ marginBottom: 16 }}>
           <Checkbox>Show only not activists</Checkbox>
