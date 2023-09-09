facd1746a37ccdf8b37c5fceded6186497e8f198
Vadzim Antonau <vanet@tut.by>
2023-09-09 14:07:54 +0300

    feat(job-found): add methods to transform job found info to employment record and back

--- nestjs/src/profile/dto/job-found.dto.ts
+++ nestjs/src/profile/dto/job-found.dto.ts
@@ -1,3 +1,4 @@
+import { EmploymentRecord } from '@entities/user';
 import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
 import { IsBoolean, IsOptional, IsString } from 'class-validator';
@@ -19,4 +20,27 @@ export class JobFoundDto {
   @IsString()
   @Expose()
   public jobFoundOfficeLocation?: string | null;
+
+  public static jobFoundToEmploymentRecord(jobFound: JobFoundDto) {
+    return {
+      title: '',
+      dateTo: '',
+      dateFrom: '',
+      companyName: jobFound.jobFoundCompanyName ?? '',
+      officeLocation: jobFound.jobFoundOfficeLocation ?? '',
+      toPresent: true,
+    };
+  }
+
+  public static employmentRecordToJobFound(employmentRecord: EmploymentRecord | undefined) {
+    if (!employmentRecord) {
+      return null;
+    }
+
+    return {
+      jobFound: true,
+      jobFoundCompanyName: employmentRecord.companyName,
+      jobFoundOfficeLocation: employmentRecord.officeLocation,
+    };
+  }
 }
