30b8478fdd72f889b7e6f183f15b5b6f9c6c1b62
Vadzim Antonau <vanet@tut.by>
2023-09-11 13:27:44 +0300

    feat(schedule): add status column filter (#2284)
    * feat(schedule): add filter to status column at All tab
    
    * feat(schedule): add filtering by status
    
    * feat(schedule): clear status column filter when tab switched
    
    * feat(schedule): refactor FilteredTags to handle multiple filter types, add handling to close status column filter
    
    * refactor(mentor-registry): update FilteredTags
    
    * Revert "refactor(mentor-registry): update FilteredTags"
    
    This reverts commit 8359fc71d506942e2616990a557c691a17859809.
    
    * Revert "feat(schedule): refactor FilteredTags to handle multiple filter types, add handling to close status column filter"
    
    This reverts commit 2bf62bffed6b1770838f9dbf7bcd7ae56cc283e6.
    
    * feat(scheduler): implement combined filter functionality for status and type columns
    
    * feat: replace deprecated onFilterDropdownVisibleChange
    
    * refactor(schedule): rename property at getColumns
    
    * refactor(schedule): rename variable at TableView
    
    * feat(schedule): move tagFilters state to combinedFilter as filterTags
    
    * refactor(schedule): delete unused LocalStorageKeys
    
    * test(schedule): update tests for new useLocalStorage structure
    
    * refactor(schedule): fix formatting
    
    * fix(schedule): filterTags filtering condition when tab is switched
    
    * feat(schedule): add FilterTag type for tags and update regarding functionality
    
    * refactor(schedule): make CombinedFilter interface as type
    
    * refactor(schedule): fix formatting
    
    * tests(schedule): fix TableView tests

--- client/src/modules/Schedule/components/TableView/renderers.tsx
+++ client/src/modules/Schedule/components/TableView/renderers.tsx
@@ -2,13 +2,21 @@ import { Badge, Tag } from 'antd';
 import { CourseScheduleItemDto, CourseScheduleItemDtoStatusEnum } from 'api';
 import capitalize from 'lodash/capitalize';
 import { DEFAULT_TAG_COLOR_MAP, TAG_NAME_MAP } from 'modules/Schedule/constants';
-import { getTagStyle, getTaskStatusColor } from 'modules/Schedule/utils';
+import { getStatusStyle, getTagStyle, getTaskStatusColor } from 'modules/Schedule/utils';
 
 export function statusRenderer(value: CourseScheduleItemDtoStatusEnum) {
   const label = capitalize(value);
   const color = getTaskStatusColor(value);
 
   return <Badge color={color} text={label} />;
+}
+
+export function renderStatusWithStyle(statusName: CourseScheduleItemDtoStatusEnum) {
+  return (
+    <Tag style={getStatusStyle(statusName)} key={statusName}>
+      {capitalize(statusName)}
+    </Tag>
+  );
 }
 
 export function renderTagWithStyle(tagName: CourseScheduleItemDto['tag'], tagColors = DEFAULT_TAG_COLOR_MAP) {
