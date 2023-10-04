00d4620d5444ffb0312ea35d2fb2f3c9519d0a34
Vadzim Antonau <vanet@tut.by>
2023-10-04 16:08:48 +0300

    feat(schedule): update schedule mobile view with user timezone offset

--- client/src/modules/Schedule/components/MobileItemCard/MobileItemCard.tsx
+++ client/src/modules/Schedule/components/MobileItemCard/MobileItemCard.tsx
@@ -4,10 +4,19 @@ import { SwapRightOutlined } from '@ant-design/icons';
 import { coloredDateRenderer } from 'components/Table';
 import { renderTagWithStyle, statusRenderer } from '../TableView/renderers';
 import Link from 'next/link';
+import { ScheduleSettings } from 'modules/Schedule/hooks/useScheduleSettings';
+import dayjs from 'dayjs';
 
 const { Title } = Typography;
 
-export const MobileItemCard = ({ item }: { item: CourseScheduleItemDto }) => {
+export const MobileItemCard = ({
+  item,
+  timezone,
+}: {
+  item: CourseScheduleItemDto;
+  timezone: ScheduleSettings['timezone'];
+}) => {
+  const timezoneOffset = `(UTC ${dayjs().tz(timezone).format('Z')})`;
   return (
     <div style={{ padding: '12px 0px', backgroundColor: 'white', borderBottom: '1px groove' }}>
       <Row gutter={12} wrap={false}>
@@ -21,7 +30,7 @@ export const MobileItemCard = ({ item }: { item: CourseScheduleItemDto }) => {
       <Row gutter={12} wrap={false}>
         <Col flex="1">{statusRenderer(item.status)}</Col>
         <Col flex="0 0 auto">
-          {coloredDateRenderer('UTC', 'MMM D HH:mm', 'start', 'Recommended date for studying')(item.startDate, item)}
+          {coloredDateRenderer(timezone, 'MMM D HH:mm', 'start', 'Recommended date for studying')(item.startDate, item)}
           {item.endDate && (
             <>
               {' '}
@@ -29,8 +38,11 @@ export const MobileItemCard = ({ item }: { item: CourseScheduleItemDto }) => {
             </>
           )}
           {item.endDate &&
-            coloredDateRenderer('UTC', 'MMM D HH:mm', 'end', 'Recommended date for studying')(item.endDate, item)}
+            coloredDateRenderer(timezone, 'MMM D HH:mm', 'end', 'Recommended date for studying')(item.endDate, item)}
         </Col>
+      </Row>
+      <Row justify={'end'} color='secondary'>
+        {timezoneOffset}
       </Row>
     </div>
   );
