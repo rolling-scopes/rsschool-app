c32cced2ebc08febb6948d4f04a7fc42a94409d8
Vadzim Antonau <vanet@tut.by>
2023-10-16 21:40:03 +0300

    refactor: remove redundant React import

--- client/src/components/Profile/EmploymentCard.tsx
+++ client/src/components/Profile/EmploymentCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { List, Button, Form } from 'antd';
 import { ReadOutlined, FileAddOutlined } from '@ant-design/icons';
 import isEqual from 'lodash/isEqual';
