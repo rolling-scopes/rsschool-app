f46383e210137633a73fcd674d524d91824df934
Dzmitry Chervanenka <dima1772000@gmail.com>
2023-11-06 14:33:31 +0300

    fix: fix endTime calculating algorythm at getEventStatus function

--- nestjs/src/courses/course-schedule/course-schedule.service.ts
+++ nestjs/src/courses/course-schedule/course-schedule.service.ts
@@ -359,7 +359,7 @@ export class CourseScheduleService {
 
   private getEventStatus(courseEvent: CourseEvent) {
     const startTime = (courseEvent.dateTime as Date).getTime();
-    const endTime = Number(courseEvent.endTime) ?? startTime + (courseEvent.duration ?? 60) * 1000 * 60;
+    const endTime = Number(courseEvent.endTime) || startTime + (courseEvent.duration ?? 60) * 1000 * 60;
     if (endTime && endTime < Date.now()) {
       return CourseScheduleItemStatus.Archived;
     }
