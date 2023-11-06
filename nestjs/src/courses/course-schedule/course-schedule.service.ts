5329900fca277cd925d565551df243d6e6912e0b
Dzmitry Chervanenka <dima1772000@gmail.com>
2023-11-06 11:48:58 +0300

    fix: fix incorrect setting of "archived" status for events of course

--- nestjs/src/courses/course-schedule/course-schedule.service.ts
+++ nestjs/src/courses/course-schedule/course-schedule.service.ts
@@ -360,7 +360,7 @@ export class CourseScheduleService {
   private getEventStatus(courseEvent: CourseEvent) {
     const startTime = (courseEvent.dateTime as Date).getTime();
     const endTime = Number(courseEvent.endTime) ?? startTime + (courseEvent.duration ?? 60) * 1000 * 60;
-    if (endTime < Date.now()) {
+    if (endTime && endTime < Date.now()) {
       return CourseScheduleItemStatus.Archived;
     }
     if (startTime < Date.now()) {
