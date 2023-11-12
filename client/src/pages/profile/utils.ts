d8048634ed4e06751d31a150b5f747ce23eb49df
Dzmitry Chervanenka <dima1772000@gmail.com>
2023-11-12 17:48:12 +0300

    refactor:use eslint fix on Profile Page utils

--- client/src/pages/profile/utils.ts
+++ client/src/pages/profile/utils.ts
@@ -1,28 +1,28 @@
-import { StudentStats } from "common/models";
-import { CoreJsInterviewsData } from "components/Profile/CoreJsIviewsCard";
+import { StudentStats } from 'common/models';
+import { CoreJsInterviewsData } from 'components/Profile/CoreJsIviewsCard';
 
 export const hadStudentCoreJSInterview = (stats: StudentStats[]) =>
-stats.some((student: StudentStats) => student.tasks.some(({ interviewFormAnswers }) => interviewFormAnswers));
+  stats.some((student: StudentStats) => student.tasks.some(({ interviewFormAnswers }) => interviewFormAnswers));
 
 export const getStudentCoreJSInterviews = (stats: StudentStats[]) =>
-stats
-  .filter((student: StudentStats) => student.tasks.some(({ interviewFormAnswers }) => interviewFormAnswers))
-  .map(({ tasks, courseFullName, courseName, locationName }) => ({
-    courseFullName,
-    courseName,
-    locationName,
-    interviews: tasks
-      .filter(({ interviewFormAnswers }) => interviewFormAnswers)
-      .map(({ interviewFormAnswers, score, comment, interviewer, name, interviewDate }) => ({
-        score,
-        comment,
-        interviewer,
-        answers: interviewFormAnswers,
-        name,
-        interviewDate,
-      })),
-  })) as CoreJsInterviewsData[];
+  stats
+    .filter((student: StudentStats) => student.tasks.some(({ interviewFormAnswers }) => interviewFormAnswers))
+    .map(({ tasks, courseFullName, courseName, locationName }) => ({
+      courseFullName,
+      courseName,
+      locationName,
+      interviews: tasks
+        .filter(({ interviewFormAnswers }) => interviewFormAnswers)
+        .map(({ interviewFormAnswers, score, comment, interviewer, name, interviewDate }) => ({
+          score,
+          comment,
+          interviewer,
+          answers: interviewFormAnswers,
+          name,
+          interviewDate,
+        })),
+    })) as CoreJsInterviewsData[];
 
 export const checkIsProfileOwner = (githubId: string, requestedGithubId: string): boolean => {
-return githubId === requestedGithubId;
+  return githubId === requestedGithubId;
 };
