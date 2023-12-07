ef91fcc2f350efdaca715fa85fe4fb842d2d8fa7
Zoro444 <zoro.simonyan1995@gmail.com>
2023-12-07 22:57:36 +0400

    fix: add string type for optionalQueryString

--- client/src/utils/optionalQueryString.ts
+++ client/src/utils/optionalQueryString.ts
@@ -1,5 +1,7 @@
-export function optionalQueryString(value: string[] | undefined) {
+export function optionalQueryString(value: string | string[] | undefined) {
   if (Array.isArray(value)) {
     return String(value[0]).trim();
+  } else if (typeof value === 'string') {
+    return String(value).trim();
   }
 }
