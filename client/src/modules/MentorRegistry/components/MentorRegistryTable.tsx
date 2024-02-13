6204fbc1092547a93da0aeb4ca927588598a3032
Dzmitry Chervanenka <dima1772000@gmail.com>
2024-02-13 18:37:30 +0300

    refactor: remove any type from function props

--- client/src/modules/MentorRegistry/components/MentorRegistryTable.tsx
+++ client/src/modules/MentorRegistry/components/MentorRegistryTable.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Table } from 'antd';
+import { Form, Table, TablePaginationConfig } from 'antd';
 import FilteredTags from 'modules/Schedule/components/FilteredTags';
 import { FilterValue } from 'antd/lib/table/interface';
 import { MentorRegistryDto } from 'api';
@@ -16,7 +16,10 @@ type Props = {
   columns: ColumnType<MentorRegistryDto>[];
   handleTagClose: (tag: string) => void;
   handleClearAllButtonClick: () => void;
-  handleTableChange: (_: any, filters: Record<MentorsRegistryColumnKey, FilterValue | string[] | null>) => void;
+  handleTableChange: (
+    _: TablePaginationConfig,
+    filters: Record<MentorsRegistryColumnKey, FilterValue | string[] | null>,
+  ) => void;
 };
 
 export function MentorRegistryTable(props: Props) {
