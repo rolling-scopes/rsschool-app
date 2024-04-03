919c27f88eddf757c2e45874b876f6342aaacf17
Valery <valery_dluski@epam.com>
2024-04-03 13:17:38 +0200

    fix: prettier

--- server/src/migrations/1712137476312-Course.ts
+++ server/src/migrations/1712137476312-Course.ts
@@ -1,14 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Course1712137476312 implements MigrationInterface {
-    name = 'Course1712137476312'
+  name = 'Course1712137476312';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "course" ADD "certificateThreshold" integer DEFAULT '70'`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "course" ADD "certificateThreshold" integer DEFAULT '70'`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "course" DROP COLUMN "certificateThreshold"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "course" DROP COLUMN "certificateThreshold"`);
+  }
 }
