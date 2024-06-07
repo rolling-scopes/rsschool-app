10ddef0beee16163b82688cadb012cc98092e6c9
Alphajax <45921176+Alphajax@users.noreply.github.com>
2024-06-07 09:58:25 +0300

    feat: implement deleting all cross check criteria  (#2480)
    * feat: implement delete all crosscheck criterias button without functionality
    
    * feat: implement deleteAllTaskCriteria endpoint
    
    * feat: implement deleting cross check criteria for task
    
    * refactor: delete unneccessary api endpoint
    
    * refactor: fix eslint issue

---
+++ client/src/modules/CrossCheck/DeleteAllCrossCheckCriteriaButton.tsx
@@ -0,0 +1,21 @@
+import DeleteOutlined from '@ant-design/icons/DeleteOutlined';
+import { Button, Popconfirm } from 'antd';
+import { CriteriaDto } from 'api';
+
+interface Props {
+  setDataCriteria: (criteria: CriteriaDto[]) => void;
+}
+export function DeleteAllCrossCheckCriteriaButton({ setDataCriteria }: Props) {
+  const deleteAllCrossCheckCriteria = () => {
+    setDataCriteria([]);
+  };
+  return (
+    <div style={{ textAlign: 'right' }}>
+      <Popconfirm title="Are you sure you want to delete all items?" onConfirm={deleteAllCrossCheckCriteria}>
+        <Button icon={<DeleteOutlined style={{ marginRight: 5 }} />} style={{ marginTop: 15 }} danger>
+          Delete all
+        </Button>
+      </Popconfirm>
+    </div>
+  );
+}
