ba2dccc44c37c51e380ff63d29fd6ae0c5bbff86
Maksim Shylau <34455330+AlreadyBored@users.noreply.github.com>
2024-06-23 19:55:06 +0200

    feat: prohibit submission of links to private RS repos (#2493)

--- client/src/services/validators.ts
+++ client/src/services/validators.ts
@@ -8,6 +8,7 @@ export const urlPattern = /^(http|https):\/\/.+(\.[a-z]{2,10})/g;
 export const urlWithIpPattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/;
 
 export const notGithubPattern = /^((?!https:\/\/github.com\/).)*$/gi;
+export const privateRsRepoPattern = /https:\/\/github\.com\/rolling-scopes-school\//;
 
 export const githubPrUrl = /https:\/\/github.com\/(\w|\d|-)+\/(\w|\d|-)+\/pull\/(\d)+/gi;
 export const githubRepoUrl = /https:\/\/github.com\/(\w|\d|-)+\/(\w|\d|-)+/gi;
