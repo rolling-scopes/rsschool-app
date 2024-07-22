cf4df024805841f07ff387b27f21dc48a35e1254
Natalia Gulko <natalia_gulko1@epam.com>
2024-07-22 17:27:22 +0300

    chore: remove todo

--- client/src/modules/CrossCheck/constants.ts
+++ client/src/modules/CrossCheck/constants.ts
@@ -26,7 +26,6 @@ export enum EditableTableColumnsDataIndex {
   Actions = 'actions',
 }
 
-// TODO: double check and get rid of in favor of CriteriaDtoTypeEnum
 export enum TaskType {
   Title = 'title',
   Subtask = 'subtask',
