d86e6587db7a6d298055d40410b9b68a7665851c
apalchys <andrei@palchys.com>
2024-09-16 10:32:16 +0200

    fix: add fields to ExportCourseDto and filter invite/test courses

--- nestjs/src/courses/dto/export-course.dto.ts
+++ nestjs/src/courses/dto/export-course.dto.ts
@@ -4,18 +4,22 @@ export class ExportCourseDto {
   constructor(course: Course) {
     this.id = course.id;
     this.name = course.name;
+    this.fullName = course.fullName;
     this.startDate = course.startDate.toISOString();
     this.endDate = course.endDate.toISOString();
     this.alias = course.alias;
     this.discipline = course.discipline ? { id: course.discipline.id, name: course.discipline.name } : null;
     this.description = course.description;
+    this.descriptionUrl = course.descriptionUrl;
     this.registrationEndDate = course.registrationEndDate.toISOString();
   }
 
   id: number;
   name: string;
+  fullName: string;
   alias: string;
   description: string;
+  descriptionUrl: string;
   discipline: { id: number; name: string } | null;
   registrationEndDate: string;
   startDate: string;
