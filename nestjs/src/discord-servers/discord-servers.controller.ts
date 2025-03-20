4b4878cc11f94ee8a20d2d4a9329ee3074983b83
Alex Tokarev <aliaksei.tokarev@gmail.com>
2025-03-20 15:27:37 +0100

    fix: add acess to admin/courses page for course manager

--- nestjs/src/discord-servers/discord-servers.controller.ts
+++ nestjs/src/discord-servers/discord-servers.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Get, Post, Put, Delete, Param, Body, ParseIntPipe, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, ParseIntPipe, UseGuards, Req } from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { DefaultGuard, RequiredRoles, Role, RoleGuard } from '../auth';
+import { CourseRole, CurrentRequest, DefaultGuard, RequiredRoles, Role, RoleGuard } from '../auth';
 import { DiscordServersService } from './discord-servers.service';
 import { DiscordServerDto, CreateDiscordServerDto, UpdateDiscordServerDto } from './dto';
+import { IdNameDto } from 'src/core/dto';
 
 @Controller('discord-servers')
 @ApiTags('discord-servers')
-@RequiredRoles([Role.Admin])
 @UseGuards(DefaultGuard, RoleGuard)
 export class DiscordServersController {
   constructor(private readonly service: DiscordServersService) {}
 
   @Post()
+  @RequiredRoles([Role.Admin])
   @ApiOperation({ operationId: 'createDiscordServer' })
   @ApiOkResponse({ type: DiscordServerDto })
   public async create(@Body() dto: CreateDiscordServerDto) {
@@ -20,14 +21,17 @@ export class DiscordServersController {
   }
 
   @Get()
+  @RequiredRoles([Role.Admin, CourseRole.Manager])
   @ApiOperation({ operationId: 'getDiscordServers' })
   @ApiOkResponse({ type: [DiscordServerDto] })
-  public async getAll() {
+  public async getAll(@Req() req: CurrentRequest) {
+    const fullInfo = req.user.isAdmin;
     const items = await this.service.getAll();
-    return items.map(item => new DiscordServerDto(item));
+    return items.map(item => (fullInfo ? new DiscordServerDto(item) : new IdNameDto(item)));
   }
 
   @Put(':id')
+  @RequiredRoles([Role.Admin])
   @ApiOperation({ operationId: 'updateDiscordServer' })
   @ApiOkResponse({ type: DiscordServerDto })
   public async update(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateDiscordServerDto) {
@@ -36,6 +40,7 @@ export class DiscordServersController {
   }
 
   @Delete(':id')
+  @RequiredRoles([Role.Admin])
   @ApiOperation({ operationId: 'deleteDiscordServer' })
   @ApiOkResponse({ type: DiscordServerDto })
   public async delete(@Param('id', ParseIntPipe) id: number) {
