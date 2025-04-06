9d4a78780a3a60b094081e0dce6c0cf207624544
Pavel Bodal <pavelbodal@gmail.com>
2025-04-06 16:43:25 +0200

    style: change code indentation

--- server/src/migrations/1743946332709-CourseTaskProcessing.ts
+++ server/src/migrations/1743946332709-CourseTaskProcessing.ts
@@ -1,14 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CourseTaskProcessing1743946332709 implements MigrationInterface {
-    name = 'CourseTaskProcessing1743946332709'
+  name = 'CourseTaskProcessing1743946332709';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "course_task" ADD "isProcessing" boolean NOT NULL DEFAULT false`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "course_task" ADD "isProcessing" boolean NOT NULL DEFAULT false`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "course_task" DROP COLUMN "isProcessing"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "course_task" DROP COLUMN "isProcessing"`);
+  }
 }
