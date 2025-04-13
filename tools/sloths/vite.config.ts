f86a522f83862d9c64c97839f7bec280ad7b61ed
Maksim Shylau <shilaums@gmail.com>
2025-04-13 12:44:56 +0200

    chore: replace plugin with supported one

--- tools/sloths/vite.config.ts
+++ tools/sloths/vite.config.ts
@@ -3,13 +3,13 @@ import path from 'node:path';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueI18n from '@intlify/vite-plugin-vue-i18n';
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueI18n({
+    VueI18nPlugin({
       include: path.resolve(__dirname, './src/locales/**'),
     }),
   ],
