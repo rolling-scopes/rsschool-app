6c61a46ed15d90797bea5643cf93694d4cc777dc
Pavel Bodal <pavelbodal@gmail.com>
2025-05-17 09:15:10 +0200

    refactor: change migration file style

--- server/src/migrations/1747380525126-CourseTaskInterviewCreatingPairs.ts
+++ server/src/migrations/1747380525126-CourseTaskInterviewCreatingPairs.ts
@@ -1,14 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CourseTaskInterviewCreatingPairs1747380525126 implements MigrationInterface {
-    name = 'CourseTaskInterviewCreatingPairs1747380525126'
+  name = 'CourseTaskInterviewCreatingPairs1747380525126';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "course_task" ADD "isCreatingInterviewPairs" boolean NOT NULL DEFAULT false`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "course_task" ADD "isCreatingInterviewPairs" boolean NOT NULL DEFAULT false`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "course_task" DROP COLUMN "isCreatingInterviewPairs"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "course_task" DROP COLUMN "isCreatingInterviewPairs"`);
+  }
 }
