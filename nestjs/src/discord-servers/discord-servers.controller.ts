402c3c761ecfa6e71bffc7fe75139800e4aa11c5
Alex Tokarev <aliaksei.tokarev@gmail.com>
2025-07-08 22:12:21 +0200

    fix: rename getDiscordServerById endpoint to getInviteLinkByDiscordServerId with URL invite/:id

--- nestjs/src/discord-servers/discord-servers.controller.ts
+++ nestjs/src/discord-servers/discord-servers.controller.ts
@@ -38,9 +38,9 @@ export class DiscordServersController {
     return items.map(item => new IdNameDto(item));
   }
 
-  @Get(':id')
+  @Get('invite/:id')
   @RequiredRoles([Role.Admin, CourseRole.Mentor])
-  @ApiOperation({ operationId: 'getDiscordServerById' })
+  @ApiOperation({ operationId: 'getInviteLinkByDiscordServerId' })
   @ApiOkResponse({ type: String })
   public async getById(@Param('id', ParseIntPipe) id: number) {
     const item = await this.service.getById(id);
