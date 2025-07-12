fd5fd05f318bef35d041a4140512d12a36f5500e
Andrei Yurkouski <emp74ark@gmail.com>
2025-07-12 16:12:27 +0200

    refactor(theme): replace inline styles with theme token for the 'WelcomeCard' component

--- client/src/modules/TeamDistribution/components/WelcomeCard/WelcomeCard.tsx
+++ client/src/modules/TeamDistribution/components/WelcomeCard/WelcomeCard.tsx
@@ -1,4 +1,5 @@
-import { Button, Card, Col, Row, Typography } from 'antd';
+import { Button, Card, Col, Row, theme, Typography } from 'antd';
+
 import { PlusOutlined } from '@ant-design/icons';
 import { useMedia } from 'react-use';
 
@@ -11,21 +12,18 @@ type Props = {
 
 export default function WelcomeCard({ isManager, handleCreateTeamDistribution }: Props) {
   const mobileView = useMedia('(max-width: 768px)');
+  const { token } = theme.useToken();
   return (
     <Card
-      title={
-        <Text style={{ color: '#000' }}>
-          {isManager ? 'Create student teams to solve group tasks!' : 'Become a member of the team!'}
-        </Text>
-      }
+      title={<Text>{isManager ? 'Create student teams to solve group tasks!' : 'Become a member of the team!'}</Text>}
       style={{
         marginTop: 24,
-        backgroundColor: '#D6E4FF',
+        backgroundColor: token.colorPrimaryBg,
       }}
     >
       <Row gutter={[24, 12]} style={{ minHeight: '100px' }}>
         <Col sm={24} md={12}>
-          <Text style={{ color: '#000' }}>
+          <Text>
             Group task – it is a possibility to unite with your colleagues to develop the best solutions, and to gain
             knowledge and skills.
             <br /> To become a member of a team, you can create your own team or join an existing team. If you don't
