8dbbc8b09662d7dedebad77cb748619cb305ed2a
Andrei Yurkouski <emp74ark@gmail.com>
2025-11-04 18:55:35 +0100

    test: ExpandWidget tests

--- client/src/components/Profile/ui/ExpandButtonWidget.tsx
+++ client/src/components/Profile/ui/ExpandButtonWidget.tsx
@@ -3,7 +3,14 @@ import { Button } from 'antd';
 
 export function ExpandButtonWidget({ onClick }: { onClick: () => void }) {
   return (
-    <Button type="dashed" style={{padding: '0.3em 0.5em'}} onClick={onClick} aria-label="Open details" title="Open details">
+    <Button
+      type="dashed"
+      style={{ padding: '0.3em 0.5em' }}
+      onClick={onClick}
+      data-testid="expand-button"
+      aria-label="Open details"
+      title="Open details"
+    >
       <FullscreenOutlined />
     </Button>
   );
---
+++ client/src/components/Profile/ui/__tests__/ExpandButtonWidget.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import { ExpandButtonWidget } from '@client/components/Profile/ui';
+import userEvent from '@testing-library/user-event';
+
+describe('ExpandButtonWidget', () => {
+  it('should render correctly', () => {
+    render(<ExpandButtonWidget onClick={() => console.log('test')}/>)
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument()
+    expect(button.title).toBe('Open details')
+  })
+
+  it('should call onClick callback', async () => {
+    const onClick = jest.fn()
+    render(<ExpandButtonWidget onClick={onClick}/>)
+    const button = screen.getByRole('button');
+    await userEvent.click(button)
+    expect(onClick).toHaveBeenCalledTimes(1)
+  })
+});
