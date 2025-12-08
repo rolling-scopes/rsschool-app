a5ffcadaecd319733dc7e61f5789ef301ed1a6ec
Andrei Yurkouski <emp74ark@gmail.com>
2025-12-08 12:17:38 +0100

    fix: optimize solidarity svg and prioritize image loading

--- client/src/components/SolidarityUkraine.tsx
+++ client/src/components/SolidarityUkraine.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
 export function SolidarityUkraine() {
-  return <Image src="/static/svg/solidarity-Ukraine.svg" alt="Stand With Ukraine" width={146} height={32} />;
+  return (
+    <Image src="/static/svg/solidarity-with-Ukraine.svg" alt="Stand With Ukraine" width={146} height={32} priority />
+  );
 }
