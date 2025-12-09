3acc0fe025bf68f5c6087fb8e1435eab78c809d6
Andrei Yurkouski <emp74ark@gmail.com>
2025-12-09 22:22:49 +0100

    Revert "fix: optimize solidarity svg and prioritize image loading"
    This reverts commit a5ffcadaecd319733dc7e61f5789ef301ed1a6ec.

--- client/src/components/SolidarityUkraine.tsx
+++ client/src/components/SolidarityUkraine.tsx
@@ -1,7 +1,5 @@
 import Image from 'next/image';
 
 export function SolidarityUkraine() {
-  return (
-    <Image src="/static/svg/solidarity-with-Ukraine.svg" alt="Stand With Ukraine" width={146} height={32} priority />
-  );
+  return <Image src="/static/svg/solidarity-Ukraine.svg" alt="Stand With Ukraine" width={146} height={32} />;
 }
