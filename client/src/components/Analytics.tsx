9e89c7e68e9a9866740b6eed817f71dba8498328
Andrei Yurkouski <emp74ark@gmail.com>
2025-12-23 22:22:50 +0100

    chore: run analytics scripts on the client side only

--- client/src/components/Analytics.tsx
+++ client/src/components/Analytics.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Script from 'next/script';
 
 const enableAnalytics = process.env.NODE_ENV === 'production';
@@ -9,8 +11,16 @@ export function Analytics() {
 
   return (
     <>
-      <Script defer src="https://www.googletagmanager.com/gtag/js?id=G-WJLHZ9CCXJ" />
-      <Script id="google-analytics">
+      <Script
+        defer
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-WJLHZ9CCXJ"
+      />
+
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+      >
         {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
@@ -22,6 +32,7 @@ export function Analytics() {
 
       <Script
         defer
+        strategy="afterInteractive"
         src="https://static.cloudflareinsights.com/beacon.min.js"
         data-cf-beacon={'{"token": "e607238d732c4713b01b851ed3df61c2"}'}
       />
