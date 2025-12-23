3846328e43e9fa592bcb93bdd644c6126d67fcdd
Andrei Yurkouski <emp74ark@gmail.com>
2025-12-23 22:31:31 +0100

    refactor: handle errors in Google Analytics script initialization

--- client/src/components/Analytics.tsx
+++ client/src/components/Analytics.tsx
@@ -15,11 +15,17 @@ export function Analytics() {
 
       <Script id="google-analytics" strategy="afterInteractive">
         {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+        try {
+            if (window !== undefined) {
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-        gtag('config', 'G-WJLHZ9CCXJ');
+            gtag('config', 'G-WJLHZ9CCXJ');
+          }
+        } catch (error) {
+          console.error('Google analytics error', error);
+        }
       `}
       </Script>
 
