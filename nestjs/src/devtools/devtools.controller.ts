3bacf13e30c16d20c7525c3dbb71172dcdbd9476
Andrei Yurkouski <emp74ark@gmail.com>
2026-01-30 21:02:08 +0100

    feat: remove additional entity

--- nestjs/src/devtools/devtools.controller.ts
+++ nestjs/src/devtools/devtools.controller.ts
@@ -1,34 +1,17 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { DevtoolsService } from './devtools.service';
-import { CreateTestUserDto } from './dto/create-testUser.dto';
-import { UpdateTestUserDto } from './dto/update-testUser.dto';
 
 @Controller('devtools')
 export class DevtoolsController {
   constructor(private readonly devtoolsService: DevtoolsService) {}
 
-  @Get('test-users')
+  @Get('user')
   async getTestUsers() {
-    return this.devtoolsService.getTestUsers();
+    return this.devtoolsService.getUsers();
   }
 
-  @Get('test-user/:id')
-  async getTestUserById(@Param('id') id: string) {
-    return this.devtoolsService.getTestUserById({ id });
-  }
-
-  @Post('test-user')
-  async createTestUser(@Body() dto: CreateTestUserDto) {
-    return this.devtoolsService.createTestUser({ dto });
-  }
-
-  @Patch('test-user/:id')
-  async updateTestUserById(@Param('id') id: string, @Body() dto: UpdateTestUserDto) {
-    return this.devtoolsService.updateTestUserById({ dto, id });
-  }
-
-  @Delete('test-user/:id')
-  async deleteTestUserById(@Param('id') id: string) {
-    return this.devtoolsService.deleteTestUserById({ id });
+  @Get('user/:id')
+  async getTestUserById(@Param('id') id: number) {
+    return this.devtoolsService.getUserById({ id });
   }
 }
--- nestjs/src/devtools/devtools.service.ts
+++ nestjs/src/devtools/devtools.service.ts
@@ -1,33 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { TestUser } from './entities/testUser';
-import { CreateTestUserDto } from './dto/create-testUser.dto';
-import { UpdateTestUserDto } from './dto/update-testUser.dto';
+import { User } from '@entities/user';
 
 @Injectable()
 export class DevtoolsService {
   constructor(
-    @InjectRepository(TestUser)
-    private testUserRepository: Repository<TestUser>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
   ) {}
-  async getTestUsers() {
-    return this.testUserRepository.find();
+  async getUsers() {
+    return this.userRepository.find();
   }
 
-  async getTestUserById({ id }: { id: string }) {
-    return this.testUserRepository.findBy({ id });
-  }
-
-  async createTestUser({ dto }: { dto: CreateTestUserDto }) {
-    return this.testUserRepository.save(dto);
-  }
-
-  async updateTestUserById({ id, dto }: { id: string; dto: UpdateTestUserDto }) {
-    return this.testUserRepository.update(id, dto);
-  }
-
-  async deleteTestUserById({ id }: { id: string }) {
-    return this.testUserRepository.delete(id);
+  async getUserById({ id }: { id: number }) {
+    return this.userRepository.findBy({ id });
   }
 }
