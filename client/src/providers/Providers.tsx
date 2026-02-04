820bb6beab1bcd6b27bf41af3ae702bf8b23401e
Andrei Yurkouski <emp74ark@gmail.com>
2026-02-04 22:19:39 +0100

    refactor: show client devtools for non-production env

--- client/src/providers/Providers.tsx
+++ client/src/providers/Providers.tsx
@@ -6,7 +6,7 @@ import { DevToolsProvider } from './DevToolsProvider';
 type Props = { children: ReactNode };
 type Provider = (props: Props) => ReactNode;
 
-const isDev = process.env.NODE_ENV === 'development';
+const isProd = process.env.NODE_ENV === 'production';
 
 function composeProviders(...providers: Provider[]): Provider {
   return providers.reduceRight<Provider>(
@@ -20,4 +20,4 @@ function composeProviders(...providers: Provider[]): Provider {
   );
 }
 
-export const Providers = composeProviders(ThemeProvider, MessageProvider, ...(isDev ? [DevToolsProvider] : []));
+export const Providers = composeProviders(ThemeProvider, MessageProvider, ...(isProd ? [] : [DevToolsProvider]));
