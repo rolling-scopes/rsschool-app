2f758b6bd85d4bb8de6a92fc0f91a904ee04725e
Andrei <andrei@palchys.com>
2026-03-17 19:28:52 +0100

    fix(client): correct ant design prop replacements

--- client/src/modules/Tasks/components/TaskModal/TaskModal.tsx
+++ client/src/modules/Tasks/components/TaskModal/TaskModal.tsx
@@ -143,7 +143,7 @@ export function TaskModal({
         <Col span={24}>
           <Space direction="vertical" size={8} style={{ width: '100%', marginBottom: 24 }}>
             <Text>{LABELS.usedInCourses}</Text>
-            <Card bodyStyle={{ padding: 8 }}>
+            <Card styles={{ body: { padding: 8 } }}>
               {formData?.courses?.length ? (
                 <Space size={[0, 8]} wrap>
                   {formData.courses.map(({ name, isActive }) => (
