62f825dc3cb2505c7963f874432c9456f2abf210
apalchys <andrei@palchys.com>
2026-09-11 21:48:47 +0200

    test: share dashboard mentor card fixtures

--- client/src/modules/StudentDashboard/components/MentorCard/MentorCard.test.tsx
+++ client/src/modules/StudentDashboard/components/MentorCard/MentorCard.test.tsx
@@ -22,25 +22,6 @@ const PROPS_MOCK: MentorCardProps = {
 };
 
 describe('MentorCard', () => {
-  describe('when student has a mentor', () => {
-    it.each`
-      info
-      ${MENTOR_MOCK.githubId}
-      ${MENTOR_MOCK.name}
-      ${MENTOR_MOCK.cityName}
-      ${MENTOR_MOCK.countryName}
-      ${MENTOR_MOCK.contactsEmail}
-      ${MENTOR_MOCK.contactsNotes}
-      ${MENTOR_MOCK.contactsPhone}
-      ${MENTOR_MOCK.contactsSkype}
-      ${MENTOR_MOCK.contactsTelegram}
-    `('should render mentor info "$info"', ({ info }: { info: string }) => {
-      render(<MentorCard {...PROPS_MOCK} />);
-
-      expect(screen.getByText(new RegExp(info))).toBeInTheDocument();
-    });
-  });
-
   describe('when student does not have a mentor', () => {
     const propsWithoutMentor = { ...PROPS_MOCK, mentor: undefined };
 
@@ -48,26 +29,30 @@ describe('MentorCard', () => {
       render(<MentorCard {...propsWithoutMentor} />);
 
       expect(screen.queryByText(MENTOR_MOCK.githubId)).not.toBeInTheDocument();
-    });
-
-    it('should render note', () => {
-      render(<MentorCard {...propsWithoutMentor} />);
 
       expect(screen.getByText(ASSERTION)).toBeInTheDocument();
     });
   });
 
-  it('should render "Submit task" button', () => {
-    render(<MentorCard {...PROPS_MOCK} />);
-
-    const submitButton = screen.getByRole('button', { name: /submit task/i });
-    expect(submitButton).toBeInTheDocument();
-  });
-
   it('should open modal window when "Submit task" was clicked', async () => {
     render(<MentorCard {...PROPS_MOCK} />);
 
+    for (const info of [
+      MENTOR_MOCK.githubId,
+      MENTOR_MOCK.name,
+      MENTOR_MOCK.cityName,
+      MENTOR_MOCK.countryName,
+      MENTOR_MOCK.contactsEmail,
+      MENTOR_MOCK.contactsNotes,
+      MENTOR_MOCK.contactsPhone,
+      MENTOR_MOCK.contactsSkype,
+      MENTOR_MOCK.contactsTelegram,
+    ]) {
+      expect(screen.getByText(new RegExp(info))).toBeInTheDocument();
+    }
+
     const submitButton = screen.getByRole('button', { name: /submit task/i });
+    expect(submitButton).toBeInTheDocument();
     fireEvent.click(submitButton);
 
     const modal = await screen.findByRole('dialog', { name: /submit task for mentor review/i });
