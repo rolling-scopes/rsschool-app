1402ff07b6b4bfeb56ffe29b5172918a1235ad92
apalchys <andrei@palchys.com>
2026-09-12 04:40:53 +0200

    test(client): streamline profile expand button

--- client/src/components/Profile/ui/__tests__/ExpandButtonWidget.test.tsx
+++ client/src/components/Profile/ui/__tests__/ExpandButtonWidget.test.tsx
@@ -1,20 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { ExpandButtonWidget } from '@client/components/Profile/ui';
-import userEvent from '@testing-library/user-event';
 
 describe('ExpandButtonWidget', () => {
-  it('should render correctly', () => {
-    render(<ExpandButtonWidget onClick={() => console.log('test')} />);
-    const button = screen.getByRole('button');
-    expect(button).toBeInTheDocument();
-    expect(button.title).toBe('Open details');
-  });
-
-  it('should call onClick callback', async () => {
+  it('renders correctly and calls onClick', () => {
     const onClick = vi.fn();
     render(<ExpandButtonWidget onClick={onClick} />);
     const button = screen.getByRole('button');
-    await userEvent.click(button);
+
+    expect(button).toBeInTheDocument();
+    expect(button.title).toBe('Open details');
+    fireEvent.click(button);
     expect(onClick).toHaveBeenCalledTimes(1);
   });
 });
