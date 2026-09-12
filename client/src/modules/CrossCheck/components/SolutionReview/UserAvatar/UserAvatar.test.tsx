4bb365050a82e087af73dcc571f700a7df526745
apalchys <andrei@palchys.com>
2026-09-12 04:48:27 +0200

    test(client): reuse solution review avatar render

--- client/src/modules/CrossCheck/components/SolutionReview/UserAvatar/UserAvatar.test.tsx
+++ client/src/modules/CrossCheck/components/SolutionReview/UserAvatar/UserAvatar.test.tsx
@@ -10,40 +10,28 @@ function getAvatarImg(container: HTMLElement) {
 }
 
 describe('<UserAvatar />', () => {
-  it('uses the github avatar for a reviewer when contacts are visible', () => {
-    const { container } = render(
+  it('selects the avatar for each role and visibility state', () => {
+    const { container, rerender } = render(
       <UserAvatar author={author} role={CrossCheckMessageDtoRoleEnum.Reviewer} areContactsVisible size={32} />,
     );
 
     expect(getAvatarImg(container)).toHaveAttribute('src', 'https://cdn.rs.school/avatars/octocat.png?size=64');
-  });
 
-  it('uses the expert icon for a reviewer when contacts are hidden', () => {
-    const { container } = render(
+    rerender(
       <UserAvatar author={author} role={CrossCheckMessageDtoRoleEnum.Reviewer} areContactsVisible={false} size={32} />,
     );
 
     expect(getAvatarImg(container)).toHaveAttribute('src', '/static/svg/sloths/Expert.svg');
-  });
 
-  it('uses the expert icon for a reviewer when author is null', () => {
-    const { container } = render(
-      <UserAvatar author={null} role={CrossCheckMessageDtoRoleEnum.Reviewer} areContactsVisible size={32} />,
-    );
+    rerender(<UserAvatar author={null} role={CrossCheckMessageDtoRoleEnum.Reviewer} areContactsVisible size={32} />);
 
     expect(getAvatarImg(container)).toHaveAttribute('src', '/static/svg/sloths/Expert.svg');
-  });
 
-  it('uses the github avatar for a student when contacts are visible (size doubled)', () => {
-    const { container } = render(
-      <UserAvatar author={author} role={CrossCheckMessageDtoRoleEnum.Student} areContactsVisible size={24} />,
-    );
+    rerender(<UserAvatar author={author} role={CrossCheckMessageDtoRoleEnum.Student} areContactsVisible size={24} />);
 
     expect(getAvatarImg(container)).toHaveAttribute('src', 'https://cdn.rs.school/avatars/octocat.png?size=48');
-  });
 
-  it('uses the thanks icon for a student when contacts are hidden', () => {
-    const { container } = render(
+    rerender(
       <UserAvatar author={author} role={CrossCheckMessageDtoRoleEnum.Student} areContactsVisible={false} size={24} />,
     );
 
